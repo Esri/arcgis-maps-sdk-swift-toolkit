d375e4cbf302abe04f93c90e54646332593eb413
Mark Dostal <mhdostal@aol.com>
2021-05-14 17:18:41 -0500

    Create and setup initial Toolkit swift package.

---
+++ Tests/ArcGISToolkitSwiftTests/arcgis_runtime_toolkit_swiftTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+@testable import ArcGISToolkitSwift
+
+final class arcgis_runtime_toolkit_swiftTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
