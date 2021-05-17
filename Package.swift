7b182cfe2c4db441d75457db3e651796eabd8f77
Mark Dostal <mhdostal@aol.com>
2021-05-17 14:29:13 -0500

    use relative url for ArcGIS package

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "ArcGIS", path: "/Users/Shared/Source/ArcGIS/swift")
+        .package(name: "ArcGIS", path: "../swift")
 
     ],
     targets: [
