2b68f637844d7507f5c98b15258d20e9ed747340
Mark Dostal <mhdostal@aol.com>
2021-05-24 10:28:28 -0500

    Add copyright to Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,17 @@
 ***REMOVED*** swift-tools-version:5.3
 ***REMOVED*** The swift-tools-version declares the minimum version of Swift required to build this package.
+***REMOVED***.
+
+***REMOVED*** Licensed under the Apache License, Version 2.0 (the "License");
+***REMOVED*** you may not use this file except in compliance with the License.
+***REMOVED*** You may obtain a copy of the License at
+***REMOVED*** http:***REMOVED***www.apache.org/licenses/LICENSE-2.0
+
+***REMOVED*** Unless required by applicable law or agreed to in writing, software
+***REMOVED*** distributed under the License is distributed on an "AS IS" BASIS,
+***REMOVED*** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+***REMOVED*** See the License for the specific language governing permissions and
+***REMOVED*** limitations under the License.
 
 import PackageDescription
 
