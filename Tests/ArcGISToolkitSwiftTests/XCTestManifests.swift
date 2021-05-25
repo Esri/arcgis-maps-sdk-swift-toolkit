5bdeff743b5f48a39a297de70a4e9cf1e764b615
Mark Dostal <mhdostal@aol.com>
2021-05-25 13:36:32 -0500

    Add .gitignore; remove unnecessary file.

--- Tests/ArcGISToolkitSwiftTests/XCTestManifests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-***REMOVED***return [
-***REMOVED******REMOVED***testCase(arcgis_runtime_toolkit_swiftTests.allTests),
-***REMOVED***]
-***REMOVED***
-#endif
