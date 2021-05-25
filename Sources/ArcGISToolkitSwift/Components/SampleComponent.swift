68b8b560245a32546019223078722aff3084f7da
Mark Dostal <mdostal@esri.com>
2021-05-25 13:24:32 -0500

    Update Sources/ArcGISToolkitSwift/Components/SampleComponent.swift
    Co-authored-by: Philip Ridgeway <philip.ridgeway@gmail.com>

--- Sources/ArcGISToolkitSwift/Components/SampleComponent.swift
+++ Sources/ArcGISToolkitSwift/Components/SampleComponent.swift
@@ -15,7 +15,7 @@ import SwiftUI
 import ArcGIS
 
 public struct SampleComponent: View {
-    public  init(mapView: MapView) {
+    public init(mapView: MapView) {
         self.mapView = mapView
     }
     
