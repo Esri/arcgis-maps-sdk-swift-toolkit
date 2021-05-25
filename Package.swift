64ebf6ebac231519cf8c6e29baf568b7673b8fc4
Mark Dostal <mhdostal@aol.com>
2021-05-25 13:41:45 -0500

    Remove unnecessary descriptions.

--- Package.swift
+++ Package.swift
@@ -22,20 +22,15 @@ let package = Package(
 ***REMOVED******REMOVED***.macOS(.v11)
 ***REMOVED***],
 ***REMOVED***products: [
-***REMOVED******REMOVED******REMOVED*** Products define the executables and libraries a package produces, and make them visible to other packages.
 ***REMOVED******REMOVED***.library(
 ***REMOVED******REMOVED******REMOVED***name: "ArcGISToolkitSwift",
 ***REMOVED******REMOVED******REMOVED***targets: ["ArcGISToolkitSwift"]),
 ***REMOVED***],
 ***REMOVED***dependencies: [
-***REMOVED******REMOVED******REMOVED*** Dependencies declare other packages that this package depends on.
-***REMOVED******REMOVED******REMOVED*** .package(url: /* package url */, from: "1.0.0"),
 ***REMOVED******REMOVED***.package(name: "ArcGIS", path: "../swift")
 
 ***REMOVED***],
 ***REMOVED***targets: [
-***REMOVED******REMOVED******REMOVED*** Targets are the basic building blocks of a package. A target can define a module or a test suite.
-***REMOVED******REMOVED******REMOVED*** Targets can depend on other targets in this package, and on products in packages this package depends on.
 ***REMOVED******REMOVED***.target(
 ***REMOVED******REMOVED******REMOVED***name: "ArcGISToolkitSwift",
 ***REMOVED******REMOVED******REMOVED***dependencies: ["ArcGIS"]),
