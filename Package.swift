3be9e7156f461bd175cb9a89ec222995df723f8a
Mark Dostal <mdostal@esri.com>
2021-06-02 15:37:57 -0500

    Update Package.swift
    Co-authored-by: Zachary A Kline <zkline@esri.com>

--- Package.swift
+++ Package.swift
@@ -28,7 +28,6 @@ let package = Package(
 ***REMOVED***],
 ***REMOVED***dependencies: [
 ***REMOVED******REMOVED***.package(name: "ArcGIS", path: "../swift")
-
 ***REMOVED***],
 ***REMOVED***targets: [
 ***REMOVED******REMOVED***.target(
