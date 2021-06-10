0a80722c65782c5aae944d72e19492c71aaa89ae
Mark Dostal <mhdostal@aol.com>
2021-06-10 11:33:51 -0500

    Documentation and cleanup.

--- Sources/ArcGISToolkit/Components/OverviewMap.swift
+++ Sources/ArcGISToolkit/Components/OverviewMap.swift
@@ -18,33 +18,41 @@
 import Combine
 ***REMOVED***
 
-***REMOVED***/ <#Description#>
+***REMOVED***/ OverviewMap is a small, secondary MapView (sometimes called an "inset map"), superimposed on an existing MapView, which shows the visible extent of the main MapView.
 public struct OverviewMap: View {
-***REMOVED******REMOVED***/ <#Description#>
+***REMOVED******REMOVED***/ A binding to an optional `MapViewProxy`. When a proxy is
+***REMOVED******REMOVED***/   available, the binding will be updated by the view. The proxy is
+***REMOVED******REMOVED***/   necessary for accessing `MapView` functionality to get and set viewpoints.
 ***REMOVED***public var proxy: Binding<MapViewProxy?>
 
-***REMOVED******REMOVED***/ <#Description#>
+***REMOVED******REMOVED***/ The Map displayed in the OverviewMap.
 ***REMOVED***public var map: Map
 
-***REMOVED******REMOVED***/ <#Description#>
-***REMOVED***public var extentSymbol: SimpleFillSymbol
+***REMOVED******REMOVED***/ The fill symbol used to display the main MapView extent.
+***REMOVED******REMOVED***/ The default is a transparent SimpleFillSymbol with a red, 1 point width outline.
+***REMOVED***public var extentSymbol: FillSymbol
 ***REMOVED***
-***REMOVED******REMOVED***/ <#Description#>
+***REMOVED******REMOVED***/ The factor to multiply the main GeoView's scale by. The OverviewMap will display
+***REMOVED******REMOVED***/ at the product of mainGeoViewScale * scaleFactor.  The default is 25.0.
 ***REMOVED***public var scaleFactor: Double
 ***REMOVED***
-***REMOVED******REMOVED***/ The geometry of the extent Graphic displaying the main map view's extent.
+***REMOVED******REMOVED***/ The geometry of the extent Graphic displaying the main map view's extent.  Updating
+***REMOVED******REMOVED***/ this property will update the display of the OverviewMap.
 ***REMOVED***@State private var extentGeometry: Envelope?
 
+***REMOVED******REMOVED***/ The viewpoint of the OverviewMap's MapView.  Updating
+***REMOVED******REMOVED***/ this property will update the display of the OverviewMap.
 ***REMOVED***@State private var overviewMapViewpoint: Viewpoint?
 ***REMOVED***
-***REMOVED******REMOVED***/ <#Description#>
+***REMOVED******REMOVED***/ Creates an OverviewMap.
 ***REMOVED******REMOVED***/ - Parameters:
-***REMOVED******REMOVED***/   - proxy: <#proxy description#>
-***REMOVED******REMOVED***/   - map: <#map description#>
-***REMOVED******REMOVED***/   - extentSymbol: <#extentSymbol description#>
+***REMOVED******REMOVED***/   - proxy: The binding to an optional MapViewProxy.
+***REMOVED******REMOVED***/   - map: The Map to display in the OverviewMap.
+***REMOVED******REMOVED***/   - extentSymbol: The FillSymbol used to display the main MapView's extent.
+***REMOVED******REMOVED***/   - scaleFactor: The scale factor used to calculate the OverviewMap's scale.
 ***REMOVED***public init(proxy: Binding<MapViewProxy?>,
 ***REMOVED******REMOVED******REMOVED******REMOVED***map: Map = Map(basemap: Basemap.topographic()),
-***REMOVED******REMOVED******REMOVED******REMOVED***extentSymbol: SimpleFillSymbol = SimpleFillSymbol(
+***REMOVED******REMOVED******REMOVED******REMOVED***extentSymbol: FillSymbol = SimpleFillSymbol(
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***style: .solid,
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***color: .clear,
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***outline: SimpleLineSymbol(
@@ -60,7 +68,7 @@ public struct OverviewMap: View {
 ***REMOVED******REMOVED***self.extentSymbol = extentSymbol
 ***REMOVED******REMOVED***self.scaleFactor = scaleFactor
 ***REMOVED***
-
+***REMOVED***
 ***REMOVED***public var body: some View {
 ***REMOVED******REMOVED***ZStack {
 ***REMOVED******REMOVED******REMOVED***MapView(map: map,
@@ -73,24 +81,10 @@ public struct OverviewMap: View {
 ***REMOVED******REMOVED******REMOVED******REMOVED***.border(Color.black, width: 1)
 ***REMOVED******REMOVED******REMOVED******REMOVED***.onReceive(proxy.wrappedValue?.viewpointChangedPublisher
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.receive(on: DispatchQueue.main)
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** I think "throttle" is what we need here because that does
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** not reset the timer when a new value is published, whereas
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** debounce will reset the timer, so if there are multiple
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** values published that arriver faster than the timeout,
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** none of them will be sent because the timer gets reset.
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** That said, neither of these solve the issue of the
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** extent rectangle not getting drawn while the user is
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** panning the map.  The extent rectangle disappears when the user
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** starts panning and will not reappear until the panning stops.
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.debounce(for: .seconds(0.25),
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***  scheduler: DispatchQueue.main)
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.throttle(for: .seconds(0.25),
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***  scheduler: DispatchQueue.main,
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***  latest: true
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***)
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.throttle(for: .seconds(0.25),
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***  scheduler: DispatchQueue.main,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***  latest: true
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***)
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.eraseToAnyPublisher() ?? Empty<Void, Never>().eraseToAnyPublisher()
 ***REMOVED******REMOVED******REMOVED******REMOVED***) {
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***guard let centerAndScaleViewpoint = proxy.wrappedValue?.currentViewpoint(type: .centerAndScale),
@@ -102,11 +96,10 @@ public struct OverviewMap: View {
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***if let newExtent = boundingGeometryViewpoint.targetGeometry as? Envelope {
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***extentGeometry = newExtent
 ***REMOVED******REMOVED******REMOVED******REMOVED***
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***
+
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***if let viewpointGeometry = centerAndScaleViewpoint.targetGeometry as? Point {
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***let viewpoint = Viewpoint(center: viewpointGeometry,
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***  scale: centerAndScaleViewpoint.targetScale * scaleFactor)
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***print("overviewMapViewpoint updated")
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***overviewMapViewpoint = viewpoint
 ***REMOVED******REMOVED******REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED***
