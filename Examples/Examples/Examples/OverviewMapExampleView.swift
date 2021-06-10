ace40422efb6492cd9bdf3639c9ce7007ab414a4
Mark Dostal <mhdostal@aol.com>
2021-06-10 16:42:51 -0500

    Remove extra characters in copyright notice.

--- Examples/Examples/Examples/OverviewMapExampleView.swift
+++ Examples/Examples/Examples/OverviewMapExampleView.swift
@@ -1,4 +1,4 @@
-// Copyright 2021 Esri./APsPl0cx9SjC7zrU5rCZKCw
+// Copyright 2021 Esri.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
