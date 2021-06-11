63123f892a703dcb998ec6ef1f4f5ba9752330b6
Mark Dostal <mhdostal@aol.com>
2021-06-11 12:53:06 -0500

    Doc changes and consolidation of `guard` and `if` statements.

--- Sources/ArcGISToolkit/Components/OverviewMap.swift
+++ Sources/ArcGISToolkit/Components/OverviewMap.swift
@@ -15,38 +15,38 @@ import SwiftUI
 import Combine
 import ArcGIS
 
-/// OverviewMap is a small, secondary MapView (sometimes called an "inset map"), superimposed on an existing MapView, which shows the visible extent of the main MapView.
+/// `OverviewMap` is a small, secondary `MapView` (sometimes called an "inset map"), superimposed on an existing `MapView`, which shows the visible extent of the main `MapView`.
 public struct OverviewMap: View {
     /// A binding to an optional `MapViewProxy`. When a proxy is
     /// available, the binding will be updated by the view. The proxy is
     /// necessary for accessing `MapView` functionality to get and set viewpoints.
     public var proxy: Binding<MapViewProxy?>
     
-    /// The Map displayed in the OverviewMap.
+    /// The `Map` displayed in the `OverviewMap`.
     public var map: Map
     
-    /// The fill symbol used to display the main MapView extent.
-    /// The default is a transparent SimpleFillSymbol with a red, 1 point width outline.
+    /// The fill symbol used to display the main `MapView` extent.
+    /// The default is a transparent `SimpleFillSymbol` with a red, 1 point width outline.
     public var extentSymbol: FillSymbol
     
-    /// The factor to multiply the main GeoView's scale by. The OverviewMap will display
+    /// The factor to multiply the main `GeoView`'s scale by. The `OverviewMap` will display
     /// at the product of mainGeoViewScale * scaleFactor. The default is `25.0`.
     public var scaleFactor: Double
     
-    /// The geometry of the extent Graphic displaying the main map view's extent. Updating
-    /// this property will update the display of the OverviewMap.
+    /// The geometry of the extent `Graphic` displaying the main `MapView`'s extent. Updating
+    /// this property will update the display of the `OverviewMap`.
     @State private var extentGeometry: Envelope?
     
-    /// The viewpoint of the OverviewMap's MapView. Updating
-    /// this property will update the display of the OverviewMap.
+    /// The viewpoint of the `OverviewMap'`s `MapView`. Updating
+    /// this property will update the display of the `OverviewMap`.
     @State private var overviewMapViewpoint: Viewpoint?
     
-    /// Creates an OverviewMap.
+    /// Creates an `OverviewMap`.
     /// - Parameters:
-    ///   - proxy: The binding to an optional MapViewProxy.
-    ///   - map: The Map to display in the OverviewMap.
-    ///   - extentSymbol: The FillSymbol used to display the main MapView's extent.
-    ///   - scaleFactor: The scale factor used to calculate the OverviewMap's scale.
+    ///   - proxy: The binding to an optional `MapViewProxy`.
+    ///   - map: The `Map` to display in the `OverviewMap`.
+    ///   - extentSymbol: The `FillSymbol` used to display the main `MapView`'s extent.
+    ///   - scaleFactor: The scale factor used to calculate the `OverviewMap`'s scale.
     public init(proxy: Binding<MapViewProxy?>,
                 map: Map = Map(basemap: Basemap.topographic()),
                 extentSymbol: FillSymbol = SimpleFillSymbol(
@@ -85,18 +85,14 @@ public struct OverviewMap: View {
                         .eraseToAnyPublisher() ?? Empty<Void, Never>().eraseToAnyPublisher()
             ) {
                 guard let centerAndScaleViewpoint = proxy.wrappedValue?.currentViewpoint(type: .centerAndScale),
-                      let boundingGeometryViewpoint = proxy.wrappedValue?.currentViewpoint(type: .boundingGeometry)
+                      let boundingGeometryViewpoint = proxy.wrappedValue?.currentViewpoint(type: .boundingGeometry),
+                      let newExtent = boundingGeometryViewpoint.targetGeometry as? Envelope,
+                      let newCenter = centerAndScaleViewpoint.targetGeometry as? Point
                 else { return }
                 
-                if let newExtent = boundingGeometryViewpoint.targetGeometry as? Envelope {
-                    extentGeometry = newExtent
-                }
-                
-                if let viewpointGeometry = centerAndScaleViewpoint.targetGeometry as? Point {
-                    let viewpoint = Viewpoint(center: viewpointGeometry,
-                                              scale: centerAndScaleViewpoint.targetScale * scaleFactor)
-                    overviewMapViewpoint = viewpoint
-                }
+                extentGeometry = newExtent
+                overviewMapViewpoint = Viewpoint(center: newCenter,
+                                                 scale: centerAndScaleViewpoint.targetScale * scaleFactor)
             }
         }
     }
