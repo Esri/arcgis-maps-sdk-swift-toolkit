8d3a57602a920b9451643deb9751798bf5d2f48a
Mark Dostal <mhdostal@aol.com>
2021-06-11 12:53:06 -0500

    Doc changes and consolidation of `guard` and `if` statements.

--- Sources/ArcGISToolkit/Components/OverviewMap.swift
+++ Sources/ArcGISToolkit/Components/OverviewMap.swift
@@ -15,38 +15,38 @@
 import Combine
 ***REMOVED***
 
-***REMOVED***/ OverviewMap is a small, secondary MapView (sometimes called an "inset map"), superimposed on an existing MapView, which shows the visible extent of the main MapView.
+***REMOVED***/ `OverviewMap` is a small, secondary `MapView` (sometimes called an "inset map"), superimposed on an existing `MapView`, which shows the visible extent of the main `MapView`.
 public struct OverviewMap: View {
 ***REMOVED******REMOVED***/ A binding to an optional `MapViewProxy`. When a proxy is
 ***REMOVED******REMOVED***/ available, the binding will be updated by the view. The proxy is
 ***REMOVED******REMOVED***/ necessary for accessing `MapView` functionality to get and set viewpoints.
 ***REMOVED***public var proxy: Binding<MapViewProxy?>
 ***REMOVED***
-***REMOVED******REMOVED***/ The Map displayed in the OverviewMap.
+***REMOVED******REMOVED***/ The `Map` displayed in the `OverviewMap`.
 ***REMOVED***public var map: Map
 ***REMOVED***
-***REMOVED******REMOVED***/ The fill symbol used to display the main MapView extent.
-***REMOVED******REMOVED***/ The default is a transparent SimpleFillSymbol with a red, 1 point width outline.
+***REMOVED******REMOVED***/ The fill symbol used to display the main `MapView` extent.
+***REMOVED******REMOVED***/ The default is a transparent `SimpleFillSymbol` with a red, 1 point width outline.
 ***REMOVED***public var extentSymbol: FillSymbol
 ***REMOVED***
-***REMOVED******REMOVED***/ The factor to multiply the main GeoView's scale by. The OverviewMap will display
+***REMOVED******REMOVED***/ The factor to multiply the main `GeoView`'s scale by. The `OverviewMap` will display
 ***REMOVED******REMOVED***/ at the product of mainGeoViewScale * scaleFactor. The default is `25.0`.
 ***REMOVED***public var scaleFactor: Double
 ***REMOVED***
-***REMOVED******REMOVED***/ The geometry of the extent Graphic displaying the main map view's extent. Updating
-***REMOVED******REMOVED***/ this property will update the display of the OverviewMap.
+***REMOVED******REMOVED***/ The geometry of the extent `Graphic` displaying the main `MapView`'s extent. Updating
+***REMOVED******REMOVED***/ this property will update the display of the `OverviewMap`.
 ***REMOVED***@State private var extentGeometry: Envelope?
 ***REMOVED***
-***REMOVED******REMOVED***/ The viewpoint of the OverviewMap's MapView. Updating
-***REMOVED******REMOVED***/ this property will update the display of the OverviewMap.
+***REMOVED******REMOVED***/ The viewpoint of the `OverviewMap'`s `MapView`. Updating
+***REMOVED******REMOVED***/ this property will update the display of the `OverviewMap`.
 ***REMOVED***@State private var overviewMapViewpoint: Viewpoint?
 ***REMOVED***
-***REMOVED******REMOVED***/ Creates an OverviewMap.
+***REMOVED******REMOVED***/ Creates an `OverviewMap`.
 ***REMOVED******REMOVED***/ - Parameters:
-***REMOVED******REMOVED***/   - proxy: The binding to an optional MapViewProxy.
-***REMOVED******REMOVED***/   - map: The Map to display in the OverviewMap.
-***REMOVED******REMOVED***/   - extentSymbol: The FillSymbol used to display the main MapView's extent.
-***REMOVED******REMOVED***/   - scaleFactor: The scale factor used to calculate the OverviewMap's scale.
+***REMOVED******REMOVED***/   - proxy: The binding to an optional `MapViewProxy`.
+***REMOVED******REMOVED***/   - map: The `Map` to display in the `OverviewMap`.
+***REMOVED******REMOVED***/   - extentSymbol: The `FillSymbol` used to display the main `MapView`'s extent.
+***REMOVED******REMOVED***/   - scaleFactor: The scale factor used to calculate the `OverviewMap`'s scale.
 ***REMOVED***public init(proxy: Binding<MapViewProxy?>,
 ***REMOVED******REMOVED******REMOVED******REMOVED***map: Map = Map(basemap: Basemap.topographic()),
 ***REMOVED******REMOVED******REMOVED******REMOVED***extentSymbol: FillSymbol = SimpleFillSymbol(
@@ -85,18 +85,14 @@ public struct OverviewMap: View {
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.eraseToAnyPublisher() ?? Empty<Void, Never>().eraseToAnyPublisher()
 ***REMOVED******REMOVED******REMOVED***) {
 ***REMOVED******REMOVED******REMOVED******REMOVED***guard let centerAndScaleViewpoint = proxy.wrappedValue?.currentViewpoint(type: .centerAndScale),
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***  let boundingGeometryViewpoint = proxy.wrappedValue?.currentViewpoint(type: .boundingGeometry)
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***  let boundingGeometryViewpoint = proxy.wrappedValue?.currentViewpoint(type: .boundingGeometry),
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***  let newExtent = boundingGeometryViewpoint.targetGeometry as? Envelope,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***  let newCenter = centerAndScaleViewpoint.targetGeometry as? Point
 ***REMOVED******REMOVED******REMOVED******REMOVED***else { return ***REMOVED***
 ***REMOVED******REMOVED******REMOVED******REMOVED***
-***REMOVED******REMOVED******REMOVED******REMOVED***if let newExtent = boundingGeometryViewpoint.targetGeometry as? Envelope {
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***extentGeometry = newExtent
-***REMOVED******REMOVED******REMOVED***
-***REMOVED******REMOVED******REMOVED******REMOVED***
-***REMOVED******REMOVED******REMOVED******REMOVED***if let viewpointGeometry = centerAndScaleViewpoint.targetGeometry as? Point {
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***let viewpoint = Viewpoint(center: viewpointGeometry,
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***  scale: centerAndScaleViewpoint.targetScale * scaleFactor)
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***overviewMapViewpoint = viewpoint
-***REMOVED******REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED******REMOVED***extentGeometry = newExtent
+***REMOVED******REMOVED******REMOVED******REMOVED***overviewMapViewpoint = Viewpoint(center: newCenter,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** scale: centerAndScaleViewpoint.targetScale * scaleFactor)
 ***REMOVED******REMOVED***
 ***REMOVED***
 ***REMOVED***
