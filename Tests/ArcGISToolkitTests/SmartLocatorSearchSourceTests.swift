684bb7b3981944b3542b1c037e8be48ad48eb325
Mark Dostal <mhdostal@aol.com>
2021-08-12 11:07:07 -0500

    More tests.

--- Tests/ArcGISToolkitTests/SmartLocatorSearchSourceTests.swift
+++ Tests/ArcGISToolkitTests/SmartLocatorSearchSourceTests.swift
@@ -19,118 +19,56 @@ import ArcGISToolkit
 import SwiftUI
 
 class SmartLocatorSearchSourceTests: XCTestCase {
-}
+    func testRepeatSearchResultThreshold() async throws {
+        let locator = SmartLocatorSearchSource()
+        locator.searchArea = Envelope.edinburgh
 
-//class SearchViewModelTests: XCTestCase {
-//    func testAcceptSuggestion() async throws {
-//        let model = SearchViewModel(sources: [LocatorSearchSource()])
-//
-//        model.currentQuery = "Magers & Quinn Bookseller"
-//        await model.updateSuggestions()
-//        let suggestionionResults = try XCTUnwrap(model.suggestions.get())
-//        let suggestion = try XCTUnwrap(suggestionionResults.first)
-//
-//        await model.acceptSuggestion(suggestion)
-//        let results = try XCTUnwrap(model.results.get())
-//        XCTAssertEqual(results.count, 1)
-//
-//        // TODO:  look into setting model.selectedResults in didSet of `results`.
-//        XCTAssertNotNil(model.selectedResult)
-//        try XCTAssertNil(model.suggestions.get())
-//    }
-//
-//    func testCommitSearch() async throws {
-//        let model = SearchViewModel(sources: [LocatorSearchSource()])
-//
-//        // No search - results are nil.
-//        try XCTAssertNil(model.results.get())
-//
-//        // Search with no results - result count is 0.
-//        model.currentQuery = "No results found blah blah blah blah"
-//        await model.commitSearch(false)
-//        var results = try XCTUnwrap(model.results.get())
-//        XCTAssertEqual(results.count, 0)
-//
-//        XCTAssertNil(model.selectedResult)
-//        try XCTAssertNil(model.suggestions.get())
-//
-//        // Search with one result.
-//        model.currentQuery = "Magers & Quinn Bookseller"
-//        await model.commitSearch(false)
-//        results = try XCTUnwrap(model.results.get())
-//        XCTAssertEqual(results.count, 1)
-//
-//        // One results automatically populates `selectedResult`.
-//        XCTAssertNotNil(model.selectedResult)
-//        try XCTAssertNil(model.suggestions.get())
-//
-//        // Search with multiple results.
-//        model.currentQuery = "Magers & Quinn"
-//        await model.commitSearch(false)
-//        results = try XCTUnwrap(model.results.get())
-//        XCTAssertGreaterThanOrEqual(results.count, 1)
-//
-//        XCTAssertNil(model.selectedResult)
-//        try XCTAssertNil(model.suggestions.get())
-//    }
-//
-//    func testIsEligibleForRequery() async {
-//        let model = SearchViewModel(sources: [LocatorSearchSource()])
-//
-//        // isEligibleForRequery defaults to `false`.
-//        XCTAssertFalse(model.isEligibleForRequery)
-//
-//        // There are no results, so setting `queryArea` has
-//        // no effect on `isEligibleForRequery`.
-//        model.queryArea = createPolygon()
-//        XCTAssertFalse(model.isEligibleForRequery)
-//
-//        // We have results and a new polygon, `isEligibleForRequery` is true.
-//        model.currentQuery = "Magers & Quinn Bookseller"
-//        await model.commitSearch(false)
-//        model.queryArea = createPolygon()
-//        XCTAssertTrue(model.isEligibleForRequery)
-//    }
-//
-//    func testUpdateSuggestions() async throws {
-//        let model = SearchViewModel(sources: [LocatorSearchSource()])
-//
-//        // No currentQuery - suggestions are nil.
-//        try XCTAssertNil(model.suggestions.get())
-//
-//        // UpdateSuggestions with no results - result count is 0.
-//        model.currentQuery = "No results found blah blah blah blah"
-//        await model.updateSuggestions()
-//        var results = try XCTUnwrap(model.suggestions.get())
-//        XCTAssertEqual(results.count, 0)
-//
-//        XCTAssertNil(model.selectedResult)
-//        try XCTAssertNil(model.results.get())
-//
-//        // UpdateSuggestions with results.
-//        model.currentQuery = "Magers & Quinn"
-//        await model.updateSuggestions()
-//        results = try XCTUnwrap(model.suggestions.get())
-//        XCTAssertGreaterThanOrEqual(results.count, 1)
-//
-//        XCTAssertNil(model.selectedResult)
-//        try XCTAssertNil(model.results.get())
-//    }
-//
-//}
+        // Threshold of 0 means no re-query.
+        locator.repeatSearchResultThreshold = 0
+        var searchResults = try await locator.search(
+            "Dunkin' Donuts",
+            area: nil
+        )
+        var results = try XCTUnwrap(searchResults)
+        XCTAssertEqual(results.count, 0)
+        
+        // Threshold of 1+ means requery with fewer restrictions
+        locator.repeatSearchResultThreshold = 1
+        searchResults = try await locator.search(
+            "Dunkin' Donuts",
+            area: nil
+        )
+        results = try XCTUnwrap(searchResults)
+        XCTAssertGreaterThanOrEqual(results.count, 1)
+    }
+    
+    func testRepeatSuggestResultThreshold() async throws {
+        let locator = SmartLocatorSearchSource()
+        locator.searchArea = Envelope.edinburgh
 
-extension SearchViewModelTests {
-    func createPolygon() -> Polygon {
-        let builder = PolygonBuilder(spatialReference: .wgs84)
-        let _ = builder.add(point: .london)
-        let _ = builder.add(point: .paris)
-        let _ = builder.add(point: .rome)
-        return builder.toGeometry() as! ArcGIS.Polygon
+        // Threshold of 0 means no re-query.
+        locator.repeatSuggestResultThreshold = 0
+        var suggestResults = try await locator.suggest(
+            "Dunkin' Donuts"
+        )
+        var results = try XCTUnwrap(suggestResults)
+        XCTAssertEqual(results.count, 0)
+        
+        // Threshold of 1 -> requery with fewer restrictions
+        locator.repeatSuggestResultThreshold = 1
+        suggestResults = try await locator.suggest(
+            "Dunkin' Donuts"
+        )
+        results = try XCTUnwrap(suggestResults)
+        XCTAssertGreaterThanOrEqual(results.count, 1)
     }
 }
 
-extension Point {
-    static let paris = Point(x: 2.23522, y: 48.8566, spatialReference: .wgs84)
-    static let rome = Point(x: 12.4964, y: 41.9028, spatialReference: .wgs84)
-    static let london = Point(x: -0.1278, y: 51.5074, spatialReference: .wgs84)
+extension Envelope {
+    static let edinburgh = Envelope(
+        xMin: -365155.60783391213,
+        yMin: 7536778.456812576,
+        xMax: -347494.47622280417,
+        yMax: 7559866.706991681
+    )
 }
