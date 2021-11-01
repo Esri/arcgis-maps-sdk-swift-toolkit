11731ec092fcba74da4ab06c299814f2c2b543b2
Mark Dostal <mdostal@esri.com>
2021-11-01 15:31:23 -0500

    Update Tests/ArcGISToolkitTests/SearchViewModelTests.swift
    Co-authored-by: Philip Ridgeway <philip.ridgeway@gmail.com>

--- Tests/ArcGISToolkitTests/SearchViewModelTests.swift
+++ Tests/ArcGISToolkitTests/SearchViewModelTests.swift
@@ -11,12 +11,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import Foundation
-
 import XCTest
 import ArcGIS
 import ArcGISToolkit
-import SwiftUI
 import Combine
 
 @MainActor
