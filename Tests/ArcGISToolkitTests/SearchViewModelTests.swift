a6682430cd7ec7a236cca12857a027042bdb46c3
Mark Dostal <mhdostal@aol.com>
2021-11-03 16:22:00 -0500

    Use SearchOutcome instead of suggestions and results.

--- Tests/ArcGISToolkitTests/SearchViewModelTests.swift
+++ Tests/ArcGISToolkitTests/SearchViewModelTests.swift
@@ -28,17 +28,16 @@ class SearchViewModelTests: XCTestCase {
         model.updateSuggestions()
         
         // Get suggestion
-        let suggestions = try await model.$suggestions.compactMap({ $0 }).first
+        let suggestions = try await searchSuggestions(model)
         let suggestion = try XCTUnwrap(suggestions?.get().first)
         
         model.acceptSuggestion(suggestion)
         
-        let results = try await model.$results.compactMap({ $0 }).first
+        let results = try await searchResults(model, dropFirst: true)
         let result = try XCTUnwrap(results?.get())
         XCTAssertEqual(result.count, 1)
-        XCTAssertNil(model.suggestions)
         
-        // With only one results, model should set `selectedResult` property.
+        // With only one result, model should set `selectedResult` property.
         XCTAssertEqual(result.first, model.selectedResult)
     }
     
@@ -54,13 +53,13 @@ class SearchViewModelTests: XCTestCase {
         
         model.commitSearch()
         
-        let results = try await model.$results.compactMap({ $0 }).first
+        let results = try await searchResults(model)
         let result = try XCTUnwrap(results?.get().first)
         XCTAssertEqual(result.owningSource.name, activeSource.name)
         
         model.updateSuggestions()
         
-        let suggestions = try await model.$suggestions.compactMap({ $0 }).first
+        let suggestions = try await searchSuggestions(model, dropFirst: true)
         let suggestion = try XCTUnwrap(suggestions?.get().first)
         XCTAssertEqual(suggestion.owningSource.name, activeSource.name)
     }
@@ -68,50 +67,47 @@ class SearchViewModelTests: XCTestCase {
     func testCommitSearch() async throws {
         let model = SearchViewModel(sources: [LocatorSearchSource()])
         
-        // No search - results are nil.
-        XCTAssertNil(model.results)
+        // No search - searchOutcome is nil.
+        XCTAssertNil(model.searchOutcome)
         
         // Search with no results - result count is 0.
         model.currentQuery = "No results found blah blah blah blah"
         
         model.commitSearch()
         
-        var results = try await model.$results.compactMap({ $0 }).first
+        var results = try await searchResults(model)
         var result = try XCTUnwrap(results?.get())
-        XCTAssert(result.count, [])
+        XCTAssertEqual(result, [])
         XCTAssertNil(model.selectedResult)
-        XCTAssertNil(model.suggestions)
         
         // Search with one result.
         model.currentQuery = "Magers & Quinn Booksellers"
         
         model.commitSearch()
         
-        results = try await model.$results.compactMap({ $0 }).first
+        results = try await searchResults(model)
         result = try XCTUnwrap(results?.get())
         XCTAssertEqual(result.count, 1)
         
         // One results automatically populates `selectedResult`.
         XCTAssertEqual(result.first, model.selectedResult)
-        XCTAssertNil(model.suggestions)
         
         // Search with multiple results.
         model.currentQuery = "Magers & Quinn"
         
         model.commitSearch()
         
-        results = try await model.$results.compactMap({ $ 0}).first
+        results = try await searchResults(model)
         result = try XCTUnwrap(results?.get())
         XCTAssertGreaterThan(result.count, 1)
         
         XCTAssertNil(model.selectedResult)
-        XCTAssertNil(model.suggestions)
         
         model.selectedResult = result.first!
         
         model.commitSearch()
         
-        results = try await model.$results.compactMap({ $0 }).dropFirst().first
+        results = try await searchResults(model, dropFirst: true)
         result = try XCTUnwrap(results?.get())
         XCTAssertGreaterThan(result.count, 1)
         
@@ -121,31 +117,26 @@ class SearchViewModelTests: XCTestCase {
     func testCurrentQuery() async throws {
         let model = SearchViewModel(sources: [LocatorSearchSource()])
         
-        // Empty `currentQuery` should produce nil results and suggestions.
+        // Empty `currentQuery` should produce nil searchOutcome.
         model.currentQuery = ""
-        XCTAssertNil(model.results)
-        XCTAssertNil(model.suggestions)
+        XCTAssertNil(model.searchOutcome)
         
         // Valid `currentQuery` should produce non-nil results.
         model.currentQuery = "Coffee"
         
         model.commitSearch()
         
-        let results = try await model.$results.compactMap({ $0 }).first
+        let results = try await searchResults(model)
         XCTAssertNotNil(results)
         
-        // Changing the `currentQuery` should set results to nil.
+        // Changing the `currentQuery` should set searchOutcome to nil.
         model.currentQuery = "Coffee in Portland"
-        XCTAssertNil(model.results)
+        XCTAssertNil(model.searchOutcome)
         
         model.updateSuggestions()
         
-        let suggestions = try await model.$suggestions.compactMap({ $0 }).first
+        let suggestions = try await searchSuggestions(model)
         XCTAssertNotNil(suggestions)
-        
-        // Changing the `currentQuery` should set suggestions to nil.
-        model.currentQuery = "Coffee in Edinburgh"
-        XCTAssertNil(model.suggestions)
         
         // Changing current query after search with 1 result
         // should set `selectedResult` to nil
@@ -153,7 +144,7 @@ class SearchViewModelTests: XCTestCase {
         
         model.commitSearch()
         
-        _ = try await model.$results.compactMap({ $0 }).first
+        _ = try await searchResults(model, dropFirst: true)
         XCTAssertNotNil(model.selectedResult)
         model.currentQuery = "Hotel"
         XCTAssertNil(model.selectedResult)
@@ -169,7 +160,7 @@ class SearchViewModelTests: XCTestCase {
         
         model.commitSearch()
         
-        _ = try await model.$results.compactMap({ $0 }).first
+        _ = try await searchResults(model)
         XCTAssertFalse(model.isEligibleForRequery)
         
         // Offset extent by 10% - isEligibleForRequery should still be `false`.
@@ -196,7 +187,7 @@ class SearchViewModelTests: XCTestCase {
         
         model.commitSearch()
         
-        _ = try await model.$results.compactMap({ $0 }).dropFirst().first
+        _ = try await searchResults(model, dropFirst: true)
         XCTAssertFalse(model.isEligibleForRequery)
         
         // Expand extent by 1.1x - isEligibleForRequery should still be `false`.
@@ -227,7 +218,7 @@ class SearchViewModelTests: XCTestCase {
         
         model.commitSearch()
         
-        var results = try await model.$results.compactMap({ $0 }).first
+        var results = try await searchResults(model)
         var result = try XCTUnwrap(results?.get())
         XCTAssertGreaterThan(result.count, 1)
         
@@ -248,23 +239,23 @@ class SearchViewModelTests: XCTestCase {
         
         model.commitSearch()
         
-        results = try await model.$results.compactMap({ $0 }).first
+        results = try await searchResults(model)
         result = try XCTUnwrap(results?.get())
-        XCTAssertEqual(result.count, 0)
+        XCTAssertEqual(result, [])
         
         model.queryArea = Polygon.minneapolis
         
         model.commitSearch()
         
         // A note about the use of `.dropFirst()`:
-        // Because `model.results` is not changed between the previous call
+        // Because `model.searchOutcome` is not changed between the previous call
         // to `model.commitSearch()` and the one right above, the
-        // `try await model.$results...` call will return the last result
+        // `try await model.searchOutCome...` call will return the last result
         // received (from the first `model.commitSearch()` call), which is
         // incorrect.  Calling `.dropFirst()` will remove that one
         // and will give us the next one, which is the correct one (the result
         // from the second `model.commitSearch()` call).
-        results = try await model.$results.compactMap({ $0 }).dropFirst().first
+        results = try await searchResults(model, dropFirst: true)
         result = try XCTUnwrap(results?.get())
         XCTAssertEqual(result.count, 1)
     }
@@ -278,7 +269,7 @@ class SearchViewModelTests: XCTestCase {
         
         model.commitSearch()
         
-        var results = try await model.$results.compactMap({ $0 }).first
+        var results = try await searchResults(model)
         var result = try XCTUnwrap(results?.get())
         
         var resultPoint = try XCTUnwrap(
@@ -304,7 +295,7 @@ class SearchViewModelTests: XCTestCase {
         
         model.commitSearch()
         
-        results = try await model.$results.compactMap({ $0 }).first
+        results = try await searchResults(model)
         result = try XCTUnwrap(results?.get())
         
         resultPoint = try XCTUnwrap(
@@ -335,7 +326,7 @@ class SearchViewModelTests: XCTestCase {
         
         model.repeatSearch()
         
-        var results = try await model.$results.compactMap({ $0 }).first
+        var results = try await searchResults(model)
         var result = try XCTUnwrap(results?.get())
         XCTAssertGreaterThan(result.count, 1)
         
@@ -356,7 +347,7 @@ class SearchViewModelTests: XCTestCase {
         
         model.repeatSearch()
         
-        results = try await model.$results.compactMap({ $0 }).first
+        results = try await searchResults(model)
         result = try XCTUnwrap(results?.get())
         XCTAssertEqual(result.count, 0)
         
@@ -364,7 +355,7 @@ class SearchViewModelTests: XCTestCase {
         
         model.repeatSearch()
         
-        results = try await model.$results.compactMap({ $0 }).dropFirst().first
+        results = try await searchResults(model, dropFirst: true)
         result = try XCTUnwrap(results?.get())
         XCTAssertEqual(result.count, 1)
     }
@@ -378,7 +369,7 @@ class SearchViewModelTests: XCTestCase {
         
         model.commitSearch()
         
-        var results = try await model.$results.compactMap({ $0 }).first
+        var results = try await searchResults(model)
         var result = try XCTUnwrap(results?.get())
         XCTAssertEqual(result.count, 1)
         
@@ -386,7 +377,7 @@ class SearchViewModelTests: XCTestCase {
         
         model.commitSearch()
         
-        results = try await model.$results.compactMap({ $0 }).dropFirst().first
+        results = try await searchResults(model, dropFirst: true)
         result = try XCTUnwrap(results?.get())
         XCTAssertGreaterThan(result.count, 1)
         
@@ -394,23 +385,23 @@ class SearchViewModelTests: XCTestCase {
         
         model.updateSuggestions()
         
-        let suggestionResults = try await model.$suggestions.compactMap({ $0 }).first
+        let suggestionResults = try await searchSuggestions(model)
         let suggestions = try XCTUnwrap(suggestionResults?.get())
         
         let collectionSuggestion = try XCTUnwrap(suggestions.filter(\.isCollection).first)
-        let singleSuggestion = try XCTUnwrap(suggestions.filter(!\.isCollection).first)
+        let singleSuggestion = try XCTUnwrap(suggestions.filter { !$0.isCollection }.first)
         
         model.resultMode = .automatic
         
         model.acceptSuggestion(collectionSuggestion)
         
-        results = try await model.$results.compactMap({ $0 }).first
+        results = try await searchResults(model, dropFirst: true)
         result = try XCTUnwrap(results?.get())
         XCTAssertGreaterThan(result.count, 1)
         
-        Task { model.acceptSuggestion(singleSuggestion) }
+        model.acceptSuggestion(singleSuggestion)
         
-        results = try await model.$results.compactMap({ $0 }).dropFirst().first
+        results = try await searchResults(model)
         result = try XCTUnwrap(results?.get())
         XCTAssertEqual(result.count, 1)
     }
@@ -419,14 +410,14 @@ class SearchViewModelTests: XCTestCase {
         let model = SearchViewModel(sources: [LocatorSearchSource()])
         
         // No currentQuery - suggestions are nil.
-        XCTAssertNil(model.suggestions)
+        XCTAssertNil(model.searchOutcome)
         
         // UpdateSuggestions with no results - result count is 0.
         model.currentQuery = "No results found blah blah blah blah"
         
         model.updateSuggestions()
         
-        var suggestionResults = try await model.$suggestions.compactMap({ $0 }).first
+        var suggestionResults = try await searchSuggestions(model)
         var suggestions = try XCTUnwrap(suggestionResults?.get())
         XCTAssertEqual(suggestions, [])
         
@@ -435,12 +426,45 @@ class SearchViewModelTests: XCTestCase {
         
         model.updateSuggestions()
         
-        suggestionResults = try await model.$suggestions.compactMap({ $0 }).first
+        suggestionResults = try await searchSuggestions(model, dropFirst: true)
         suggestions = try XCTUnwrap(suggestionResults?.get())
         XCTAssertGreaterThanOrEqual(suggestions.count, 1)
         
         XCTAssertNil(model.selectedResult)
-        XCTAssertNil(model.results)
+    }
+}
+
+extension SearchViewModelTests {
+    func searchResults(
+        _ model: SearchViewModel,
+        dropFirst: Bool = false
+    ) async throws -> Result<[SearchResult], SearchError>? {
+        let searchOutcome = dropFirst ?
+        try await model.$searchOutcome.compactMap({ $0 }).dropFirst().first :
+        try await model.$searchOutcome.compactMap({ $0 }).first
+        
+        switch searchOutcome {
+        case .results(let results):
+            return results
+        default:
+            return nil
+        }
+    }
+    
+    func searchSuggestions(
+        _ model: SearchViewModel,
+        dropFirst: Bool = false
+    ) async throws -> Result<[SearchSuggestion], SearchError>? {
+        let searchOutcome = dropFirst ?
+        try await model.$searchOutcome.compactMap({ $0 }).dropFirst().first :
+        try await model.$searchOutcome.compactMap({ $0 }).first
+        
+        switch searchOutcome {
+        case .suggestions(let suggestions):
+            return suggestions
+        default:
+            return nil
+        }
     }
 }
 
