ea32623611936b5fce28e0e324167af0931aa317
Mark Dostal <mdostal@esri.com>
2021-11-15 14:55:54 -0600

    Update Sources/ArcGISToolkit/Components/Search/SearchView.swift
    Co-authored-by: Philip Ridgeway <philip.ridgeway@gmail.com>

--- Sources/ArcGISToolkit/Components/Search/SearchView.swift
+++ Sources/ArcGISToolkit/Components/Search/SearchView.swift
@@ -27,14 +27,9 @@ public struct SearchView: View {
         viewpoint: Binding<Viewpoint?>? = nil,
         resultsOverlay: GraphicsOverlay? = nil
     ) {
-        if let searchViewModel = searchViewModel {
-            self.searchViewModel = searchViewModel
-        }
-        else {
-            self.searchViewModel = SearchViewModel(
-                sources: [LocatorSearchSource()]
-            )
-        }
+        self.searchViewModel = searchViewModel ?? SearchViewModel(
+            sources: [LocatorSearchSource()]
+        )
         self.resultsOverlay = resultsOverlay
         self.viewpoint = viewpoint
     }
