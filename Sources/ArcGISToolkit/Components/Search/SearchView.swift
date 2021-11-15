1dcacba65d65448972d3821e8be42929f460ce67
Mark Dostal <mdostal@esri.com>
2021-11-15 15:04:08 -0600

    Update Sources/ArcGISToolkit/Components/Search/SearchView.swift
    Co-authored-by: Philip Ridgeway <philip.ridgeway@gmail.com>

--- Sources/ArcGISToolkit/Components/Search/SearchView.swift
+++ Sources/ArcGISToolkit/Components/Search/SearchView.swift
@@ -131,7 +131,7 @@ public struct SearchView: View {
     
     // MARK: Modifiers
     
-    /// Determines whether a built-in result view will be shown. If `false`, the result display/selection
+    /// Specifies whether a built-in result view will be shown. If `false`, the result display/selection
     /// list is not shown. Set to `false` if you want to define a custom result list. You might use a
     /// custom result list to show results in a separate list, disconnected from the rest of the search view.
     /// Defaults to `true`.
