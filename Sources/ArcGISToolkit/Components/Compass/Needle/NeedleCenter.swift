44dd4871eadaab8880bde21e551be16ead32a535
David Feinzimer <dfeinzimer@esri.com>
2022-02-07 17:34:59 -0600

    Documentation fix

--- Sources/ArcGISToolkit/Components/Compass/Needle/NeedleCenter.swift
+++ Sources/ArcGISToolkit/Components/Compass/Needle/NeedleCenter.swift
@@ -15,7 +15,7 @@
 
 ***REMOVED***/ Represents the center of the spinning needle at the center of the compass.
 struct NeedleCenter: View {
-***REMOVED******REMOVED***/ The bronze color component of the compass needle.
+***REMOVED******REMOVED***/ The bronze color of the center of the compass needle.
 ***REMOVED***private let color = Color(red: 241, green: 169, blue: 59)
 
 ***REMOVED***var body: some View {
