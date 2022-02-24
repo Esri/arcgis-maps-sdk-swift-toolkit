e5cb1cb27df3422114e1d82ace4ec2f87bf1ccf9
David Feinzimer <dfeinzimer@esri.com>
2022-02-24 10:47:04 -0800

    Split `Bookmarks` init into two

--- Sources/ArcGISToolkit/Components/Bookmarks/Bookmarks.swift
+++ Sources/ArcGISToolkit/Components/Bookmarks/Bookmarks.swift
@@ -33,29 +33,42 @@ public struct Bookmarks: View {
     }
 
     /// Creates a `Bookmarks` component.
-    /// - precondition: `bookmarks` or `map` is non-nil.
     /// - precondition: `viewpoint` is non-nil or the `selectionChangedActions` modifier is
     /// implemented.
     /// - Parameters:
     ///   - isPresented: Determines if the bookmarks list is presented.
     ///   - bookmarks: A list of bookmarks. Use this when displaying bookmarks defined at run-time.
-    ///   Use either `bookmarks` or `map` exclusively.
-    ///   - map: A web map authored with pre-existing bookmarks. Use either `map` or `bookmarks`
-    ///   exclusively.
     ///   - viewpoint: A viewpoint binding that will be updated when a bookmark is selected. Use
     ///   either `viewpoint` or `selectionChangedActions` exclusively.
     public init(
         isPresented: Binding<Bool>,
-        bookmarks: [Bookmark]? = nil,
-        map: Map? = nil,
+        bookmarks: [Bookmark],
         viewpoint: Binding<Viewpoint?>? = nil
     ) {
-        precondition((bookmarks != nil) || (map != nil))
         bookmarksList = BookmarksList(
             bookmarks: bookmarks,
             isPresented: isPresented,
+            viewpoint: viewpoint
+        )
+        _isPresented = isPresented
+    }
+
+    /// Creates a `Bookmarks` component.
+    /// - precondition: `viewpoint` is non-nil or the `selectionChangedActions` modifier is
+    /// implemented.
+    /// - Parameters:
+    ///   - isPresented: Determines if the bookmarks list is presented.
+    ///   - map: A web map authored with pre-existing bookmarks.
+    ///   - viewpoint: A viewpoint binding that will be updated when a bookmark is selected. Use
+    ///   either `viewpoint` or `selectionChangedActions` exclusively.
+    public init(
+        isPresented: Binding<Bool>,
+        map: Map,
+        viewpoint: Binding<Viewpoint?>? = nil
+    ) {
+        bookmarksList = BookmarksList(
+            isPresented: isPresented,
             map: map,
-            selectionChangedActions: selectionChangedActions,
             viewpoint: viewpoint
         )
         _isPresented = isPresented
