4a81710dceb4df8e91ea31d10e11f4b7c23a397d
David Feinzimer <dfeinzimer@esri.com>
2022-03-04 08:38:09 -0800

    Match commented line spacing

--- Examples/Examples/FloorFilterExampleView.swift
+++ Examples/Examples/FloorFilterExampleView.swift
@@ -31,8 +31,8 @@ struct FloorFilterExampleView: View {
 ***REMOVED******REMOVED******REMOVED*** Create the map from a portal item and assign to the mapView.
 ***REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED*** Multiple sites/facilities: Esri IST map with all buildings.
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***let portal = Portal(url: URL(string: "https:***REMOVED***indoors.maps.arcgis.com/")!, isLoginRequired: false)
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***let portalItem = PortalItem(portal: portal, id: Item.ID(rawValue: "49520a67773842f1858602735ef538b5")!)
+***REMOVED******REMOVED******REMOVED***let portal = Portal(url: URL(string: "https:***REMOVED***indoors.maps.arcgis.com/")!, isLoginRequired: false)
+***REMOVED******REMOVED******REMOVED***let portalItem = PortalItem(portal: portal, id: Item.ID(rawValue: "49520a67773842f1858602735ef538b5")!)
 ***REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED*** Redlands Campus map.
 ***REMOVED******REMOVED***let portal = Portal(url: URL(string: "https:***REMOVED***runtimecoretest.maps.arcgis.com/")!, isLoginRequired: false)
