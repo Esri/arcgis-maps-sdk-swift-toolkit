dc027f4214b473abdc01e0345ba8c54ad8b0d6bc
David Feinzimer <dfeinzimer@esri.com>
2022-03-16 11:47:43 -0700

    Update FloorFilterExampleView.swift
    Match FF II

--- Examples/Examples/FloorFilterExampleView.swift
+++ Examples/Examples/FloorFilterExampleView.swift
@@ -16,26 +16,12 @@
 ***REMOVED***
 
 struct FloorFilterExampleView: View {
-***REMOVED***private let map: Map
-***REMOVED***
-***REMOVED***@State
-***REMOVED***private var viewpoint = Viewpoint(
-***REMOVED******REMOVED***center: Point(x: -117.19496, y: 34.05713, spatialReference: .wgs84),
-***REMOVED******REMOVED***scale: 10_000
-***REMOVED***)
-***REMOVED***
-***REMOVED***@State
-***REMOVED***private var isMapLoaded: Bool = false
-
-***REMOVED***private let filterAlignment = Alignment.bottomLeading
-
-***REMOVED***init() {
-***REMOVED******REMOVED******REMOVED*** Create the map from a portal item and assign to the mapView.
-***REMOVED******REMOVED***
+***REMOVED******REMOVED***/ Make a map from a portal item.
+***REMOVED***static func makeMap() -> Map {
 ***REMOVED******REMOVED******REMOVED*** Multiple sites/facilities: Esri IST map with all buildings.
 ***REMOVED******REMOVED******REMOVED***let portal = Portal(url: URL(string: "https:***REMOVED***indoors.maps.arcgis.com/")!, isLoginRequired: false)
 ***REMOVED******REMOVED******REMOVED***let portalItem = PortalItem(portal: portal, id: Item.ID(rawValue: "49520a67773842f1858602735ef538b5")!)
-***REMOVED******REMOVED***
+
 ***REMOVED******REMOVED******REMOVED*** Redlands Campus map.
 ***REMOVED******REMOVED***let portal = Portal(url: URL(string: "https:***REMOVED***runtimecoretest.maps.arcgis.com/")!, isLoginRequired: false)
 ***REMOVED******REMOVED***let portalItem = PortalItem(portal: portal, id: Item.ID(rawValue: "7687805bd42549f5ba41237443d0c60a")!) ***REMOVED***<= another multiple sites/facilities
@@ -44,9 +30,22 @@ struct FloorFilterExampleView: View {
 ***REMOVED******REMOVED******REMOVED***let portal = Portal(url: URL(string: "https:***REMOVED***indoors.maps.arcgis.com/")!, isLoginRequired: false)
 ***REMOVED******REMOVED******REMOVED***let portalItem = PortalItem(portal: portal, id: Item.ID(rawValue: "f133a698536f44c8884ad81f80b6cfc7")!)
 
-***REMOVED******REMOVED***map = Map(item: portalItem)
+***REMOVED******REMOVED***return Map(item: portalItem)
 ***REMOVED***
-***REMOVED***
+
+***REMOVED***private let filterAlignment = Alignment.bottomLeading
+
+***REMOVED***@State
+***REMOVED***private var isMapLoaded: Bool = false
+
+***REMOVED***private var map = makeMap()
+
+***REMOVED***@State
+***REMOVED***private var viewpoint = Viewpoint(
+***REMOVED******REMOVED***center: Point(x: -117.19496, y: 34.05713, spatialReference: .wgs84),
+***REMOVED******REMOVED***scale: 100_000
+***REMOVED***)
+
 ***REMOVED***var body: some View {
 ***REMOVED******REMOVED***MapView(
 ***REMOVED******REMOVED******REMOVED***map: map,
@@ -63,8 +62,8 @@ struct FloorFilterExampleView: View {
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***floorManager: floorManager,
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***viewpoint: $viewpoint
 ***REMOVED******REMOVED******REMOVED******REMOVED***)
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.frame(maxWidth: 300, maxHeight: 300)
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.padding(36)
+***REMOVED******REMOVED******REMOVED******REMOVED***.frame(maxWidth: 300, maxHeight: 300)
+***REMOVED******REMOVED******REMOVED******REMOVED***.padding(36)
 ***REMOVED******REMOVED***
 ***REMOVED***
 ***REMOVED******REMOVED***.task {
