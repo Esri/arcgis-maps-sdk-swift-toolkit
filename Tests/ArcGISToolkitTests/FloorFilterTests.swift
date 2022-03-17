7e2cb5f5f23eccb9d27cb179d8f73d3ef5833531
David Feinzimer <dfeinzimer@esri.com>
2022-03-16 17:57:23 -0700

    Update FloorFilterTests.swift

--- Tests/ArcGISToolkitTests/FloorFilterTests.swift
+++ Tests/ArcGISToolkitTests/FloorFilterTests.swift
@@ -12,22 +12,45 @@
 ***REMOVED*** limitations under the License.
 
 ***REMOVED***
+import Combine
 ***REMOVED***
 import XCTest
 @testable ***REMOVED***Toolkit
 
 class FloorFilterTests: XCTestCase {
+***REMOVED******REMOVED***/ Applies credentials necessary to run tests.
+***REMOVED***override func setUp() async throws {
+***REMOVED******REMOVED***await addCredentials()
+***REMOVED***
+
+***REMOVED***private var cancellables = Set<AnyCancellable>()
+
 ***REMOVED******REMOVED***/ Tests general behavior of `FloorFilterViewModel`.
 ***REMOVED***func testFloorFilterViewModel() async {
-***REMOVED******REMOVED***let map = FloorFilterTests.makeMap()
-***REMOVED******REMOVED******REMOVED*** FIXME: Credentials required
-***REMOVED******REMOVED***try? await map.load()
-***REMOVED******REMOVED***print(map.loadStatus)
+***REMOVED******REMOVED***guard let map = await makeMap() else {
+***REMOVED******REMOVED******REMOVED***return
+***REMOVED***
+***REMOVED******REMOVED***guard let floorManager = map.floorManager else {
+***REMOVED******REMOVED******REMOVED***XCTFail("No FloorManager available")
+***REMOVED******REMOVED******REMOVED***return
+***REMOVED***
+***REMOVED******REMOVED***let viewModel = await FloorFilterViewModel(floorManager: floorManager)
+***REMOVED******REMOVED***let expectation = XCTestExpectation(
+***REMOVED******REMOVED******REMOVED***description: "View model successfully initialized"
+***REMOVED******REMOVED***)
+***REMOVED******REMOVED***await viewModel.$isLoading
+***REMOVED******REMOVED******REMOVED***.sink { loading in
+***REMOVED******REMOVED******REMOVED******REMOVED***if !loading {
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***expectation.fulfill()
+***REMOVED******REMOVED******REMOVED***
+***REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED***.store(in: &cancellables)
+***REMOVED******REMOVED***wait(for: [expectation], timeout: 10.0)
 ***REMOVED***
 
 ***REMOVED******REMOVED***/ Get a map constructed from an ArcGIS portal item.
 ***REMOVED******REMOVED***/ - Returns: A map constructed from an ArcGIS portal item.
-***REMOVED***static func makeMap() -> Map {
+***REMOVED***private func makeMap() async -> Map? {
 ***REMOVED******REMOVED******REMOVED*** Multiple sites/facilities: Esri IST map with all buildings.
 ***REMOVED******REMOVED******REMOVED***let portal = Portal(url: URL(string: "https:***REMOVED***indoors.maps.arcgis.com/")!, isLoginRequired: false)
 ***REMOVED******REMOVED******REMOVED***let portalItem = PortalItem(portal: portal, id: Item.ID(rawValue: "49520a67773842f1858602735ef538b5")!)
@@ -40,6 +63,17 @@ class FloorFilterTests: XCTestCase {
 ***REMOVED******REMOVED******REMOVED***let portal = Portal(url: URL(string: "https:***REMOVED***indoors.maps.arcgis.com/")!, isLoginRequired: false)
 ***REMOVED******REMOVED******REMOVED***let portalItem = PortalItem(portal: portal, id: Item.ID(rawValue: "f133a698536f44c8884ad81f80b6cfc7")!)
 
-***REMOVED******REMOVED***return Map(item: portalItem)
+***REMOVED******REMOVED***let map = Map(item: portalItem)
+***REMOVED******REMOVED***do {
+***REMOVED******REMOVED******REMOVED***try await map.load()
+***REMOVED*** catch {
+***REMOVED******REMOVED******REMOVED***XCTFail("\(#fileID), \(#function), \(#line), \(error.localizedDescription)")
+***REMOVED******REMOVED******REMOVED***return nil
+***REMOVED***
+***REMOVED******REMOVED***guard map.loadStatus == .loaded else {
+***REMOVED******REMOVED******REMOVED***XCTFail("\(#fileID), \(#function), \(#line), Map not loaded")
+***REMOVED******REMOVED******REMOVED***return nil
+***REMOVED***
+***REMOVED******REMOVED***return map
 ***REMOVED***
 ***REMOVED***
