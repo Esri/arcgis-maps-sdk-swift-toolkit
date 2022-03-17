1faaaf5539dc9d051386400b58cd7002c4b06ece
David Feinzimer <dfeinzimer@esri.com>
2022-03-16 17:57:23 -0700

    Update FloorFilterTests.swift

--- Tests/ArcGISToolkitTests/FloorFilterTests.swift
+++ Tests/ArcGISToolkitTests/FloorFilterTests.swift
@@ -12,22 +12,45 @@
 // limitations under the License.
 
 import ArcGIS
+import Combine
 import SwiftUI
 import XCTest
 @testable import ArcGISToolkit
 
 class FloorFilterTests: XCTestCase {
+    /// Applies credentials necessary to run tests.
+    override func setUp() async throws {
+        await addCredentials()
+    }
+
+    private var cancellables = Set<AnyCancellable>()
+
     /// Tests general behavior of `FloorFilterViewModel`.
     func testFloorFilterViewModel() async {
-        let map = FloorFilterTests.makeMap()
-        // FIXME: Credentials required
-        try? await map.load()
-        print(map.loadStatus)
+        guard let map = await makeMap() else {
+            return
+        }
+        guard let floorManager = map.floorManager else {
+            XCTFail("No FloorManager available")
+            return
+        }
+        let viewModel = await FloorFilterViewModel(floorManager: floorManager)
+        let expectation = XCTestExpectation(
+            description: "View model successfully initialized"
+        )
+        await viewModel.$isLoading
+            .sink { loading in
+                if !loading {
+                    expectation.fulfill()
+                }
+            }
+            .store(in: &cancellables)
+        wait(for: [expectation], timeout: 10.0)
     }
 
     /// Get a map constructed from an ArcGIS portal item.
     /// - Returns: A map constructed from an ArcGIS portal item.
-    static func makeMap() -> Map {
+    private func makeMap() async -> Map? {
         // Multiple sites/facilities: Esri IST map with all buildings.
 //        let portal = Portal(url: URL(string: "https://indoors.maps.arcgis.com/")!, isLoginRequired: false)
 //        let portalItem = PortalItem(portal: portal, id: Item.ID(rawValue: "49520a67773842f1858602735ef538b5")!)
@@ -40,6 +63,17 @@ class FloorFilterTests: XCTestCase {
 //        let portal = Portal(url: URL(string: "https://indoors.maps.arcgis.com/")!, isLoginRequired: false)
 //        let portalItem = PortalItem(portal: portal, id: Item.ID(rawValue: "f133a698536f44c8884ad81f80b6cfc7")!)
 
-        return Map(item: portalItem)
+        let map = Map(item: portalItem)
+        do {
+            try await map.load()
+        } catch {
+            XCTFail("\(#fileID), \(#function), \(#line), \(error.localizedDescription)")
+            return nil
+        }
+        guard map.loadStatus == .loaded else {
+            XCTFail("\(#fileID), \(#function), \(#line), Map not loaded")
+            return nil
+        }
+        return map
     }
 }
