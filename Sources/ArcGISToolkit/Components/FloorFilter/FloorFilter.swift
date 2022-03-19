8943b38d17d40ac45843a265f5fb70367f09b412
David Feinzimer <dfeinzimer@esri.com>
2022-03-19 12:44:53 -0700

    Update FloorFilter.swift

--- Sources/ArcGISToolkit/Components/FloorFilter/FloorFilter.swift
+++ Sources/ArcGISToolkit/Components/FloorFilter/FloorFilter.swift
@@ -39,6 +39,14 @@ public struct FloorFilter: View {
         self.viewpoint = viewpoint
     }
 
+    /// A Boolean value that indicates whether the levels view is currently collapsed.
+    @State
+    private var isLevelsViewCollapsed: Bool = false
+
+    /// A Boolean value that indicates whether the site/facility selector is hidden.
+    @State
+    private var isSelectorHidden: Bool = true
+
     /// The alignment configuration.
     private let alignment: Alignment
 
@@ -49,14 +57,6 @@ public struct FloorFilter: View {
           viewModel.selectedFacility!.levels.isEmpty)
     }
 
-    /// A Boolean value that indicates whether the site/facility selector is hidden.
-    @State
-    private var isSelectorHidden: Bool = true
-
-    /// A Boolean value that indicates whether the levels view is currently collapsed.
-    @State
-    private var isLevelsViewCollapsed: Bool = false
-
     /// Displays the available levels.
     private var levelsAndDividerView: some View {
         Group {
@@ -65,7 +65,7 @@ public struct FloorFilter: View {
                     Divider()
                         .frame(width: 30)
                 }
-                LevelsView(
+                LevelSelector(
                     topAligned: topAligned,
                     levels: sortedLevels,
                     isCollapsed: $isLevelsViewCollapsed
@@ -181,124 +181,5 @@ public struct FloorFilter: View {
             // Ensure space for filter text field on small screens in landscape
             .frame(minHeight: 100)
             .environmentObject(viewModel)
-    }
-}
-
-/// A view displaying the levels in the selected facility.
-struct LevelsView: View {
-    /// The alignment configuration.
-    var topAligned: Bool
-
-    /// The levels to display.
-    let levels: [FloorLevel]
-
-    /// A Boolean value indicating the whether the view shows only the selected level or all levels.
-    /// If the value is`false`, the view will display all levels; if it is `true`, the view will only display
-    /// the selected level.
-    @Binding
-    var isCollapsed: Bool
-
-    /// The view model used by the `LevelsView`.
-    @EnvironmentObject var viewModel: FloorFilterViewModel
-
-    /// The height of the scroll view's content.
-    @State
-    private var scrollViewContentHeight: CGFloat = .zero
-
-    /// Returns the short name of the currently selected level, the first level or "None" if none of the listed
-    /// are available.
-    private var selectedLevelName: String {
-        if let shortName = viewModel.selectedLevel?.shortName {
-            return shortName
-        } else if let firstLevelShortName = levels.first?.shortName {
-            return firstLevelShortName
-        } else {
-            return "None"
-        }
-    }
-
-    public var body: some View {
-        VStack {
-            if !isCollapsed,
-                levels.count > 1 {
-                if !topAligned {
-                    CollapseButton(isCollapsed: $isCollapsed)
-                    Divider()
-                        .frame(width: 30)
-                }
-                ScrollView {
-                    LevelsStack(levels: levels)
-                        .background(
-                            GeometryReader { geometry -> Color in
-                                DispatchQueue.main.async {
-                                    scrollViewContentHeight = geometry.size.height
-                                }
-                                return .clear
-                            }
-                        )
-                }
-                    .frame(maxHeight: scrollViewContentHeight)
-                if topAligned {
-                    Divider()
-                        .frame(width: 30)
-                    CollapseButton(isCollapsed: $isCollapsed)
-                }
-            } else {
-                Button {
-                    if levels.count > 1 {
-                        isCollapsed.toggle()
-                    }
-                } label: {
-                    Text(selectedLevelName)
-                        .lineLimit(1)
-                }
-                    .selected(true)
-            }
-        }
-    }
-}
-
-/// A vertical list of floor levels.
-struct LevelsStack: View {
-    let levels: [FloorLevel]
-
-    /// The view model used by the `LevelsView`.
-    @EnvironmentObject var viewModel: FloorFilterViewModel
-
-    var body: some View {
-        VStack {
-            ForEach(levels) { level in
-                Button {
-                    viewModel.setLevel(level)
-                } label: {
-                    Text(level.shortName)
-                        .lineLimit(1)
-                }
-                    .selected(level == viewModel.selectedLevel)
-            }
-        }
-    }
-}
-
-/// A button used to collapse the floor level list.
-struct CollapseButton: View {
-    /// Allows the user to toggle the visibility of the site and facility selector.
-    @Binding
-    var isCollapsed: Bool
-
-    var body: some View {
-        Button {
-            withAnimation {
-                isCollapsed.toggle()
-            }
-        } label: {
-            Image(systemName: "xmark")
-        }
-            .padding(EdgeInsets(
-                top: 2,
-                leading: 4,
-                bottom: 2,
-                trailing: 4
-            ))
     }
 }
