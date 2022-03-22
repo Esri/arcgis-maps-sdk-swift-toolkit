b2b9868b6aa05397300f19ca6a4d1591eea80fc8
David Feinzimer <dfeinzimer@esri.com>
2022-03-22 11:34:35 -0700

    Update Result.swift
    Undo unintended cahnge

--- Sources/ArcGISToolkit/Extensions/Result.swift
+++ Sources/ArcGISToolkit/Extensions/Result.swift
@@ -12,6 +12,11 @@
 // limitations under the License.
 
 extension Result where Failure == Error {
+    /// Creates a result based on the outcome of the given task. If the task
+    /// succeeds, the result is `success`. If the task fails, the result is
+    /// `failure`.
+    ///
+    /// Returns `nil` in the event that the task was cancelled.
     init?(awaiting task: () async throws -> Success) async {
         do {
             self = .success(try await task())
