14d30a584150d30bae74e6b78126d86aeabd4870
David Feinzimer <dfeinzimer@gmail.com>
2022-03-22 09:33:57 -0700

    Update Examples/Examples/BookmarksExampleView.swift
    Co-authored-by: Mark Dostal <mdostal@esri.com>

--- Examples/Examples/BookmarksExampleView.swift
+++ Examples/Examples/BookmarksExampleView.swift
@@ -23,7 +23,7 @@ struct BookmarksExampleView: View {
 ***REMOVED***private var verticalSizeClass: UserInterfaceSizeClass?
 
 ***REMOVED******REMOVED***/ A web map with predefined bookmarks.
-***REMOVED***private let map = Map(url: URL(string: "https:***REMOVED***runtime.maps.arcgis.com/home/item.html?id=16f1b8ba37b44dc3884afc8d5f454dd2")!)!
+***REMOVED***private let map = Map(url: URL(string: "https:***REMOVED***www.arcgis.com/home/item.html?id=16f1b8ba37b44dc3884afc8d5f454dd2")!)!
 
 ***REMOVED******REMOVED***/ Indicates if the bookmarks list is shown or not.
 ***REMOVED******REMOVED***/ - Remark: This allows a developer to control how the bookmarks menu is shown/hidden,
