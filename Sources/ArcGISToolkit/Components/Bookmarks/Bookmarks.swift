dfa54b1732caf0543f8f453181a70cea6655fcfe
David Feinzimer <dfeinzimer@gmail.com>
2022-03-22 18:02:53 -0700

    Update Sources/ArcGISToolkit/Components/Bookmarks/Bookmarks.swift
    Co-authored-by: Zachary A Kline <zkline@esri.com>

--- Sources/ArcGISToolkit/Components/Bookmarks/Bookmarks.swift
+++ Sources/ArcGISToolkit/Components/Bookmarks/Bookmarks.swift
@@ -89,7 +89,7 @@ public struct Bookmarks: View {
 ***REMOVED******REMOVED***/   - isPresented: Determines if the bookmarks list is presented.
 ***REMOVED******REMOVED***/   - mapOrScene: A `GeoModel` authored with pre-existing bookmarks.
 ***REMOVED******REMOVED***/   - viewpoint: A viewpoint binding that will be updated when a bookmark is selected.
-***REMOVED******REMOVED***/   Alternately, you can use the `.onSelectionChanged` modifier to handle bookmark selection.
+***REMOVED******REMOVED***/   Alternately, you can use the `onSelectionChanged(perform:)` modifier to handle bookmark selection.
 ***REMOVED***public init(
 ***REMOVED******REMOVED***isPresented: Binding<Bool>,
 ***REMOVED******REMOVED***mapOrScene: GeoModel,
