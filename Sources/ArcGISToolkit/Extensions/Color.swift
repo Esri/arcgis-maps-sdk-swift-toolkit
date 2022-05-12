e4d5422c342fac3a8754a846d6de04b7bc13b29c
David Feinzimer <dfeinzimer@gmail.com>
2022-05-12 13:33:12 -0700

    Update Color.swift

--- Sources/ArcGISToolkit/Extensions/Color.swift
+++ Sources/ArcGISToolkit/Extensions/Color.swift
@@ -15,7 +15,7 @@ import SwiftUI
 
 extension Color {
     /// Initializes a new color with RGB integer values.
-    /// - Precondition: `red`, `blue` and `green` are values between 0 and 255 inclusive.
+    /// - Precondition: `red`, `green` and `blue` are values between 0 and 255 inclusive.
     init(red: Int, green: Int, blue: Int) {
         let validRange = 0...255
         precondition(validRange.contains(red))
