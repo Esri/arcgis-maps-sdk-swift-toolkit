693a32914ba65533befa80339a8bed9e4c9606db
Ryan Olson <rolson@esri.com>
2022-05-23 20:54:57 -0600

    hack fix for multiple challenges

--- Sources/ArcGISToolkit/Components/Authentication/Authenticator.swift
+++ Sources/ArcGISToolkit/Components/Authentication/Authenticator.swift
@@ -68,6 +68,9 @@ public final class Authenticator: ObservableObject {
     
     private func observeChallengeQueue() async {
         for await queuedChallenge in challengeQueue {
+            // Give chance to stop presenting last view for the case of queued up challenges.
+            try? await Task.sleep(nanoseconds: 250 * 1_000_000)
+            
             if let queuedArcGISChallenge = queuedChallenge as? QueuedArcGISChallenge,
                let url = queuedArcGISChallenge.arcGISChallenge.request.url,
                let config = oAuthConfigurations.first(where: { $0.canBeUsed(for: url) }) {
