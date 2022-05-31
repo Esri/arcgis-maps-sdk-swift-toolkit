2cef99ca56151b5494827df23efc543173004f00
David Feinzimer <dfeinzimer@esri.com>
2022-05-31 13:33:49 -0700

    Update FloorFilterViewModelTests.swift

--- Tests/ArcGISToolkitTests/FloorFilterViewModelTests.swift
+++ Tests/ArcGISToolkitTests/FloorFilterViewModelTests.swift
@@ -20,7 +20,7 @@ import XCTest
 @MainActor
 final class FloorFilterViewModelTests: XCTestCase {
     /// Tests that a `FloorFilterViewModel` succesfully initializes with a `FloorManager`.`
-    func testInitWithFloorManager() async {
+    func testInitWithFloorManagerAndViewpoint() async {
         guard let map = await makeMap(),
               let floorManager = map.floorManager else {
             return
