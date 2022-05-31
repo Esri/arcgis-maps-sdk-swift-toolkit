975b315e0ed69407b9b2ab504e94bc1b25734bad
David Feinzimer <dfeinzimer@esri.com>
2022-05-31 12:07:13 -0700

    Update View.swift

--- Sources/ArcGISToolkit/Extensions/View.swift
+++ Sources/ArcGISToolkit/Extensions/View.swift
@@ -17,7 +17,7 @@ import SwiftUI
 struct SelectedModifier: ViewModifier {
     /// A Boolean value that indicates whether view should display as selected.
     var isSelected: Bool
-    
+
     func body(content: Content) -> some View {
         if isSelected {
             content
@@ -37,7 +37,7 @@ extension View {
     /// Returns a new `View` that allows a parent `View` to be informed of a child view's size.
     /// - Parameter perform: The closure to be executed when the content size of the receiver
     /// changes.
-    /// - Returns: A new `View`.
+    /// - Returns: The modified view.
     func onSizeChange(perform: @escaping (CGSize) -> Void) -> some View {
         background(
             GeometryReader { geometry in
