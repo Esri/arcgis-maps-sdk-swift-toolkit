29d5513a086012cd49009f2dbc10c9ed4e2c3821
David Feinzimer <dfeinzimer@esri.com>
2022-06-20 17:11:31 -0700

    Add Utility Network Trace Tool

---
+++ Examples/Examples/UtilityNetworkTraceExampleView.swift
@@ -0,0 +1,78 @@
+// Copyright 2022 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import ArcGIS
+import ArcGISToolkit
+import SwiftUI
+
+struct UtilityNetworkTraceExampleView: View {
+    @StateObject private var map = makeMap()
+    
+    /// Provides the ability to inspect map components.
+    @State var mapViewProxy: MapViewProxy?
+    
+    /// Provides the ability to detect tap locations in the context of the map view.
+    @State var pointInMap: Point?
+    
+    /// Provides the ability to detect tap locations in the context of the screen.
+    @State var pointInScreen: CGPoint?
+    
+    /// A container for graphical trace results.
+    @State var resultGraphicsOverlay = GraphicsOverlay()
+    
+    @State var viewpoint: Viewpoint?
+    
+    var body: some View {
+        MapViewReader { mapViewProxy in
+            MapView(
+                map: map,
+                viewpoint: viewpoint,
+                graphicsOverlays: [resultGraphicsOverlay]
+            )
+            .onSingleTapGesture { pointInScreen, pointInMap in
+                self.pointInScreen = pointInScreen
+                self.pointInMap = pointInMap
+                self.mapViewProxy = mapViewProxy
+            }
+            .edgesIgnoringSafeArea(.bottom)
+            .overlay {
+                UtilityNetworkTrace(
+                    $resultGraphicsOverlay,
+                    map,
+                    $pointInMap,
+                    $pointInScreen,
+                    $mapViewProxy,
+                    $viewpoint
+                )
+                .frame(
+                    maxWidth: .infinity,
+                    maxHeight: .infinity,
+                    alignment: .bottomLeading
+                )
+            }
+        }
+    }
+    
+    /// Make a map from a portal item.
+    static func makeMap() -> Map {
+        let portal = Portal(
+            url: URL(string: "https://www.arcgis.com/")!,
+            isLoginRequired: false
+        )
+        let portalItem = PortalItem(
+            portal: portal,
+            id: Item.ID(rawValue: "471eb0bf37074b1fbb972b1da70fb310")!
+        )
+        return Map(item: portalItem)
+    }
+}
---
+++ Sources/ArcGISToolkit/Components/UtilityNetworkTrace/UtilityNetworkTraceViewModelTrace.swift
@@ -0,0 +1,58 @@
+// Copyright 2022 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import ArcGIS
+import SwiftUI
+
+extension UtilityNetworkTraceViewModel {
+    /// A trace performed on a utility network.
+    struct Trace {
+        /// A collection of asset labels for all elements returned in the trace.
+        var assetLabels = [String]()
+        
+        /// A user given color for the trace with a default value of green.
+        var color: Color = .green {
+            didSet {
+                // Update the color of any pre-existing starting points
+                startingPoints.forEach { startingPoint in
+                    guard let symbol = startingPoint.graphic.symbol as? SimpleMarkerSymbol else { return }
+                    symbol.color = UIColor(color)
+                }
+            }
+        }
+        
+        /// The chosen named trace configuration.
+        var configuration: UtilityNamedTraceConfiguration?
+        
+        var functionOutputs = [UtilityNetworkTraceFunctionOutput]()
+        
+        /// A collection of graphics generated from the trace.
+        var graphics = [Graphic]()
+        
+        /// A user given name for the trace.
+        var name: String = ""
+        
+        /// A collection of starting points for the trace.
+        var startingPoints = [UtilityNetworkTraceStartingPoint]()
+        
+        /// A trace result set comprised of an collection of UtilityElement objects.
+        var utilityElementTraceResult: UtilityElementTraceResult?
+        
+        /// A trace result set comprised of a collection of UtilityTraceFunctionOutput objects.
+        var utilityFunctionTraceResult: UtilityFunctionTraceResult?
+        
+        /// The trace result comprised of a set of Geometry objects representing the network elements
+        /// identified by the trace.
+        var utilityGeometryTraceResult: UtilityGeometryTraceResult?
+    }
+}
