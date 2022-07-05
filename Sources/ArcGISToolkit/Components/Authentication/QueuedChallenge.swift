aac6d2bf8cd4d1497b7d5a1ff089cef2e90c456b
Ryan Olson <rolson@esri.com>
2022-07-05 15:09:38 -0600

    doc

--- Sources/ArcGISToolkit/Components/Authentication/QueuedChallenge.swift
+++ Sources/ArcGISToolkit/Components/Authentication/QueuedChallenge.swift
@@ -14,13 +14,19 @@
 import Foundation
 import ArcGIS
 
+/// A type that represents a challenge in the queue of authentication challenges.
 protocol QueuedChallenge: AnyObject {
+    /// Waits for the challenge to complete.
     func complete() async
 }
 
+/// An object that represents an ArcGIS authentication challenge in the queue of challenges.
 final class QueuedArcGISChallenge: QueuedChallenge {
+    /// The ArcGIS authentication challenge.
     let arcGISChallenge: ArcGISAuthenticationChallenge
     
+    /// Creates a `QueuedArcGISChallenge`.
+    /// - Parameter arcGISChallenge: The associated ArcGIS authentication challenge.
     init(arcGISChallenge: ArcGISAuthenticationChallenge) {
         self.arcGISChallenge = arcGISChallenge
     }
@@ -58,22 +64,29 @@ final class QueuedArcGISChallenge: QueuedChallenge {
     }
 }
 
+/// An object that represents a network authentication challenge in the queue of challenges.
 final class QueuedNetworkChallenge: QueuedChallenge {
+    /// The associated network authentication challenge.
     let networkChallenge: NetworkAuthenticationChallenge
     
+    /// Creates a `QueuedNetworkChallenge`.
+    /// - Parameter networkChallenge: The associated network authentication challenge.
     init(networkChallenge: NetworkAuthenticationChallenge) {
         self.networkChallenge = networkChallenge
     }
-
-    func resume(with response: NetworkAuthenticationChallengeDisposition) {
+    
+    /// Resumes the queued challenge.
+    /// - Parameter disposition: The disposition to resume with.
+    func resume(with disposition: NetworkAuthenticationChallengeDisposition) {
         guard _disposition == nil else { return }
-        _disposition = response
+        _disposition = disposition
     }
     
     /// Use a streamed property because we need to support multiple listeners
     /// to know when the challenge completed.
     @Streamed private var _disposition: (NetworkAuthenticationChallengeDisposition)?
     
+    /// The resulting disposition of the challenge.
     var disposition: NetworkAuthenticationChallengeDisposition {
         get async {
             await $_disposition
@@ -86,12 +99,17 @@ final class QueuedNetworkChallenge: QueuedChallenge {
         _ = await disposition
     }
     
+    /// An enumeration that describes the kind of challenge.
     enum Kind {
+        /// A challenge for an untrusted host.
         case serverTrust
+        /// A challenge that requires a login via username and password.
         case login
+        /// A challenge that requires a client certificate.
         case certificate
     }
     
+    /// The kind of challenge.
     var kind: Kind {
         switch networkChallenge.kind {
         case .serverTrust:
