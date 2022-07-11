6bf1f353fa558b901fa1c5c3a84c4903a6d1edc4
David Feinzimer <dfeinzimer@esri.com>
2022-07-11 10:29:00 -0700

    Update UtilityNetworkTrace.swift

--- Sources/ArcGISToolkit/Components/UtilityNetworkTrace/UtilityNetworkTrace.swift
+++ Sources/ArcGISToolkit/Components/UtilityNetworkTrace/UtilityNetworkTrace.swift
@@ -267,7 +267,6 @@ public struct UtilityNetworkTrace: View {
             .lineLimit(1)
             .frame(maxWidth: .infinity, alignment: .center)
         List {
-            Text(selectedStartingPoint?.utilityElement.globalID.uuidString ?? "N/A")
             ForEach(Array(selectedStartingPoint!.geoElement.attributes.sorted(by: { $0.key < $1.key})), id: \.key) { item in
                 HStack{
                     Text(item.key)
