25a711b1e67792185cc6b4e8815d1bb85ff182bf
Ryan Olson <rolson@esri.com>
2022-07-13 15:05:15 -0600

    skip for placeholder

--- Tests/ArcGISToolkitTests/BasemapGalleryItemTests.swift
+++ Tests/ArcGISToolkitTests/BasemapGalleryItemTests.swift
@@ -28,7 +28,7 @@ import Combine
 final class BasemapGalleryItemTests: XCTestCase {
     override func setUp() async throws {
         ArcGISRuntimeEnvironment.apiKey = APIKey("<#API Key#>")
-        try XCTSkipIf(ArcGISRuntimeEnvironment.apiKey == nil)
+        try XCTSkipIf(ArcGISRuntimeEnvironment.apiKey == .placeholder)
     }
     
     func testInit() async throws {
