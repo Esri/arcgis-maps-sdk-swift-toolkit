d7bd17aad04e06328bb4541c6450ba8f4b3c2c8e
Ryan Olson <rolson@esri.com>
2022-07-13 14:29:28 -0600

    add test

--- Tests/ArcGISToolkitTests/AuthenticatorTests.swift
+++ Tests/ArcGISToolkitTests/AuthenticatorTests.swift
@@ -11,7 +11,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import SwiftUI
 import XCTest
 @testable import ArcGISToolkit
 import ArcGIS
