69d3e639a1a3e35d46c882579df655e527fcf74d
David Feinzimer <dfeinzimer@esri.com>
2022-07-15 16:35:57 -0700

    Update UtilityNetworkTraceExampleView.swift

--- Examples/Examples/UtilityNetworkTraceExampleView.swift
+++ Examples/Examples/UtilityNetworkTraceExampleView.swift
@@ -58,7 +58,7 @@ struct UtilityNetworkTraceExampleView: View {
                         $viewpoint
                     )
                     .task {
-                        await ArcGISURLSession.credentialStore.add(try! await .publicSample)
+                        await ArcGISRuntimeEnvironment.credentialStore.add(try! await .publicSample)
                     }
                 }
                 .padding()
