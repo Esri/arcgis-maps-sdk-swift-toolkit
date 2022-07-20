fb54190424b36e72c8ef32e8f5f27f5d8c4c37ec
R Olson <rolson@esri.com>
2022-07-20 13:13:40 -0600

    Update Sources/ArcGISToolkit/Components/Authentication/Login.swift
    Co-authored-by: Mark Dostal <mdostal@esri.com>

--- Sources/ArcGISToolkit/Components/Authentication/Login.swift
+++ Sources/ArcGISToolkit/Components/Authentication/Login.swift
@@ -119,7 +119,7 @@ extension LoginViewModifier {
         )
     }
     
-    /// Creates a `UsernamePasswordViewModifier` with a queued ArcGIS challenge.
+    /// Creates a `LoginViewModifier` with a queued ArcGIS challenge.
     @MainActor
     init(challenge: QueuedArcGISChallenge) {
         self.init(
