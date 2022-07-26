11137b9a5f3dab002007d738cd4a55e67886ded0
David Feinzimer <dfeinzimer@esri.com>
2022-07-26 08:15:33 -0700

    Update Sources/ArcGISToolkit/Components/UtilityNetworkTrace/UtilityNetworkTrace.swift
    Co-authored-by: Mark Dostal <mdostal@esri.com>

--- Sources/ArcGISToolkit/Components/UtilityNetworkTrace/UtilityNetworkTrace.swift
+++ Sources/ArcGISToolkit/Components/UtilityNetworkTrace/UtilityNetworkTrace.swift
@@ -58,8 +58,7 @@ public struct UtilityNetworkTrace: View {
 ***REMOVED******REMOVED***/ The current user activity.
 ***REMOVED***@State private var currentActivity: UserActivity = .creatingTrace(nil)
 ***REMOVED***
-***REMOVED******REMOVED***/ A value indicating whether the viewpoint should be automatically changed to show the resulting
-***REMOVED******REMOVED***/ extent of a trace.
+***REMOVED******REMOVED***/ A Boolean value indicating whether the map should be zoomed to the extent of the trace result.
 ***REMOVED***@State private var shouldZoomOnTraceCompletion = false
 ***REMOVED***
 ***REMOVED******REMOVED***/ A Boolean value indicating if the warning that all traces will be deleted is presented.
