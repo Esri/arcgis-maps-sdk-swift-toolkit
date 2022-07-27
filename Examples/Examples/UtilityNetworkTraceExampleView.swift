2fd1c9b5e727ddb140622f75b5f98334c79f4663
David Feinzimer <dfeinzimer@esri.com>
2022-07-27 09:43:08 -0700

    Remove programatic starting points from sample

--- Examples/Examples/UtilityNetworkTraceExampleView.swift
+++ Examples/Examples/UtilityNetworkTraceExampleView.swift
@@ -33,9 +33,6 @@ struct UtilityNetworkTraceExampleView: View {
     /// A container for graphical trace results.
     @State var resultGraphicsOverlay = GraphicsOverlay()
     
-    /// Optional pre-defined starting points for the utility network trace.
-    @State var startingPoints: [UtilityNetworkTraceSimpleStartingPoint] = []
-    
     /// The map viewpoint used by the `UtilityNetworkTrace` to pan/zoom the map to selected features.
     @State var viewpoint: Viewpoint?
     
@@ -62,8 +59,7 @@ struct UtilityNetworkTraceExampleView: View {
                         mapPoint: $mapPoint,
                         viewPoint: $viewPoint,
                         mapViewProxy: $mapViewProxy,
-                        viewpoint: $viewpoint,
-                        startingPoints: $startingPoints
+                        viewpoint: $viewpoint
                     )
                     .task {
                         await ArcGISRuntimeEnvironment.credentialStore.add(try! await .publicSample)
@@ -71,17 +67,6 @@ struct UtilityNetworkTraceExampleView: View {
                 }
                 .padding()
                 .frame(width: 360)
-            }
-            .overlay(alignment: .topLeading) {
-                Button {
-                    Task {
-                        await setPredefinedStartingPoints()
-                    }
-                } label: {
-                    Text("Set predefined starting points")
-                }
-                .buttonStyle(.borderedProminent)
-                .padding()
             }
         }
     }
@@ -104,30 +89,6 @@ private extension ArcGISCredential {
                 username: "viewer01",
                 password: "I68VGU^nMurF"
             )
-        }
-    }
-}
-
-extension UtilityNetworkTraceExampleView {
-    /// Queries the map for a feature with a certain ID and sets the list of starting points.
-    func setPredefinedStartingPoints() async {
-        let targetID = UUID(uuidString: "2A6D25D5-8B9E-400A-BC07-4A11BD8B6C82")
-        let groupLayer = map.operationalLayers.first as? GroupLayer
-        let parameters = QueryParameters()
-        parameters.addObjectId(1740)
-        for layer in groupLayer?.layers ?? [] {
-            guard let layer = layer as? FeatureLayer,
-                  let table = layer.featureTable else { continue }
-            let query = try? await table.queryFeatures(parameters: parameters)
-            query?.features().forEach { element in
-                if let feature = element as? ArcGISFeature,
-                   let id = feature.attributes["globalid"] as? UUID,
-                   id == targetID {
-                    startingPoints = [
-                        UtilityNetworkTraceSimpleStartingPoint(geoElement: element)
-                    ]
-                }
-            }
         }
     }
 }
