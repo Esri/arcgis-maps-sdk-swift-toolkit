4a09a9524d8c94cd80ebaec77d4bb00d4e17216f
Mark Dostal <mhdostal@aol.com>
2022-07-29 13:54:10 -0500

    Fix height calculation; documentation.

--- Sources/ArcGISToolkit/Components/Popups/HTMLTextView.swift
+++ Sources/ArcGISToolkit/Components/Popups/HTMLTextView.swift
@@ -19,28 +19,23 @@ struct HTMLTextView: UIViewRepresentable {
 ***REMOVED******REMOVED***/ The user-defined HTML string.
 ***REMOVED***var userHTML: String = ""
 ***REMOVED***
-***REMOVED******REMOVED***/ The html string to dispay, including the header.
+***REMOVED******REMOVED***/ The HTML string to dispay, including the header.
 ***REMOVED***var displayHTML: String {
 ***REMOVED******REMOVED******REMOVED*** Set the initial scale to 1, don't allow user scaling.
-***REMOVED******REMOVED******REMOVED*** When we went to WKWebView from UIWebView, the content got smaller,
-***REMOVED******REMOVED******REMOVED*** this fixes that and also doesn't allow the user to pinch to zoom.
+***REMOVED******REMOVED******REMOVED*** This fixess small text with WKWebView and also doesn't allow the
+***REMOVED******REMOVED******REMOVED*** user to pinch to zoom.
 ***REMOVED******REMOVED***var header = "<header><meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no'></header>"
 ***REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED*** Inject css in a head element to:
 ***REMOVED******REMOVED******REMOVED*** - word wrap long content such as urls
 ***REMOVED******REMOVED******REMOVED*** - set font family to default apple font
 ***REMOVED******REMOVED******REMOVED*** - set font size to subheadline
-***REMOVED******REMOVED******REMOVED*** - remove padding from the body. Add some margin to separate from the border of the webview.
+***REMOVED******REMOVED******REMOVED*** - remove padding from the body. Add some margin to separate from the
+***REMOVED******REMOVED******REMOVED***   border of the webview.
 ***REMOVED******REMOVED******REMOVED*** - limit images to a maximum width of 100%
 ***REMOVED******REMOVED******REMOVED***
-***REMOVED******REMOVED******REMOVED*** Also wrap the passed html fragment inside <html></html>
+***REMOVED******REMOVED******REMOVED*** Also wrap the passed HTML fragment inside <html></html>
 ***REMOVED******REMOVED******REMOVED*** open/close tags so that these css styles will apply
-***REMOVED******REMOVED******REMOVED***
-***REMOVED******REMOVED******REMOVED*** In order to make hyperlinks match our
-***REMOVED******REMOVED******REMOVED*** app tint we need to be passed a color
-***REMOVED******REMOVED******REMOVED*** and to dynamically inject it into the
-***REMOVED******REMOVED******REMOVED*** HTML/CSS
-***REMOVED******REMOVED******REMOVED***
 ***REMOVED******REMOVED***header = header.appending("""
 ***REMOVED******REMOVED******REMOVED******REMOVED***<html>
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***<head>
@@ -59,20 +54,22 @@ struct HTMLTextView: UIViewRepresentable {
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***<body>
 ***REMOVED******REMOVED******REMOVED******REMOVED***""")
 ***REMOVED******REMOVED***
-***REMOVED******REMOVED******REMOVED*** The final string is the header + userHTML + closing
-***REMOVED******REMOVED***return header.appending(userHTML.trimmingCharacters(in: .whitespacesAndNewlines)).appending("</body></html>")
+***REMOVED******REMOVED******REMOVED*** The final string is the header + userHTML + "</body></html>"
+***REMOVED******REMOVED***return header.appending(
+***REMOVED******REMOVED******REMOVED***userHTML.trimmingCharacters(in: .whitespacesAndNewlines)
+***REMOVED******REMOVED***).appending("</body></html>")
 ***REMOVED***
 ***REMOVED***
 ***REMOVED******REMOVED***/ The height of the view, calculated in the `webView(didFinish:)` delegate method.
-***REMOVED***@Binding private var dynamicHeight: CGFloat
-
+***REMOVED***@Binding private var height: CGFloat
+***REMOVED***
 ***REMOVED******REMOVED***/ Creates an `HTMLTextView`.
 ***REMOVED******REMOVED***/ - Parameters:
-***REMOVED******REMOVED***/   - html: The html string to be displayed.
-***REMOVED******REMOVED***/   - dynamicHeight: A binding to the calculated height of the `WKWebView`.
-***REMOVED***init(html: String, dynamicHeight: Binding<CGFloat>) {
+***REMOVED******REMOVED***/   - html: The HTML string to be displayed.
+***REMOVED******REMOVED***/   - height: A binding to the calculated height of the `WKWebView`.
+***REMOVED***init(html: String, height: Binding<CGFloat>) {
 ***REMOVED******REMOVED***userHTML = html
-***REMOVED******REMOVED***_dynamicHeight = dynamicHeight
+***REMOVED******REMOVED***_height = height
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***func makeUIView(context: Context) -> WKWebView {
@@ -81,8 +78,10 @@ struct HTMLTextView: UIViewRepresentable {
 ***REMOVED***
 ***REMOVED***func updateUIView(_ uiView: WKWebView, context: Context) {
 ***REMOVED******REMOVED***uiView.isOpaque = false
-***REMOVED******REMOVED******REMOVED*** This is a case where we always want the background to be white regardless of light/dark mode. If the user wants to implement dark mode,
-***REMOVED******REMOVED******REMOVED*** within their HTML, the background of the HTML will be shown over this background.
+***REMOVED******REMOVED******REMOVED*** This is a case where we always want the background to be white
+***REMOVED******REMOVED******REMOVED*** regardless of light/dark mode. If the user wants to implement dark
+***REMOVED******REMOVED******REMOVED*** mode, within their HTML, the background of the HTML will be shown
+***REMOVED******REMOVED******REMOVED*** over this background.
 ***REMOVED******REMOVED***uiView.backgroundColor = .white
 ***REMOVED******REMOVED***uiView.scrollView.backgroundColor = .white
 ***REMOVED******REMOVED***uiView.scrollView.isScrollEnabled = false
@@ -93,18 +92,27 @@ struct HTMLTextView: UIViewRepresentable {
 ***REMOVED***class Coordinator: NSObject, WKNavigationDelegate {
 ***REMOVED******REMOVED***var parent: HTMLTextView
 ***REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED***/ A Boolean value specifying when content starts arriving for the main frame.
 ***REMOVED******REMOVED***private var hasCommitted = false
-
+***REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED***/ A Boolean value specifying whether we've calculated a height for the web view or not.
+***REMOVED******REMOVED***private var hasHeight = false
+***REMOVED******REMOVED***
 ***REMOVED******REMOVED***init(_ parent: HTMLTextView) {
 ***REMOVED******REMOVED******REMOVED***self.parent = parent
 ***REMOVED***
 ***REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED*** WKNavigationDelegate method invoked when content starts
+***REMOVED******REMOVED******REMOVED*** arriving for the main frame.
 ***REMOVED******REMOVED***public func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
 ***REMOVED******REMOVED******REMOVED***hasCommitted = true
 ***REMOVED***
 ***REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED*** WKNavigationDelegate method for navigation actions.
-***REMOVED******REMOVED***func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction) async -> WKNavigationActionPolicy {
+***REMOVED******REMOVED***func webView(
+***REMOVED******REMOVED******REMOVED***_ webView: WKWebView,
+***REMOVED******REMOVED******REMOVED***decidePolicyFor navigationAction: WKNavigationAction
+***REMOVED******REMOVED***) async -> WKNavigationActionPolicy {
 ***REMOVED******REMOVED******REMOVED***if navigationAction.navigationType == .linkActivated,
 ***REMOVED******REMOVED******REMOVED***   (navigationAction.request.url?.scheme?.lowercased() == "http" ||
 ***REMOVED******REMOVED******REMOVED******REMOVED***navigationAction.request.url?.scheme?.lowercased() == "https"),
@@ -119,20 +127,30 @@ struct HTMLTextView: UIViewRepresentable {
 ***REMOVED******REMOVED***
 ***REMOVED***
 ***REMOVED******REMOVED***
-***REMOVED******REMOVED******REMOVED*** WKNavigationDelegate method where the size calculation happens.
+***REMOVED******REMOVED******REMOVED*** WKNavigationDelegate method invoked when a main frame navigation
+***REMOVED******REMOVED******REMOVED*** completes.  This is where the height calculation happens.
 ***REMOVED******REMOVED***func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!){
 ***REMOVED******REMOVED******REMOVED***webView.evaluateJavaScript("document.readyState") { [weak self] complete, _ in
-***REMOVED******REMOVED******REMOVED******REMOVED***guard complete != nil, let webView = webView  else {
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***return
-***REMOVED******REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED******REMOVED***guard complete != nil,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***  let webView = webView,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***  self?.hasCommitted ?? false
+***REMOVED******REMOVED******REMOVED******REMOVED***else { return ***REMOVED***
+***REMOVED******REMOVED******REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED******REMOVED***webView.evaluateJavaScript("document.body.scrollHeight") { height, _ in
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** Pass the new height to the delegate so that is can change the
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** cell height with performBatchUpdates
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***guard let height = height as? CGFloat else {
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***guard let self = self else { return ***REMOVED***
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***guard let height = height as? CGFloat,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***  !self.hasHeight else {
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***return
 ***REMOVED******REMOVED******REMOVED******REMOVED***
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***self?.parent.dynamicHeight = height
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** Set the new height, if we have one.
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***self.parent.height = height
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** With certain HTML strings, the JavaScript above kept
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** getting called, with increasingly large heights.  This
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** prevents that from happening.  As this block is only
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** called after the `document.readyState` is "complete",
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** this should be OK.
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***self.hasHeight = true
 ***REMOVED******REMOVED******REMOVED***
 ***REMOVED******REMOVED***
 ***REMOVED***
