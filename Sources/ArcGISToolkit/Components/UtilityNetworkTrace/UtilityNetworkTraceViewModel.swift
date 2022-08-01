ce3d73054a936b0add276083581bcf5c970d8f26
David Feinzimer <dfeinzimer@esri.com>
2022-08-01 10:44:19 -0700

    Update UtilityNetworkTraceViewModel.swift

--- Sources/ArcGISToolkit/Components/UtilityNetworkTrace/UtilityNetworkTraceViewModel.swift
+++ Sources/ArcGISToolkit/Components/UtilityNetworkTrace/UtilityNetworkTraceViewModel.swift
@@ -436,9 +436,8 @@ import SwiftUI
         _ = completedTraces[index].startingPoints.map { $0.graphic?.isSelected = isSelected }
     }
     
-    /// Loads the named trace configurations in the network.
-    /// Returns the named trace configurations in the network on the provided map.
     /// - Parameter map: A web map containing one or more utility networks.
+    /// - Returns: The named trace configurations in the network on the provided map.
     private func utilityNamedTraceConfigurations(from map: Map) async -> [UtilityNamedTraceConfiguration] {
         guard let network = network else { return [] }
         return (try? await map.getNamedTraceConfigurations(from: network)) ?? []
@@ -446,16 +445,17 @@ import SwiftUI
 }
 
 extension UtilityNetworkTraceViewModel {
-    /// Finds the location on the line nearest the input point, expressed as the fraction along the line’s total
-    /// geodesic length.
+    /// Finds the location on the line nearest the input point.
     /// - Parameters:
     ///   - inputGeometry: The line to be measured.
     ///   - point: A location along the line.
+    /// - Returns: A location along the line expressed as a fraction of its total length.
+    /// - Precondition: `inputGeometry` is a `Polyline`.
     private func fractionAlongEdge(
         of inputGeometry: Geometry,
         at point: Point
     ) -> Double {
-        var geometry = inputGeometry
+        guard var geometry = inputGeometry as? Polyline else { return .zero }
         // Remove Z
         if geometry.hasZ {
             geometry = GeometryEngine.makeGeometry(
@@ -475,7 +475,7 @@ extension UtilityNetworkTraceViewModel {
         }
         
         return GeometryEngine.polyline(
-            geometry as! Polyline,
+            geometry,
             fractionalLengthClosestTo: point,
             tolerance: 10
         )
