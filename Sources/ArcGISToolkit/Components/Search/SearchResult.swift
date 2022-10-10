275245be8bb27dca89a9a8ca54114f7f58fc530f
Philip Ridgeway <pridgeway@esri.com>
2022-10-10 12:58:46 -0700

    Address build error in SearchResult.swift
    The file was using an initializer of `Viewpoint` that no longer exists.

--- Sources/ArcGISToolkit/Components/Search/SearchResult.swift
+++ Sources/ArcGISToolkit/Components/Search/SearchResult.swift
@@ -99,7 +99,7 @@ extension SearchResult {
                 geometry: geocodeResult.displayLocation,
                 attributes: geocodeResult.attributes
             ),
-            selectionViewpoint: geocodeResult.extent.map(Viewpoint.init)
+            selectionViewpoint: geocodeResult.extent.map { .init(targetExtent: $0) }
         )
     }
 }
