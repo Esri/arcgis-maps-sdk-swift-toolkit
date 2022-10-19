51418481df7b005bc96fe8c84c8d05f6225352c7
Mark Dostal <mhdostal@aol.com>
2022-10-19 10:22:47 -0500

    Use view model instead of AsyncImage.

--- Sources/ArcGISToolkit/Components/Popups/PopupMedia/Images/AsyncImageView.swift
+++ Sources/ArcGISToolkit/Components/Popups/PopupMedia/Images/AsyncImageView.swift
@@ -13,27 +13,17 @@
 
 import SwiftUI
 import ArcGIS
-import Combine
 
 /// A view displaying an async image, with error display and progress view.
 struct AsyncImageView: View {
     /// The `URL` of the image.
     let url: URL
     
-    var imageURL: URL
-    
     /// The `ContentMode` defining how the image fills the available space.
     let contentMode: ContentMode
     
-    /// The `ContentMode` defining how the image fills the available space.
-    let refreshInterval: UInt64
-    
-//    @State var timer: Timer?
-    var timer: Publishers.Autoconnect<Timer.TimerPublisher>
-
-    @State var refreshing: Bool = false
-    
-    @State var currentImage: Image?
+    /// The data model for an `AsyncImageView`.
+    @StateObject var viewModel: AsyncImageViewModel
     
     /// Creates an `AsyncImageView`.
     /// - Parameters:
@@ -42,71 +32,38 @@ struct AsyncImageView: View {
     ///   - refreshInterval: The refresh interval, in milliseconds. A refresh interval of 0 means never refresh.
     public init(url: URL, contentMode: ContentMode = .fit, refreshInterval: UInt64 = 0) {
         self.url = url
-        self.imageURL = url
         self.contentMode = contentMode
-        self.refreshInterval = refreshInterval
-        print("self.refreshInterval = \(self.refreshInterval)")
-        let interval = refreshInterval > 0 ? Double(refreshInterval) / 1000 : TimeInterval.greatestFiniteMagnitude
-        timer = Timer.publish(every: interval, on: .main, in: .common).autoconnect()
+        
+        _viewModel = StateObject(
+            wrappedValue: AsyncImageViewModel(
+                imageURL: url,
+                refreshInterval: refreshInterval
+            )
+        )
     }
     
     var body: some View {
         ZStack {
-            // Display the current image behind the AsyncImage so when the
-            // AsyncImage is refreshing we don't get a white background with
-            // just the progress view.
-            if let currentImage {
-                currentImage
-                    .resizable()
-                    .aspectRatio(contentMode: contentMode)
-            }
-            AsyncImage(url: refreshing ? nil : imageURL) { phase in
-                if let image = phase.image {
-                    // Displays the loaded image.
-                    image
-                        .resizable()
-                        .aspectRatio(contentMode: contentMode)
-                        .task(id: refreshing) {
-                            print("ImageDrawing, refreshing = \(refreshing)")
-                            refreshing = false
-                            currentImage = image
-                        }
-                } else if phase.error != nil, !refreshing {
-                    // Displays an error notification.
-                    HStack(alignment: .center) {
-                        Image(systemName: "exclamationmark.circle")
-                            .aspectRatio(contentMode: .fit)
-                            .foregroundColor(.red)
-                        Text("An error occurred loading the image.")
+            switch viewModel.result {
+            case .success(let image):
+                if let image {
+                    ZStack {
+                        Image(uiImage: image)
+                            .resizable()
+                            .aspectRatio(contentMode: contentMode)
                     }
-                    .padding([.top, .bottom])
                 } else {
-                    // Display the progress view until image loads.
                     ProgressView()
                 }
+            case .failure(let error):
+                HStack(alignment: .center) {
+                    Image(systemName: "exclamationmark.circle")
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(.red)
+                    Text("An error occurred loading the image: \(error.localizedDescription).")
+                }
+                .padding([.top, .bottom])
             }
-        }
-        .onReceive(timer) { _ in
-            if !refreshing, currentImage != nil {
-                print("Timer fired, refreshing = \(refreshing)")
-                refreshing = true
-            }
-        }
-        .onAppear() {
-//            if refreshInterval > 0 {
-//                timer = Timer.scheduledTimer(
-//                    withTimeInterval: Double(refreshInterval) / 1000,
-//                    repeats: true,
-//                    block: { timer in
-//                        if !refreshing {
-//                            print("Timer fired, refreshing = \(refreshing)")
-//                            refreshing = true
-//                        }
-//                    })
-//            }
-        }
-        .onDisappear() {
-//            timer?.invalidate()
         }
     }
 }
