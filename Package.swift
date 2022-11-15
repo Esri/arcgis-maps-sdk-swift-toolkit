24dab9bde4c5fab18621351aa19e244a05e656e3
Mark Dostal <mhdostal@aol.com>
2022-11-15 14:04:37 -0600

    Give package the correct name

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@
 import PackageDescription
 
 let package = Package(
-***REMOVED***name: "arcgis-maps-sdk-toolkit-swift",
+***REMOVED***name: "arcgis-maps-sdk-swift-toolkit",
 ***REMOVED***platforms: [
 ***REMOVED******REMOVED***.macOS("12"), .iOS("15")
 ***REMOVED***],
