4420a4b88dc81506526195296517939f95954238
Mark Dostal <mhdostal@aol.com>
2022-12-16 12:22:46 -0600

    update doc for models.

--- Sources/ArcGISToolkit/Utility/SceneDataModel.swift
+++ Sources/ArcGISToolkit/Utility/SceneDataModel.swift
@@ -14,7 +14,13 @@
 import SwiftUI
 import ArcGIS
 
-/// A data model class containing a Scene.
+/// A very basic data model class containing a Scene. Since a `Scene` is not an observable object,
+/// clients can use `SceneDataModel` as an example of how you would store a scene in a data model
+/// class. The class inherits from `ObservableObject` and the `Scene` is defined as an @Published
+/// property. This allows SwiftUI views to be updated automatically when a new scene is set on the model.
+/// Being stored in the model also prevents the scene from continually being created during redraws.
+/// The data model class would be expanded upon in client code to contain other properties required
+/// for the model.
 public class SceneDataModel: ObservableObject {
     /// The `Scene` used for display in a `SceneView`.
     @Published public var scene: ArcGIS.Scene
