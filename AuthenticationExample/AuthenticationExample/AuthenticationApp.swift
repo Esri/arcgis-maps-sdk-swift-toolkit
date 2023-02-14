eac4bc7d59e7518699e9485159a36e015cf61350
Nimesh Jarecha <njarecha@esri.com>
2023-02-14 10:21:39 -0800

    fix doc

--- AuthenticationExample/AuthenticationExample/AuthenticationApp.swift
+++ AuthenticationExample/AuthenticationExample/AuthenticationApp.swift
@@ -50,7 +50,7 @@ struct AuthenticationApp: App {
 ***REMOVED******REMOVED******REMOVED***.environmentObject(authenticator)
 ***REMOVED******REMOVED******REMOVED***.task {
 ***REMOVED******REMOVED******REMOVED******REMOVED***isSettingUp = true
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** Here we setup credential stores to be persistent, which means that it will be
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** Here we setup credential stores to be persistent, which means that it will
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** synchronize with the keychain for storing credentials.
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** It also means that a user can sign in without having to be prompted for
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** credentials. Once credentials are cleared from the stores ("sign-out"),
