f7b6477c1d62e1b145c3c365c0428ebd73a016af
David Feinzimer <dfeinzimer@esri.com>
2023-03-30 15:52:05 -0700

    Update BookmarksExampleView.swift

--- Examples/Examples/BookmarksExampleView.swift
+++ Examples/Examples/BookmarksExampleView.swift
@@ -16,10 +16,8 @@ import ArcGISToolkit
 import SwiftUI
 
 struct BookmarksExampleView: View {
-    /// The data model containing a `Map` with predefined bookmarks.
-    @StateObject private var dataModel = MapDataModel(
-        map: Map(url: URL(string: "https://www.arcgis.com/home/item.html?id=16f1b8ba37b44dc3884afc8d5f454dd2")!)!
-    )
+    /// The `Map` with predefined bookmarks.
+    @State private var map = Map(url: URL(string: "https://www.arcgis.com/home/item.html?id=16f1b8ba37b44dc3884afc8d5f454dd2")!)!
     
     /// Indicates if the `Bookmarks` component is shown or not.
     /// - Remark: This allows a developer to control when the `Bookmarks` component is
@@ -31,44 +29,38 @@ struct BookmarksExampleView: View {
     @State var viewpoint: Viewpoint?
     
     var body: some View {
-        MapView(map: dataModel.map, viewpoint: viewpoint)
-            .onViewpointChanged(kind: .centerAndScale) {
-                viewpoint = $0
-            }
-            .toolbar {
-                ToolbarItem(placement: .primaryAction) {
-                    Button {
-                        showingBookmarks.toggle()
-                    } label: {
-                        Label(
-                            "Show Bookmarks",
-                            systemImage: "bookmark"
-                        )
-                    }
-                    .popover(isPresented: $showingBookmarks) {
-                        // Display the `Bookmarks` components with a pre-defined
-                        // list of bookmarks. Passing in a `Viewpoint` binding
-                        // will allow the `Bookmarks` component to handle
-                        // bookmark selection.
-                        Bookmarks(
-                            isPresented: $showingBookmarks,
-                            mapOrScene: dataModel.map,
-                            viewpoint: $viewpoint
-                        )
-                        
-                        // Display the `Bookmarks` component with the list of
-                        // bookmarks in a map.
-//                        Bookmarks(
-//                            isPresented: $showingBookmarks,
-//                            mapOrScene: map
-//                        )
-                        // In order to handle bookmark selection changes
-                        // manually, use `onSelectionChanged(perform:)`.
-//                        .onSelectionChanged {
-//                            viewpoint = $0.viewpoint
-//                        }
+        MapViewReader { mapViewProxy in
+            MapView(map: map, viewpoint: viewpoint)
+                .onViewpointChanged(kind: .centerAndScale) {
+                    viewpoint = $0
+                }
+                .toolbar {
+                    ToolbarItem(placement: .primaryAction) {
+                        Button {
+                            showingBookmarks.toggle()
+                        } label: {
+                            Label(
+                                "Show Bookmarks",
+                                systemImage: "bookmark"
+                            )
+                        }
+                        .popover(isPresented: $showingBookmarks) {
+                            // Display the `Bookmarks` component with the list of
+                            // bookmarks in a map.
+                            Bookmarks(
+                                isPresented: $showingBookmarks,
+                                mapOrScene: map
+                            )
+                            // In order to handle bookmark selection changes
+                            // manually, use `onSelectionChanged(perform:)`.
+                            .onSelectionChanged { bookmark in
+                                if let viewpoint = bookmark.viewpoint {
+                                    Task { await mapViewProxy.setViewpoint(viewpoint, duration: 1) }
+                                }
+                            }
+                        }
                     }
                 }
-            }
+        }
     }
 }
