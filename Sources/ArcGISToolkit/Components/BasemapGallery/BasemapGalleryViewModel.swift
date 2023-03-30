976fa8b3c6cb55b3e2f168cbc122f8d9f44edae6
David Feinzimer <dfeinzimer@esri.com>
2023-03-30 09:31:21 -0700

    Indentation

--- Sources/ArcGISToolkit/Components/BasemapGallery/BasemapGalleryViewModel.swift
+++ Sources/ArcGISToolkit/Components/BasemapGallery/BasemapGalleryViewModel.swift
@@ -62,7 +62,7 @@
 ***REMOVED******REMOVED***/ The error signifying the spatial reference of ``geoModel`` and the spatial reference of
 ***REMOVED******REMOVED***/ ``currentItem`` do not match.
 ***REMOVED***@Published private(set) var spatialReferenceMismatchError: SpatialReferenceMismatchError? = nil
-
+***REMOVED***
 ***REMOVED******REMOVED***/ If the `GeoModel` is not loaded when passed to the `BasemapGalleryViewModel`, then
 ***REMOVED******REMOVED***/ the geoModel will be immediately loaded. The spatial reference of geoModel dictates which
 ***REMOVED******REMOVED***/ basemaps from the gallery are enabled. When an enabled basemap is selected by the user,
@@ -109,7 +109,7 @@
 ***REMOVED***func portalDidChange(_ previousPortal: Portal?) {
 ***REMOVED******REMOVED******REMOVED*** Remove all items from `items`.
 ***REMOVED******REMOVED***items.removeAll()
-
+***REMOVED******REMOVED***
 ***REMOVED******REMOVED***guard let portal = portal else { return ***REMOVED***
 ***REMOVED******REMOVED***fetchBasemaps(from: portal)
 ***REMOVED***
