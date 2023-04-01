900fceb3000b487dbed3ba25bec62260e9cee231
Mark Dostal <mdostal@esri.com>
2023-04-01 14:36:56 -0500

    Apply suggestions from code review
    Co-authored-by: David Feinzimer <dfeinzimer@esri.com>

--- Sources/ArcGISToolkit/Components/Compass/Compass.swift
+++ Sources/ArcGISToolkit/Components/Compass/Compass.swift
@@ -134,7 +134,7 @@ public extension Compass {
 ***REMOVED***
 ***REMOVED***
 ***REMOVED******REMOVED***/ Specifies whether the ``Compass`` should automatically hide when the heading is 0.
-***REMOVED******REMOVED***/ - Parameter flag: A Boolean value indicating whether  the compass should automatically
+***REMOVED******REMOVED***/ - Parameter flag: A Boolean value indicating whether the compass should automatically
 ***REMOVED******REMOVED***/ hide/show itself when the heading is `0`.
 ***REMOVED***func automaticallyHides(_ flag: Bool) -> some View {
 ***REMOVED******REMOVED***var copy = self
