94a7a72dba6133cdf6b68ffc226a345563c103fe
David Feinzimer <dfeinzimer@esri.com>
2023-04-12 15:19:02 -0700

    Update Compass.swift

--- Sources/ArcGISToolkit/Components/Compass/Compass.swift
+++ Sources/ArcGISToolkit/Components/Compass/Compass.swift
@@ -79,8 +79,7 @@ extension Compass {
 ***REMOVED******REMOVED***/ - Parameter heading: The heading used to determine if the compass should hide.
 ***REMOVED******REMOVED***/ - Returns: `true` if the compass should hide, `false` otherwise.
 ***REMOVED***func shouldHide(forHeading heading: Double) -> Bool {
-***REMOVED******REMOVED***print(heading)
-***REMOVED******REMOVED***return (heading.rounded(.up) == 360 || heading.rounded(.down) == 0 || heading.isNaN) && autoHide
+***REMOVED******REMOVED***(heading.rounded(.up) == 360 || heading.rounded(.down) == 0 || heading.isNaN) && autoHide
 ***REMOVED***
 ***REMOVED***
 
