e6656e87663ef5c273ce34ab17bc80087267c244
David Feinzimer <dfeinzimer@esri.com>
2023-04-19 14:11:39 -0700

    Add tutorial files

---
+++ Sources/ArcGISToolkit/Documentation.docc/Resources/Compass/Compass0.swift
@@ -0,0 +1,13 @@
+***REMOVED***
+***REMOVED***Toolkit
+***REMOVED***
+
+struct CompassExampleView: View {
+***REMOVED***@State private var map = Map(basemapStyle: .arcGISImagery)
+***REMOVED***
+***REMOVED***var body: some View {
+***REMOVED******REMOVED***MapViewReader { proxy in
+***REMOVED******REMOVED******REMOVED***MapView(map: map)
+***REMOVED***
+***REMOVED***
+***REMOVED***
