2da9f4148cac7347e2ec2ec61a7e55c71174ca2e
David Feinzimer <dfeinzimer@esri.com>
2023-04-26 08:17:04 -0700

    Update ScalebarExampleView.swift

--- Examples/Examples/ScalebarExampleView.swift
+++ Examples/Examples/ScalebarExampleView.swift
@@ -16,6 +16,9 @@
 ***REMOVED***
 
 struct ScalebarExampleView: View {
+***REMOVED******REMOVED***/ The size of the map view's attribution bar.
+***REMOVED***@State private var attributionBarSize: CGSize = .zero
+***REMOVED***
 ***REMOVED******REMOVED***/ Allows for communication between the `Scalebar` and `MapView`.
 ***REMOVED***@State private var spatialReference: SpatialReference?
 ***REMOVED***
@@ -38,6 +41,9 @@ struct ScalebarExampleView: View {
 ***REMOVED***
 ***REMOVED***var body: some View {
 ***REMOVED******REMOVED***MapView(map: dataModel.map)
+***REMOVED******REMOVED******REMOVED***.onAttributionBarSizeChanged { newSize in
+***REMOVED******REMOVED******REMOVED******REMOVED***withAnimation { attributionBarSize = newSize ***REMOVED***
+***REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED***.onSpatialReferenceChanged { spatialReference = $0 ***REMOVED***
 ***REMOVED******REMOVED******REMOVED***.onUnitsPerPointChanged { unitsPerPoint = $0 ***REMOVED***
 ***REMOVED******REMOVED******REMOVED***.onViewpointChanged(kind: .centerAndScale) { viewpoint = $0 ***REMOVED***
@@ -49,7 +55,7 @@ struct ScalebarExampleView: View {
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***viewpoint: $viewpoint
 ***REMOVED******REMOVED******REMOVED******REMOVED***)
 ***REMOVED******REMOVED******REMOVED******REMOVED***.padding(.horizontal, 10)
-***REMOVED******REMOVED******REMOVED******REMOVED***.padding(.vertical, 50)
+***REMOVED******REMOVED******REMOVED******REMOVED***.padding(.vertical, 10 + attributionBarSize.height)
 ***REMOVED******REMOVED***
 ***REMOVED***
 ***REMOVED***
