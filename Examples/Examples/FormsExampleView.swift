bb4b75adec40704153efb7a8b638d42b574b5665
David Feinzimer <dfeinzimer@esri.com>
2023-06-14 15:05:37 -0700

    Updates

--- Examples/Examples/FormsExampleView.swift
+++ Examples/Examples/FormsExampleView.swift
@@ -16,11 +16,9 @@ import ArcGISToolkit
 import SwiftUI
 
 struct FormsExampleView: View {
-    @State private var isPresented = false
-    
     @State private var map = Map(url: .sampleData)!
     
-    @State private var attributes: [String: Any]?
+    @State private var feature: ArcGISFeature?
     
     /// The point on the screen the user tapped on to identify a feature.
     @State private var identifyScreenPoint: CGPoint?
@@ -32,36 +30,43 @@ struct FormsExampleView: View {
                     identifyScreenPoint = screenPoint
                 }
                 .task(id: identifyScreenPoint) {
-                    guard let screenPoint = identifyScreenPoint,
-                          let feature = try? await Result(awaiting: {
-                              try await mapViewProxy.identify(
-                                on: map.operationalLayers.first!,
-                                screenPoint: screenPoint,
-                                tolerance: 10
-                              )
-                          })
-                        .cancellationToNil()?
-                        .get()
-                        .geoElements
-                        .first as? ArcGISFeature else {
-                        isPresented = false
-                        attributes = nil
-                        return
+                    if let feature = await identifyFeature(with: mapViewProxy) {
+                        self.feature = feature
+                    } else {
+                        feature = nil
                     }
-                    isPresented = true
-                    print(feature.attributes.count)
-                    self.attributes = feature.attributes
                 }
                 .floatingPanel(
                     selectedDetent: .constant(.half),
                     horizontalAlignment: .leading,
-                    isPresented: $isPresented
+                    isPresented: Binding { feature != nil } set: { _ in }
                 ) {
                     Forms(map: map)
-                        .data(attributes)
+                        .feature(feature)
                         .padding()
                 }
                 .ignoresSafeArea(.keyboard)
+        }
+    }
+}
+
+extension FormsExampleView {
+    func identifyFeature(with proxy: MapViewProxy) async -> ArcGISFeature? {
+        if let screenPoint = identifyScreenPoint,
+              let feature = try? await Result(awaiting: {
+                  try await proxy.identify(
+                    on: map.operationalLayers.first!,
+                    screenPoint: screenPoint,
+                    tolerance: 10
+                  )
+              })
+            .cancellationToNil()?
+            .get()
+            .geoElements
+            .first as? ArcGISFeature {
+            return feature
+        } else {
+            return nil
         }
     }
 }
