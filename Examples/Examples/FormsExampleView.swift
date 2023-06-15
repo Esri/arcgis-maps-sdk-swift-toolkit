0ec3f5276e23e1c3f9e108a0d20c1165633edcb7
David Feinzimer <dfeinzimer@esri.com>
2023-06-14 18:41:57 -0700

    Update FormsExampleView.swift

--- Examples/Examples/FormsExampleView.swift
+++ Examples/Examples/FormsExampleView.swift
@@ -59,13 +59,13 @@ extension FormsExampleView {
     /// - Returns: The first identified feature.
     func identifyFeature(with proxy: MapViewProxy) async -> ArcGISFeature? {
         if let screenPoint = identifyScreenPoint,
-              let feature = try? await Result(awaiting: {
-                  try await proxy.identify(
-                    on: map.operationalLayers.first!,
-                    screenPoint: screenPoint,
-                    tolerance: 10
-                  )
-              })
+           let feature = try? await Result(awaiting: {
+               try await proxy.identify(
+                on: map.operationalLayers.first!,
+                screenPoint: screenPoint,
+                tolerance: 10
+               )
+           })
             .cancellationToNil()?
             .get()
             .geoElements
