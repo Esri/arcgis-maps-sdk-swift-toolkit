3d24119d20e7517e947e8fb5c477b3d95ee1b937
David Feinzimer <dfeinzimer@esri.com>
2023-06-15 15:46:09 -0700

    Update AsyncImageViewModel.swift

--- Sources/ArcGISToolkit/Components/Popups/PopupMedia/Images/AsyncImageViewModel.swift
+++ Sources/ArcGISToolkit/Components/Popups/PopupMedia/Images/AsyncImageViewModel.swift
@@ -107,8 +107,8 @@ struct LoadImageError: Error {
 
 extension LoadImageError: LocalizedError {
 ***REMOVED***public var errorDescription: String? {
-***REMOVED******REMOVED***return NSLocalizedString(
-***REMOVED******REMOVED******REMOVED***"The URL could not be reached or did not contain image data",
+***REMOVED******REMOVED***return String(
+***REMOVED******REMOVED******REMOVED***localized: "The URL could not be reached or did not contain image data",
 ***REMOVED******REMOVED******REMOVED***bundle: .toolkitModule,
 ***REMOVED******REMOVED******REMOVED***comment: "No Data"
 ***REMOVED******REMOVED***)
