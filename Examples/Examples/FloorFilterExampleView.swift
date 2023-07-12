cbb8b0cb8453ddc86905908346a9b683e43f60ca
David Feinzimer <dfeinzimer@esri.com>
2023-07-12 16:17:37 -0700

    Update FloorFilterExampleView.swift

--- Examples/Examples/FloorFilterExampleView.swift
+++ Examples/Examples/FloorFilterExampleView.swift
@@ -29,10 +29,12 @@ struct FloorFilterExampleView: View {
 ***REMOVED***@State private var isNavigating = false
 ***REMOVED***
 ***REMOVED******REMOVED***/ The `Map` displayed in the `MapView`.
-***REMOVED***@State private var map = Map(item: PortalItem(
-***REMOVED******REMOVED***portal: .arcGISOnline(connection: .anonymous),
-***REMOVED******REMOVED***id: Item.ID("b4b599a43a474d33946cf0df526426f5")!
-***REMOVED***))
+***REMOVED***@State private var map = Map(
+***REMOVED******REMOVED***item: PortalItem(
+***REMOVED******REMOVED******REMOVED***portal: .arcGISOnline(connection: .anonymous),
+***REMOVED******REMOVED******REMOVED***id: Item.ID("b4b599a43a474d33946cf0df526426f5")!
+***REMOVED******REMOVED***)
+***REMOVED***)
 ***REMOVED***
 ***REMOVED******REMOVED***/ A Boolean value indicating whether an error was encountered while loading the map.
 ***REMOVED***@State private var mapLoadError = false
