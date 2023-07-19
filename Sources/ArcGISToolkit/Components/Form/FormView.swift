48a17dd26b95612322d5fd3404895f1f61fc0371
David Feinzimer <dfeinzimer@esri.com>
2023-07-18 17:19:30 -0700

    Update FormView.swift

--- Sources/ArcGISToolkit/Components/Form/FormView.swift
+++ Sources/ArcGISToolkit/Components/Form/FormView.swift
@@ -27,6 +27,9 @@ public struct FormView: View {
     public init() {}
     
     public var body: some View {
+        //  When the `FormView` is hosted within a SwiftUI sheet, any `NavigationView` that may have
+        // been in the hierarchy is detached. A `NavigationView` is needed within the hierarchy to
+        // successfully present a keyboard toolbar (as is done in `MultiLineTextEntry`).
         NavigationView {
             ScrollView {
                 FormHeader(title: model.formDefinition?.title)
