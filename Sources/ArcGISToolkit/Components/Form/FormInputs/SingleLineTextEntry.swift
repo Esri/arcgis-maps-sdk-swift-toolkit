e00ffce049b57c563b040d28b272a3ba355e4979
David Feinzimer <dfeinzimer@esri.com>
2023-07-20 10:07:09 -0700

    Update SingleLineTextEntry.swift

--- Sources/ArcGISToolkit/Components/Form/FormInputs/SingleLineTextEntry.swift
+++ Sources/ArcGISToolkit/Components/Form/FormInputs/SingleLineTextEntry.swift
@@ -47,7 +47,7 @@ struct SingleLineTextEntry: View {
 ***REMOVED***var body: some View {
 ***REMOVED******REMOVED***FormElementHeader(element: element)
 ***REMOVED******REMOVED******REMOVED***.padding([.top], elementPadding)
-***REMOVED******REMOVED******REMOVED*** `MultiLineTextEntry` uses secondary foreground color so it's applied here for consistency.
+***REMOVED******REMOVED******REMOVED*** Secondary foreground color is used across entry views for consistency.
 ***REMOVED******REMOVED***HStack {
 ***REMOVED******REMOVED******REMOVED***TextField(element.label, text: $text, prompt: Text(element.hint ?? "").foregroundColor(.secondary))
 ***REMOVED******REMOVED******REMOVED******REMOVED***.focused($isFocused)
