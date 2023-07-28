37dc06105635049586d7d90058dc0406255187cb
Ryan Olson <rolson@esri.com>
2023-07-28 16:54:34 -0600

    implement username/password as a sheet

--- Sources/ArcGISToolkit/Utility/CredentialInputView.swift
+++ Sources/ArcGISToolkit/Utility/CredentialInputView.swift
@@ -238,12 +238,12 @@ extension View {
 ***REMOVED******REMOVED***/   - cancelAction: The cancel action.
 ***REMOVED******REMOVED***/   - continueAction: The continue action.
 ***REMOVED***@ViewBuilder func credentialInput(
-***REMOVED******REMOVED***fields: CredentialInputView.Fields,
+***REMOVED******REMOVED***fields: CredentialInputSheetView.Fields,
 ***REMOVED******REMOVED***isPresented: Binding<Bool>,
 ***REMOVED******REMOVED***message: String,
 ***REMOVED******REMOVED***title: String,
-***REMOVED******REMOVED***cancelAction: CredentialInputView.Action,
-***REMOVED******REMOVED***continueAction: CredentialInputView.Action
+***REMOVED******REMOVED***cancelAction: CredentialInputSheetView.Action,
+***REMOVED******REMOVED***continueAction: CredentialInputSheetView.Action
 ***REMOVED***) -> some View {
 ***REMOVED******REMOVED***modifier(
 ***REMOVED******REMOVED******REMOVED***CredentialInputModifier(
@@ -258,11 +258,34 @@ extension View {
 ***REMOVED***
 ***REMOVED***
 
+struct CredentialInputSheetView_Previews: PreviewProvider {
+***REMOVED***static var previews: some View {
+***REMOVED******REMOVED***Text("foo")
+***REMOVED******REMOVED***.credentialInput(
+***REMOVED******REMOVED******REMOVED***fields: .usernamePassword,
+***REMOVED******REMOVED******REMOVED***isPresented: .constant(true),
+***REMOVED******REMOVED******REMOVED***message: "You must sign in to access 'arcgis.com'",
+***REMOVED******REMOVED******REMOVED***title: "Authentication Required",
+***REMOVED******REMOVED******REMOVED***cancelAction: .init(
+***REMOVED******REMOVED******REMOVED******REMOVED***title: "Cancel",
+***REMOVED******REMOVED******REMOVED******REMOVED***handler: { _, _ in
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED***),
+***REMOVED******REMOVED******REMOVED***continueAction: .init(
+***REMOVED******REMOVED******REMOVED******REMOVED***title: "Continue",
+***REMOVED******REMOVED******REMOVED******REMOVED***handler: { username, password in
+***REMOVED******REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED***)
+***REMOVED******REMOVED***)
+***REMOVED***
+***REMOVED***
+
 ***REMOVED***/ A view modifier that prompts for credentials.
 struct CredentialInputModifier: ViewModifier {
 ***REMOVED***
 ***REMOVED******REMOVED***/ The fields shown in the view.
-***REMOVED***let fields: CredentialInputView.Fields
+***REMOVED***let fields: CredentialInputSheetView.Fields
 ***REMOVED***
 ***REMOVED******REMOVED***/ A Boolean value indicating whether or not the view is displayed.
 ***REMOVED***@Binding var isPresented: Bool
@@ -274,22 +297,206 @@ struct CredentialInputModifier: ViewModifier {
 ***REMOVED***let title: String
 ***REMOVED***
 ***REMOVED******REMOVED***/ The cancel action.
-***REMOVED***let cancelAction: CredentialInputView.Action
+***REMOVED***let cancelAction: CredentialInputSheetView.Action
 ***REMOVED***
 ***REMOVED******REMOVED***/ The continue action.
-***REMOVED***let continueAction: CredentialInputView.Action
+***REMOVED***let continueAction: CredentialInputSheetView.Action
 ***REMOVED***
 ***REMOVED***@ViewBuilder func body(content: Content) -> some View {
-***REMOVED******REMOVED***ZStack {
+***REMOVED******REMOVED***if #available(iOS 16.0, *) {
 ***REMOVED******REMOVED******REMOVED***content
-***REMOVED******REMOVED******REMOVED***CredentialInputView(
-***REMOVED******REMOVED******REMOVED******REMOVED***fields: fields,
-***REMOVED******REMOVED******REMOVED******REMOVED***isPresented: $isPresented,
-***REMOVED******REMOVED******REMOVED******REMOVED***message: message,
-***REMOVED******REMOVED******REMOVED******REMOVED***title: title,
-***REMOVED******REMOVED******REMOVED******REMOVED***cancelAction: cancelAction,
-***REMOVED******REMOVED******REMOVED******REMOVED***continueAction: continueAction
-***REMOVED******REMOVED******REMOVED***)
+***REMOVED******REMOVED******REMOVED******REMOVED***.sheet(isPresented: .constant(true)) {
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***Text("hello!")
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***CredentialInputSheetView(
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***fields: fields,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***isPresented: $isPresented,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***message: message,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***title: title,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***cancelAction: cancelAction,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***continueAction: continueAction
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***)
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.presentationDetents([.medium])
+***REMOVED******REMOVED******REMOVED***
+***REMOVED*** else {
+***REMOVED******REMOVED******REMOVED******REMOVED*** Fallback on earlier versions
+***REMOVED******REMOVED******REMOVED***content
+***REMOVED******REMOVED******REMOVED******REMOVED***.sheet(isPresented: .constant(true)) {
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***Text("hello!")
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***CredentialInputSheetView(
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***fields: fields,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***isPresented: $isPresented,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***message: message,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***title: title,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***cancelAction: cancelAction,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***continueAction: continueAction
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***)
+***REMOVED******REMOVED******REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+
+struct CredentialInputSheetView: View {
+***REMOVED***
+***REMOVED******REMOVED***/ The fields shown in the alert.
+***REMOVED***private let fields: Fields
+***REMOVED***
+***REMOVED******REMOVED***/ A Boolean value indicating whether or not the view is displayed.
+***REMOVED***@Binding private var isPresented: Bool
+***REMOVED***
+***REMOVED******REMOVED***/ Descriptive text that provides more details about the reason for the alert.
+***REMOVED***private let message: String
+***REMOVED***
+***REMOVED******REMOVED***/ The title of the alert.
+***REMOVED***private let title: String
+***REMOVED***
+***REMOVED******REMOVED***/ The cancel action.
+***REMOVED***private let cancelAction: Action
+***REMOVED***
+***REMOVED******REMOVED***/ The continue action.
+***REMOVED***private let continueAction: Action
+***REMOVED***
+***REMOVED******REMOVED***/ The value in the username field.
+***REMOVED******REMOVED***/
+***REMOVED******REMOVED***/ This member is unused when usage is set to `Usage.passwordOnly`.
+***REMOVED***@State private var username = ""
+
+***REMOVED******REMOVED***/ The value in the password field.
+***REMOVED***@State private var password = ""
+***REMOVED***
+***REMOVED******REMOVED***/ Creates the view.
+***REMOVED******REMOVED***/ - Parameters:
+***REMOVED******REMOVED***/   - fields: The fields shown in the alert.
+***REMOVED******REMOVED***/   - isPresented: A Boolean value indicating whether or not the view is displayed.
+***REMOVED******REMOVED***/   - message: Descriptive text that provides more details about the reason for the alert.
+***REMOVED******REMOVED***/   - title: The title of the alert.
+***REMOVED******REMOVED***/   - cancelAction: The cancel action.
+***REMOVED******REMOVED***/   - continueAction: The continue action.
+***REMOVED***init(
+***REMOVED******REMOVED***fields: Fields,
+***REMOVED******REMOVED***isPresented: Binding<Bool>,
+***REMOVED******REMOVED***message: String,
+***REMOVED******REMOVED***title: String,
+***REMOVED******REMOVED***cancelAction: Action,
+***REMOVED******REMOVED***continueAction: Action
+***REMOVED***) {
+***REMOVED******REMOVED***self.cancelAction = cancelAction
+***REMOVED******REMOVED***self.continueAction = continueAction
+***REMOVED******REMOVED***
+***REMOVED******REMOVED***_isPresented = isPresented
+***REMOVED******REMOVED***
+***REMOVED******REMOVED***self.fields = fields
+***REMOVED******REMOVED***self.message = message
+***REMOVED******REMOVED***self.title = title
+***REMOVED***
+***REMOVED***
+***REMOVED******REMOVED***/ A Boolean value indicating whether the alert should allow the continue action to proceed.
+***REMOVED***private var isContinueEnabled: Bool {
+***REMOVED******REMOVED***switch fields {
+***REMOVED******REMOVED***case .usernamePassword:
+***REMOVED******REMOVED******REMOVED***return !username.isEmpty && !password.isEmpty
+***REMOVED******REMOVED***case .password:
+***REMOVED******REMOVED******REMOVED***return !password.isEmpty
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***var usernameTextField: some View {
+***REMOVED******REMOVED***TextField(
+***REMOVED******REMOVED******REMOVED***String(
+***REMOVED******REMOVED******REMOVED******REMOVED***localized: "Username",
+***REMOVED******REMOVED******REMOVED******REMOVED***bundle: .toolkitModule
+***REMOVED******REMOVED******REMOVED***),
+***REMOVED******REMOVED******REMOVED***text: $username
+***REMOVED******REMOVED***)
+***REMOVED******REMOVED***.textInputAutocapitalization(.never)
+***REMOVED******REMOVED***.autocorrectionDisabled(true)
+***REMOVED******REMOVED***.textContentType(.username)
+***REMOVED***
+***REMOVED***
+***REMOVED***var passwordTextField: some View {
+***REMOVED******REMOVED***SecureField(
+***REMOVED******REMOVED******REMOVED***String(
+***REMOVED******REMOVED******REMOVED******REMOVED***localized: "Password",
+***REMOVED******REMOVED******REMOVED******REMOVED***bundle: .toolkitModule
+***REMOVED******REMOVED******REMOVED***),
+***REMOVED******REMOVED******REMOVED***text: $password
+***REMOVED******REMOVED***)
+***REMOVED******REMOVED***.textInputAutocapitalization(.never)
+***REMOVED******REMOVED***.autocorrectionDisabled(true)
+***REMOVED******REMOVED***.textContentType(.password)
+***REMOVED******REMOVED***.onSubmit {
+***REMOVED******REMOVED******REMOVED***continueAction.handler(username, password)
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***var body: some View {
+***REMOVED******REMOVED***VStack {
+***REMOVED******REMOVED******REMOVED***VStack(alignment: .center, spacing: 2) {
+***REMOVED******REMOVED******REMOVED******REMOVED***Text(title)
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.font(.title)
+***REMOVED******REMOVED******REMOVED******REMOVED***Text(message)
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.font(.subheadline)
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.foregroundColor(.secondary)
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.padding(.bottom)
+***REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED***.padding([.top, .horizontal])
+***REMOVED******REMOVED******REMOVED***VStack {
+***REMOVED******REMOVED******REMOVED******REMOVED***switch fields {
+***REMOVED******REMOVED******REMOVED******REMOVED***case .password:
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***passwordTextField
+***REMOVED******REMOVED******REMOVED******REMOVED***case .usernamePassword:
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***usernameTextField
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***Divider()
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***passwordTextField
+***REMOVED******REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED******REMOVED***Divider()
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.padding(.bottom)
+***REMOVED******REMOVED******REMOVED******REMOVED***HStack {
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***Spacer()
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***Button(role: .cancel) {
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***cancelAction.handler(username, password)
+***REMOVED******REMOVED******REMOVED******REMOVED*** label: {
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***Text(cancelAction.title)
+***REMOVED******REMOVED******REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.buttonStyle(.bordered)
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***Spacer()
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***Divider()
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***Spacer()
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***Button(continueAction.title) {
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***continueAction.handler(username, password)
+***REMOVED******REMOVED******REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.buttonStyle(.borderedProminent)
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.disabled(!isContinueEnabled)
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***Spacer()
+***REMOVED******REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED******REMOVED***.padding(.top)
+***REMOVED******REMOVED******REMOVED******REMOVED***.frame(maxHeight: 36)
+***REMOVED******REMOVED******REMOVED******REMOVED***Spacer()
+***REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED***.padding()
+***REMOVED******REMOVED******REMOVED***Spacer()
+***REMOVED***
+***REMOVED******REMOVED***.padding()
+***REMOVED***
+***REMOVED***
+extension CredentialInputSheetView {
+***REMOVED******REMOVED***/ The fields shown in the alert. This determines if the view is intended to require either a username
+***REMOVED******REMOVED***/ and password, or a password only.
+***REMOVED***enum Fields {
+***REMOVED******REMOVED******REMOVED***/ Indicates the view is intended to collect a password only.
+***REMOVED******REMOVED***case password
+***REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED***/ Indicates the view is intended to collect a username and password.
+***REMOVED******REMOVED***case usernamePassword
+***REMOVED***
+***REMOVED***
+
+extension CredentialInputSheetView {
+***REMOVED******REMOVED***/ A configuration for an alert action.
+***REMOVED***struct Action {
+***REMOVED******REMOVED******REMOVED***/ The title of the action.
+***REMOVED******REMOVED***let title: String
+***REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED***/ The block to execute when the action is triggered.
+***REMOVED******REMOVED***let handler: (String, String) -> Void
 ***REMOVED***
 ***REMOVED***
