61c90c7879d91f7a12138f303c1780eb53667f73
David Feinzimer <dfeinzimer@esri.com>
2023-08-03 09:38:58 -0700

    Add second test case

--- UI Test Runner/UITests/FormViewTests.swift
+++ UI Test Runner/UITests/FormViewTests.swift
@@ -68,3 +68,78 @@ final class FormViewTests: XCTestCase {
 ***REMOVED******REMOVED***)
 ***REMOVED***
 ***REMOVED***
+***REMOVED******REMOVED***/ Tests focused and unfocused state, with value (populated).
+***REMOVED***func testCase_1_2() throws {
+***REMOVED******REMOVED***let app = XCUIApplication()
+***REMOVED******REMOVED***let formViewTestsButton = app.buttons["Form View Tests"]
+***REMOVED******REMOVED***let inputValidationLabel = app.staticTexts["InputValidation"]
+***REMOVED******REMOVED***let fieldTitle = app.staticTexts["Single Line No Value, Placeholder or Description"]
+***REMOVED******REMOVED***let textField = app.textFields["Single Line No Value, Placeholder or Description Text Field"]
+***REMOVED******REMOVED***let helperText = app.staticTexts["Maximum 256 characters"]
+***REMOVED******REMOVED***let characterCount = app.staticTexts["11"]
+***REMOVED******REMOVED***let clearButton = app.buttons["Single Line No Value, Placeholder or Description Clear Button"]
+***REMOVED******REMOVED***let returnButton = app.buttons["Return"]
+***REMOVED******REMOVED***
+***REMOVED******REMOVED***app.launch()
+***REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED*** Open the Form View component test view.
+***REMOVED******REMOVED***formViewTestsButton.tap()
+***REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED*** Wait and verify that the form is opened.
+***REMOVED******REMOVED***XCTAssertTrue(
+***REMOVED******REMOVED******REMOVED***inputValidationLabel.waitForExistence(timeout: 30),
+***REMOVED******REMOVED******REMOVED***"The form failed to open after 30 seconds."
+***REMOVED******REMOVED***)
+***REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED*** Scroll to the target form element.
+***REMOVED******REMOVED***while !(fieldTitle.isHittable) {
+***REMOVED******REMOVED******REMOVED***app.scrollViews.firstMatch.swipeUp(velocity: 750)
+***REMOVED***
+***REMOVED******REMOVED***
+***REMOVED******REMOVED***XCTAssertTrue(
+***REMOVED******REMOVED******REMOVED***textField.isHittable,
+***REMOVED******REMOVED******REMOVED***"The text field wasn't found within 30 seconds."
+***REMOVED******REMOVED***)
+***REMOVED******REMOVED***
+***REMOVED******REMOVED***textField.tap()
+***REMOVED******REMOVED***
+***REMOVED******REMOVED***app.typeText("Sample text")
+***REMOVED******REMOVED***
+***REMOVED******REMOVED***XCTAssertTrue(
+***REMOVED******REMOVED******REMOVED***helperText.exists,
+***REMOVED******REMOVED******REMOVED***"The helper text wasn't visible when it should've been."
+***REMOVED******REMOVED***)
+***REMOVED******REMOVED***
+***REMOVED******REMOVED***XCTAssertTrue(
+***REMOVED******REMOVED******REMOVED***characterCount.exists,
+***REMOVED******REMOVED******REMOVED***"The character count wasn't visible when it should've been."
+***REMOVED******REMOVED***)
+***REMOVED******REMOVED***
+***REMOVED******REMOVED***XCTAssertTrue(
+***REMOVED******REMOVED******REMOVED***clearButton.isHittable,
+***REMOVED******REMOVED******REMOVED***"The clear button wasn't present when it should've been."
+***REMOVED******REMOVED***)
+***REMOVED******REMOVED***
+***REMOVED******REMOVED***returnButton.tap()
+***REMOVED******REMOVED***
+***REMOVED******REMOVED***XCTAssertTrue(
+***REMOVED******REMOVED******REMOVED***fieldTitle.isHittable,
+***REMOVED******REMOVED******REMOVED***"The title wasn't found within 30 seconds."
+***REMOVED******REMOVED***)
+***REMOVED******REMOVED***
+***REMOVED******REMOVED***XCTAssertFalse(
+***REMOVED******REMOVED******REMOVED***helperText.exists,
+***REMOVED******REMOVED******REMOVED***"The helper text was visible when it should've been hidden."
+***REMOVED******REMOVED***)
+***REMOVED******REMOVED***
+***REMOVED******REMOVED***XCTAssertTrue(
+***REMOVED******REMOVED******REMOVED***clearButton.isHittable,
+***REMOVED******REMOVED******REMOVED***"The clear button wasn't present when it should've been."
+***REMOVED******REMOVED***)
+***REMOVED******REMOVED***
+***REMOVED******REMOVED***XCTAssertTrue(
+***REMOVED******REMOVED******REMOVED***textField.isHittable,
+***REMOVED******REMOVED******REMOVED***"The text field wasn't found within 30 seconds."
+***REMOVED******REMOVED***)
+***REMOVED***
+***REMOVED***
