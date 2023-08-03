2c365ad45f1c20efc243c39f297a7f4bd1829591
David Feinzimer <dfeinzimer@esri.com>
2023-08-03 09:38:58 -0700

    Add second test case

--- UI Test Runner/UITests/FormViewTests.swift
+++ UI Test Runner/UITests/FormViewTests.swift
@@ -67,4 +67,79 @@ final class FormViewTests: XCTestCase {
             "The character count wasn't visible when it should've been."
         )
     }
+    
+    /// Tests focused and unfocused state, with value (populated).
+    func testCase_1_2() throws {
+        let app = XCUIApplication()
+        let formViewTestsButton = app.buttons["Form View Tests"]
+        let inputValidationLabel = app.staticTexts["InputValidation"]
+        let fieldTitle = app.staticTexts["Single Line No Value, Placeholder or Description"]
+        let textField = app.textFields["Single Line No Value, Placeholder or Description Text Field"]
+        let helperText = app.staticTexts["Maximum 256 characters"]
+        let characterCount = app.staticTexts["11"]
+        let clearButton = app.buttons["Single Line No Value, Placeholder or Description Clear Button"]
+        let returnButton = app.buttons["Return"]
+        
+        app.launch()
+        
+        // Open the Form View component test view.
+        formViewTestsButton.tap()
+        
+        // Wait and verify that the form is opened.
+        XCTAssertTrue(
+            inputValidationLabel.waitForExistence(timeout: 30),
+            "The form failed to open after 30 seconds."
+        )
+        
+        // Scroll to the target form element.
+        while !(fieldTitle.isHittable) {
+            app.scrollViews.firstMatch.swipeUp(velocity: 750)
+        }
+        
+        XCTAssertTrue(
+            textField.isHittable,
+            "The text field wasn't found within 30 seconds."
+        )
+        
+        textField.tap()
+        
+        app.typeText("Sample text")
+        
+        XCTAssertTrue(
+            helperText.exists,
+            "The helper text wasn't visible when it should've been."
+        )
+        
+        XCTAssertTrue(
+            characterCount.exists,
+            "The character count wasn't visible when it should've been."
+        )
+        
+        XCTAssertTrue(
+            clearButton.isHittable,
+            "The clear button wasn't present when it should've been."
+        )
+        
+        returnButton.tap()
+        
+        XCTAssertTrue(
+            fieldTitle.isHittable,
+            "The title wasn't found within 30 seconds."
+        )
+        
+        XCTAssertFalse(
+            helperText.exists,
+            "The helper text was visible when it should've been hidden."
+        )
+        
+        XCTAssertTrue(
+            clearButton.isHittable,
+            "The clear button wasn't present when it should've been."
+        )
+        
+        XCTAssertTrue(
+            textField.isHittable,
+            "The text field wasn't found within 30 seconds."
+        )
+    }
 }
