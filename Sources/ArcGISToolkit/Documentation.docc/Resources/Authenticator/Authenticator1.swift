a091d65dc5e33328ed6bb4ed3f82af300a57433b
David Feinzimer <dfeinzimer@esri.com>
2023-08-11 21:29:57 -0700

    Dev auth tutorial

---
+++ Sources/ArcGISToolkit/Documentation.docc/Resources/Authenticator/Authenticator1.swift
@@ -0,0 +1,13 @@
+***REMOVED***
+***REMOVED***Toolkit
+***REMOVED***
+
+***REMOVED***
+struct AuthenticationApp: App {
+***REMOVED***@ObservedObject var authenticator: Authenticator
+***REMOVED***
+***REMOVED***init() {
+***REMOVED******REMOVED***authenticator = Authenticator()
+***REMOVED******REMOVED***ArcGISEnvironment.authenticationManager.handleChallenges(using: authenticator)
+***REMOVED***
+***REMOVED***
