2f56606acf9a78c006b179ca99264d525acbdf46
David Feinzimer <dfeinzimer@esri.com>
2023-08-15 14:05:27 -0700

    Update Sources/ArcGISToolkit/Components/BasemapGallery/BasemapGallery.swift
    Co-authored-by: Philip Ridgeway <pridgeway@esri.com>

--- Sources/ArcGISToolkit/Components/BasemapGallery/BasemapGallery.swift
+++ Sources/ArcGISToolkit/Components/BasemapGallery/BasemapGallery.swift
@@ -15,7 +15,7 @@
 ***REMOVED***
 
 ***REMOVED***/ The `BasemapGallery` displays a collection of basemaps from ArcGIS Online, a user-defined
-***REMOVED***/ portal, or an array of `BasemapGalleryItem`s. When a new basemap is selected from the
+***REMOVED***/ portal, or an array of ``BasemapGalleryItem`` objects. When a new basemap is selected from the
 ***REMOVED***/ `BasemapGallery` and the optional `BasemapGalleryViewModel.geoModel` property is set, the
 ***REMOVED***/ basemap of the `geoModel` is replaced with the basemap in the gallery.
 ***REMOVED***/
