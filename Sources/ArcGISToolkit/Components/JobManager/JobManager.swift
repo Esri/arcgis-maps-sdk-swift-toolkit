7378d2cec7ae46449b4a0ba3f990f327b8ee8ba0
Ryan Olson <rolson@esri.com>
2023-08-18 10:04:20 -0600

    cleanup

--- Sources/ArcGISToolkit/Components/JobManager/JobManager.swift
+++ Sources/ArcGISToolkit/Components/JobManager/JobManager.swift
@@ -14,8 +14,6 @@
 ***REMOVED***
 ***REMOVED***
 
-public typealias JobID = String
-
 @MainActor
 public class JobManager: ObservableObject {
 ***REMOVED***public struct ID: RawRepresentable {
@@ -47,76 +45,42 @@ public class JobManager: ObservableObject {
 ***REMOVED******REMOVED***loadJobs()
 ***REMOVED***
 ***REMOVED***
-***REMOVED***private var isSavingSuppressed = false
+***REMOVED***@Published
+***REMOVED***public var jobs: [any JobProtocol] = []
 ***REMOVED***
-***REMOVED******REMOVED*** TODO: is this needed?
-***REMOVED***private func withSavingSuppressed<T>(body: @Sendable @MainActor () throws -> T) rethrows -> T {
-***REMOVED******REMOVED***isSavingSuppressed = true
-***REMOVED******REMOVED***defer { isSavingSuppressed = false ***REMOVED***
-***REMOVED******REMOVED***return try body()
-***REMOVED***
-***REMOVED***
-***REMOVED***private var _jobs: [JobID: any JobProtocol] = [:] {
-***REMOVED******REMOVED***willSet {
-***REMOVED******REMOVED******REMOVED***objectWillChange.send()
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***public var jobs: [any JobProtocol] {
-***REMOVED******REMOVED***Array(_jobs.values)
-***REMOVED***
-***REMOVED***
-***REMOVED******REMOVED***/ Registers a job with the job manager.
+***REMOVED******REMOVED***/ Adds a job to the job manager.
 ***REMOVED******REMOVED***/
 ***REMOVED******REMOVED***/ - Parameter job: The job to register.
 ***REMOVED***public func add(job: any JobProtocol) {
-***REMOVED******REMOVED***_jobs[UUID().uuidString] = job
+***REMOVED******REMOVED***jobs.append(job)
 ***REMOVED***
 ***REMOVED***
-***REMOVED******REMOVED***/ Unregisters a job from the job manager.
-***REMOVED******REMOVED***/
-***REMOVED******REMOVED***/ - Parameter id: The job's ID, returned from calling `register()`.
-***REMOVED******REMOVED***/ - Returns: `true` if the Job was found, `false` otherwise.
-***REMOVED***@discardableResult
-***REMOVED***public func remove(jobWithID id: JobID) -> Bool {
-***REMOVED******REMOVED***let removed = _jobs.removeValue(forKey: id) != nil
-***REMOVED******REMOVED***return removed
-***REMOVED***
-
-***REMOVED******REMOVED***/ Unregisters a job from the job manager.
+***REMOVED******REMOVED***/ Removes a job from the job manager.
 ***REMOVED******REMOVED***/
 ***REMOVED******REMOVED***/ - Parameter job: The job to unregister.
-***REMOVED******REMOVED***/ - Returns: `true` if the Job was found, `false` otherwise.
-***REMOVED***@discardableResult
-***REMOVED***public func remove(job: any JobProtocol) -> Bool {
-***REMOVED******REMOVED***guard let keyValue = _jobs.first(where: { $0.value === job ***REMOVED***) else {
-***REMOVED******REMOVED******REMOVED***return false
-***REMOVED***
-***REMOVED******REMOVED***
-***REMOVED******REMOVED***return remove(jobWithID: keyValue.key)
+***REMOVED***public func remove(job: any JobProtocol) {
+***REMOVED******REMOVED***guard let index = jobs.firstIndex(where: { $0 === job ***REMOVED***) else { return ***REMOVED***
+***REMOVED******REMOVED***jobs.remove(at: index)
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***public func removeAllJobs() {
-***REMOVED******REMOVED***_jobs.removeAll()
+***REMOVED******REMOVED***jobs.removeAll()
 ***REMOVED***
 ***REMOVED***
 ***REMOVED******REMOVED***/ Saves all managed jobs to User Defaults.
 ***REMOVED***private func saveJobs() {
-***REMOVED******REMOVED***guard !isSavingSuppressed else { return ***REMOVED***
-***REMOVED******REMOVED***let dictionary = _jobs.mapValues { $0.toJSON() ***REMOVED***
-***REMOVED******REMOVED***UserDefaults.standard.setValue(dictionary, forKey: defaultsKey)
+***REMOVED******REMOVED***let array = jobs.map { $0.toJSON() ***REMOVED***
+***REMOVED******REMOVED***UserDefaults.standard.setValue(array, forKey: defaultsKey)
 ***REMOVED***
 ***REMOVED***
 ***REMOVED******REMOVED***/ Load any jobs that have been saved to User Defaults.
 ***REMOVED***private func loadJobs() {
-***REMOVED******REMOVED***guard let dictionary = UserDefaults.standard.dictionary(forKey: defaultsKey) as? [JobID: String] else {
+***REMOVED******REMOVED***guard let strings = UserDefaults.standard.array(forKey: defaultsKey) as? [String] else {
 ***REMOVED******REMOVED******REMOVED***return
 ***REMOVED***
 ***REMOVED******REMOVED***
-***REMOVED******REMOVED***withSavingSuppressed {
-***REMOVED******REMOVED******REMOVED***_jobs = dictionary.compactMapValues {
-***REMOVED******REMOVED******REMOVED******REMOVED***try? Job.fromJSON($0) as? any JobProtocol
-***REMOVED******REMOVED***
+***REMOVED******REMOVED***jobs = strings.compactMap {
+***REMOVED******REMOVED******REMOVED***try? Job.fromJSON($0) as? any JobProtocol
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
