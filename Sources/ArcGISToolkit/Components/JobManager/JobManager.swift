b6b0975e5ff23fa1e145123fd22ecf2e101157b5
Ryan Olson <rolson@esri.com>
2023-08-21 13:25:34 -0600

    add bg processing to job manager

--- Sources/ArcGISToolkit/Components/JobManager/JobManager.swift
+++ Sources/ArcGISToolkit/Components/JobManager/JobManager.swift
@@ -13,6 +13,7 @@
 
 ***REMOVED***
 ***REMOVED***
+import BackgroundTasks
 
 ***REMOVED***/ An object that manages saving jobs when the app is backgrounded and can reload them later.
 @MainActor
@@ -41,20 +42,61 @@ public class JobManager: ObservableObject {
 ***REMOVED******REMOVED***return "com.esri.ArcGISToolkit.jobManager.\(id.rawValue).jobs"
 ***REMOVED***
 ***REMOVED***
+***REMOVED***private let backgroundTaskIdentifier = "com.esri.ArcGISToolkit.jobManager.statusCheck"
+***REMOVED***
 ***REMOVED******REMOVED***/ An initializer that takes an ID for the job manager.
 ***REMOVED******REMOVED***/ It is the callers responsibility to make sure the ID is unique.
 ***REMOVED***public init(id: ID) {
 ***REMOVED******REMOVED***self.id = id
 ***REMOVED******REMOVED***
 ***REMOVED******REMOVED***let notificationCenter = NotificationCenter.default
-***REMOVED******REMOVED***notificationCenter.addObserver(self, selector: #selector(appMovedToBackground), name: UIApplication.willResignActiveNotification, object: nil)
+***REMOVED******REMOVED***notificationCenter.addObserver(self, selector: #selector(appMovingToBackground), name: UIApplication.willResignActiveNotification, object: nil)
+***REMOVED******REMOVED***
+***REMOVED******REMOVED***BGTaskScheduler.shared.register(forTaskWithIdentifier: backgroundTaskIdentifier, using: nil) { task in
+***REMOVED******REMOVED******REMOVED***self.isBackgroundStatusChecksScheduled = false
+***REMOVED******REMOVED******REMOVED***Task {
+***REMOVED******REMOVED******REMOVED******REMOVED***print("-- performing status checks")
+***REMOVED******REMOVED******REMOVED******REMOVED***await self.performStatusChecks()
+***REMOVED******REMOVED******REMOVED******REMOVED***self.scheduleBackgroundStatusCheck()
+***REMOVED******REMOVED******REMOVED******REMOVED***task.setTaskCompleted(success: true)
+***REMOVED******REMOVED***
+***REMOVED***
 ***REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED*** Load jobs from the saved state.
 ***REMOVED******REMOVED***loadState()
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***var isBackgroundStatusChecksScheduled = false
+***REMOVED***
+***REMOVED***func scheduleBackgroundStatusCheck() {
+***REMOVED******REMOVED******REMOVED*** Return if already scheduled.
+***REMOVED******REMOVED***guard !isBackgroundStatusChecksScheduled else {
+***REMOVED******REMOVED******REMOVED***return
+***REMOVED***
+***REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED*** Do not schedule if there are no running jobs.
+***REMOVED******REMOVED***guard !JobManager.default.jobs.filter({ $0.status == .started ***REMOVED***).isEmpty else {
+***REMOVED******REMOVED******REMOVED***return
+***REMOVED***
+***REMOVED******REMOVED***
+***REMOVED******REMOVED***isBackgroundStatusChecksScheduled = true
+***REMOVED******REMOVED***
+***REMOVED******REMOVED***let request = BGAppRefreshTaskRequest(identifier: backgroundTaskIdentifier)
+***REMOVED******REMOVED***request.earliestBeginDate = Calendar.current.date(byAdding: .second, value: 30, to: .now)
+***REMOVED******REMOVED***do {
+***REMOVED******REMOVED******REMOVED***try BGTaskScheduler.shared.submit(request)
+***REMOVED******REMOVED******REMOVED***print("-- Background Task Scheduled!")
+***REMOVED*** catch(let error) {
+***REMOVED******REMOVED******REMOVED***print("-- Scheduling Error \(error.localizedDescription)")
+***REMOVED***
+***REMOVED***
+***REMOVED***
 ***REMOVED******REMOVED***/ Called when the app moves to the background.
-***REMOVED***@objc private func appMovedToBackground() {
+***REMOVED***@objc private func appMovingToBackground() {
+***REMOVED******REMOVED******REMOVED*** schedule background status checks
+***REMOVED******REMOVED***scheduleBackgroundStatusCheck()
+***REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED*** Save the jobs to the user defaults when the app moves to the background.
 ***REMOVED******REMOVED***saveState()
 ***REMOVED***
