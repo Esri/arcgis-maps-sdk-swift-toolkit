900d05b73f297b37cdb1222aa586b6ceee611384
Philip Ridgeway <pridgeway@esri.com>
2023-08-22 15:32:29 -0700

    Import only what is needed
    Imports `Combine` wherever `Published` is used and removes `SwiftUI` imports where not necessary. Addresses the following warning seen with command line builds:
    
    ```
    warning: 'Published' aliases 'Combine.Published' and cannot be used as property wrapper here because 'Combine' was not imported by this file; this is an error in Swift 6
    ```

--- Sources/ArcGISToolkit/Components/BasemapGallery/BasemapGalleryItem.swift
+++ Sources/ArcGISToolkit/Components/BasemapGallery/BasemapGalleryItem.swift
@@ -11,9 +11,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import Foundation
-import UIKit.UIImage
 import ArcGIS
+import Combine
+import UIKit
 
 ///  The `BasemapGalleryItem` encompasses an element in a `BasemapGallery`.
 public class BasemapGalleryItem: ObservableObject {
