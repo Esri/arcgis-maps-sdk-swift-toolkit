d7be1e20496d5ad0e6d4ca3b9c7556db090c7718
Ryan Olson <rolson@esri.com>
2023-08-30 09:28:35 -0600

    doc

--- Sources/ArcGISToolkit/Components/JobManager/JobManager.swift
+++ Sources/ArcGISToolkit/Components/JobManager/JobManager.swift
@@ -19,26 +19,67 @@ import OSLog
 
 /// An object that manages saving and loading jobs so that they can continue to run if the
 /// app is backgrounded or even terminated.
-/// There are 4 situations that the job manager helps with:
-/// 1. The job manager will serialize jobs to the user defaults when an app is backgrounded
-/// or terminated and then deserialize those jobs whenever an app is launched.
-/// 2. The job manager will ask the system for some background processing time when an app
+///
+/// The job manager is not instantiable, you must use the ``shared`` instance.
+///
+/// **Background**
+///
+/// Jobs are long running server operations. When a job instance is started on the client,
+/// it makes a request to a service asking it to start work on the server. At that point, the client
+/// polls the server intermittently to check the status of the work. Once the work is completed
+/// the result is downloaded with a background `URLSession`. This allows the download to
+/// complete out of process, and the download task can relaunch the app upon completion, even
+/// in the case where the app was terminated.
+///
+/// We do not expect users to keep an application in the foreground and wait for a job to complete.
+/// Once the job is started, if the app is backgrounded, we can use an app refresh
+/// background task to check the status of the work on the server. If the server work is complete
+/// we can start downloading the result in the background at that point. If the work on the server
+/// is not complete, we can reschedule another background app refresh to recheck status.
+///
+/// There is some iOS behavior to be aware of as well. In iOS, if an application is backgrounded,
+/// the operating system can terminate the app at its discretion. This means that jobs need to be
+/// serialized when an app is backgrounded so that if the app is terminated the jobs can be
+/// rehydrated upon relaunch of the app.
+///
+/// Also in iOS if the user of an app removes the app from the app switcher (swiping up) then any
+/// background downloads in progress are canceled by the operating system.
+///
+/// **Features**
+///
+/// The job manager is an `ObservableObject` with a mutable ``jobs`` property. Adding a job to this
+/// property will allow the job manager to do the work to make sure that we can rehydrate a job
+/// if an app is terminated.
+///
+/// As such, the job manager will:
+///
+///     - Serialize the job to the user defaults when the app is backgrounded
+///     - Deserialize the job when an application is relaunched
+///
+/// The job manager will help with the lifetime of jobs in other ways as well.
+///
+/// The job manager will ask the system for some background processing time when an app
 /// is backgrounded so that jobs that are not yet started on the server, can have some time to
 /// allow them to start. This means if you kick off a job and it hasn't actually started on the server
 /// when the app is backgrounded, the job should have enough time to start on the server which
-/// will cause it to enter into a polling state. When the job is in the polling state it checks
-/// the status of the server job every so often.
-/// 3. The job manager will request from the system a background refresh task (if enabled via
-/// the ``JobManager/preferredBackgroundStatusCheckSchedule`` property). This will happen
-/// when the app is backgrounded. If the system later executes the background refresh task then the
+/// will cause it to enter into a polling state. When the job reaches the polling state the
+/// status of the work on the server can be checked intermittently.
+///
+/// To enable polling while an app is backgrounded, the job manager will request from the system a
+/// background refresh task (if enabled via the ``JobManager/preferredBackgroundStatusCheckSchedule``
+/// property). If the system later executes the background refresh task then the
 /// job manager will check the status of any running jobs. At that point the jobs may start
-/// downloading their result. Note, this does not work on the simulator.
-/// 4. By default, Jobs will download their results with background URL session. This means that the
+/// downloading their result. Note, this does not work on the simulator, this behavior can only
+/// be tested on an actual device.
+///
+/// Now that the job can check status in the background, it can start downloading in the background.
+/// By default, jobs will download their results with background URL session. This means that the
 /// download can execute out of process, even if the app is terminated. If the app is terminated and
 /// then later relaunched by the system because a background downloaded completed, then you may
 /// call the ``JobManager/resumeAllPausedJobs()`` method from the application relaunch point,
 /// which will correllate the jobs to their respective downloads that completed and the jobs will
-/// then finish.
+/// then finish. The app relaunch point can happen via the SwiftUI modifier `.backgroundTask(.urlSession(...))`.
+/// In UIKit it would be the `UIApplicationDelegate` method `func application(UIApplication, handleEventsForBackgroundURLSession: String, completionHandler: () -> Void)`
 @MainActor
 public class JobManager: ObservableObject {
     /// The shared job manager.
