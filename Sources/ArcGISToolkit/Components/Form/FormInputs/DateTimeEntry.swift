4349f77694f55de90ce4809b8da9a14b12fabe5e
David Feinzimer <dfeinzimer@esri.com>
2023-09-11 11:44:45 -0700

    Extend DateFormatter

--- Sources/ArcGISToolkit/Components/Form/FormInputs/DateTimeEntry.swift
+++ Sources/ArcGISToolkit/Components/Form/FormInputs/DateTimeEntry.swift
@@ -60,9 +60,7 @@ struct DateTimeEntry: View {
 ***REMOVED***
 ***REMOVED******REMOVED***.padding([.bottom], elementPadding)
 ***REMOVED******REMOVED***.onAppear {
-***REMOVED******REMOVED******REMOVED***let dateFormatter = DateFormatter()
-***REMOVED******REMOVED******REMOVED***dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-***REMOVED******REMOVED******REMOVED***self.date = dateFormatter.date(from: element.value)
+***REMOVED******REMOVED******REMOVED***self.date = DateFormatter.arcGISDateFormatter.date(from: element.value)
 ***REMOVED***
 ***REMOVED******REMOVED***.onChange(of: date) { newDate in
 ***REMOVED******REMOVED******REMOVED******REMOVED***TODO: add `required` property to API
@@ -202,6 +200,13 @@ struct DateTimeEntry: View {
 ***REMOVED***
 ***REMOVED***
 
+private extension DateFormatter {
+***REMOVED***static let arcGISDateFormatter: DateFormatter = {
+***REMOVED******REMOVED***let dateFormatter = DateFormatter()
+***REMOVED******REMOVED***dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+***REMOVED******REMOVED***return dateFormatter
+***REMOVED***()
+***REMOVED***
 
 private extension String {
 ***REMOVED******REMOVED***/ A string indicating that no date or time has been set for a date/time field.
