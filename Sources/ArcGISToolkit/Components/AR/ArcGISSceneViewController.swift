7e0b67bdcafd38d3584e78fabe218579398dd873
Ryan Olson <rolson@esri.com>
2023-09-13 17:28:43 -0600

    fix preamble

--- Sources/ArcGISToolkit/Components/AR/ArcGISSceneViewController.swift
+++ Sources/ArcGISToolkit/Components/AR/ArcGISSceneViewController.swift
@@ -1,9 +1,15 @@
-***REMOVED***
-***REMOVED***  File.swift
-***REMOVED***  
-***REMOVED***
-***REMOVED***  Created by Ryan Olson on 9/13/23.
-***REMOVED***
+***REMOVED*** Copyright 2023 Esri.
+
+***REMOVED*** Licensed under the Apache License, Version 2.0 (the "License");
+***REMOVED*** you may not use this file except in compliance with the License.
+***REMOVED*** You may obtain a copy of the License at
+***REMOVED*** http:***REMOVED***www.apache.org/licenses/LICENSE-2.0
+
+***REMOVED*** Unless required by applicable law or agreed to in writing, software
+***REMOVED*** distributed under the License is distributed on an "AS IS" BASIS,
+***REMOVED*** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+***REMOVED*** See the License for the specific language governing permissions and
+***REMOVED*** limitations under the License.
 
 import Foundation
 import UIKit
