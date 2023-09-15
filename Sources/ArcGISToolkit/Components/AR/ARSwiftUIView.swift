164c35e29f40f0a7548c34f04f2ee44a66b6ea08
Ryan Olson <rolson@esri.com>
2023-09-15 15:11:58 -0600

    add proxy

--- Sources/ArcGISToolkit/Components/AR/ARSwiftUIView.swift
+++ Sources/ArcGISToolkit/Components/AR/ARSwiftUIView.swift
@@ -16,9 +16,19 @@ import ARKit
 import SwiftUI
 
 public struct ARSwiftUIView {
+    private(set) var alpha: CGFloat = 1.0
     private(set) var onRenderAction: ((SCNSceneRenderer, SCNScene, TimeInterval) -> Void)?
+    private(set) var onCameraTrackingStateChangeAction: ((ARSession, ARCamera) -> Void)?
+    private(set) var onGeoTrackingStatusChangeAction: ((ARSession, ARGeoTrackingStatus) -> Void)?
+    private(set) var onProxyAvailableAction: ((ARSwiftUIView.Proxy) -> Void)?
     
     init() {
+    }
+    
+    public func alpha(_ alpha: CGFloat) -> Self {
+        var view = self
+        view.alpha = alpha
+        return view
     }
     
     public func onRender(
@@ -28,12 +38,37 @@ public struct ARSwiftUIView {
         view.onRenderAction = action
         return view
     }
+    
+    public func onCameraTrackingStateChange(
+        perform action: @escaping (ARSession, ARCamera) -> Void
+    ) -> Self {
+        var view = self
+        view.onCameraTrackingStateChangeAction = action
+        return view
+    }
+    
+    public func onGeoTrackingStatusChange(
+        perform action: @escaping (ARSession, ARGeoTrackingStatus) -> Void
+    ) -> Self {
+        var view = self
+        view.onGeoTrackingStatusChangeAction = action
+        return view
+    }
+    
+    public func onProxyAvailable(
+        perform action: @escaping (ARSwiftUIView.Proxy) -> Void
+    ) -> Self {
+        var view = self
+        view.onProxyAvailableAction = action
+        return view
+    }
 }
 
 extension ARSwiftUIView: UIViewRepresentable {
     public func makeUIView(context: Context) -> ARSCNView {
         let arView = ARSCNView()
         arView.delegate = context.coordinator
+        onProxyAvailableAction?(Proxy(arView: arView))
         return arView
     }
     
@@ -56,10 +91,31 @@ extension ARSwiftUIView {
         public func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
             view.onRenderAction?(renderer, scene, time)
         }
+        
+        public func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+            view.onCameraTrackingStateChangeAction?(session, camera)
+        }
+        
+        public func session(_ session: ARSession, didChange geoTrackingStatus: ARGeoTrackingStatus) {
+            view.onGeoTrackingStatusChangeAction?(session, geoTrackingStatus)
+        }
     }
 }
 
 extension ARSwiftUIView {
-    public class Model: ObservableObject {
+    public class Proxy {
+        private let arView: ARSCNView
+        
+        init(arView: ARSCNView) {
+            self.arView = arView
+        }
+        
+        var session: ARSession {
+            arView.session
+        }
+        
+        var pointOfView: SCNNode? {
+            arView.pointOfView
+        }
     }
 }
