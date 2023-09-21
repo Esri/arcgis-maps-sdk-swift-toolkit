93d654cf0b3c64ff6505cc1810b9640d5d4eaf77
Destiny Hochhalter <dhochhalterruiz@esri.com>
2023-09-21 10:12:39 -0700

    Update example

--- Examples/Examples/TableTopExampleView.swift
+++ Examples/Examples/TableTopExampleView.swift
@@ -17,29 +17,43 @@
 
 struct TableTopExampleView: View {
 ***REMOVED***@State private var scene: ArcGIS.Scene = {
-***REMOVED******REMOVED***let scene = Scene(item: PortalItem(
-***REMOVED******REMOVED******REMOVED***portal: .arcGISOnline(connection: .anonymous),
-***REMOVED******REMOVED******REMOVED***id: PortalItem.ID("7558ee942b2547019f66885c44d4f0b1")!
-***REMOVED******REMOVED***)
-***REMOVED******REMOVED***)
+***REMOVED******REMOVED******REMOVED*** Creates a scene layer from buildings REST service.
+***REMOVED******REMOVED***let buildingsURL = URL(string: "https:***REMOVED***tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/DevA_BuildingShells/SceneServer")!
+***REMOVED******REMOVED***let buildingsLayer = ArcGISSceneLayer(url: buildingsURL)
+***REMOVED******REMOVED******REMOVED*** Creates an elevation source from Terrain3D REST service.
+***REMOVED******REMOVED***let elevationServiceURL = URL(string: "https:***REMOVED***elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")!
+***REMOVED******REMOVED***let elevationSource = ArcGISTiledElevationSource(url: elevationServiceURL)
+***REMOVED******REMOVED***let surface = Surface()
+***REMOVED******REMOVED***surface.addElevationSource(elevationSource)
+***REMOVED******REMOVED***let scene = Scene(basemapStyle: .arcGISTopographic)
+***REMOVED******REMOVED***scene.baseSurface = surface
+***REMOVED******REMOVED***scene.addOperationalLayer(buildingsLayer)
+***REMOVED******REMOVED***
 ***REMOVED******REMOVED***scene.baseSurface.navigationConstraint = .unconstrained
 ***REMOVED******REMOVED***scene.baseSurface.opacity = 0
+***REMOVED******REMOVED***
 ***REMOVED******REMOVED***return scene
 ***REMOVED***()
 ***REMOVED***
-***REMOVED***private let anchorPoint = Point(x: 4.4777, y: 51.9244, spatialReference: .wgs84)
+***REMOVED***private let anchorPoint = Point(
+***REMOVED******REMOVED***x: -122.68350326165559,
+***REMOVED******REMOVED***y: 45.53257485106716,
+***REMOVED******REMOVED***z: 10.189046687446535,
+***REMOVED******REMOVED***spatialReference: .wgs84
+***REMOVED***)
 ***REMOVED***
 ***REMOVED***var body: some View {
 ***REMOVED******REMOVED***TableTopSceneView(
 ***REMOVED******REMOVED******REMOVED***anchorPoint: anchorPoint,
 ***REMOVED******REMOVED******REMOVED***translationFactor: 1_000,
-***REMOVED******REMOVED******REMOVED***clippingDistance: 100
+***REMOVED******REMOVED******REMOVED***clippingDistance: 400
 ***REMOVED******REMOVED***) { proxy in
 ***REMOVED******REMOVED******REMOVED***SceneView(scene: scene)
 ***REMOVED******REMOVED******REMOVED******REMOVED***.onSingleTapGesture { screen, _ in
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***print("Identifying...")
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***Task.detached {
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***let results = try await proxy.identifyLayers(screenPoint: screen, tolerance: 20)
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***print("\(results.count) identify result(s).")
 ***REMOVED******REMOVED******REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED***
 ***REMOVED***
