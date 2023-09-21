8bcc9a24ef7feb80c7b7601a9ee503f5ece854d4
Destiny Hochhalter <dhochhalterruiz@esri.com>
2023-09-21 10:12:39 -0700

    Update example

--- Examples/Examples/TableTopExampleView.swift
+++ Examples/Examples/TableTopExampleView.swift
@@ -17,29 +17,43 @@ import ArcGISToolkit
 
 struct TableTopExampleView: View {
     @State private var scene: ArcGIS.Scene = {
-        let scene = Scene(item: PortalItem(
-            portal: .arcGISOnline(connection: .anonymous),
-            id: PortalItem.ID("7558ee942b2547019f66885c44d4f0b1")!
-        )
-        )
+        // Creates a scene layer from buildings REST service.
+        let buildingsURL = URL(string: "https://tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/DevA_BuildingShells/SceneServer")!
+        let buildingsLayer = ArcGISSceneLayer(url: buildingsURL)
+        // Creates an elevation source from Terrain3D REST service.
+        let elevationServiceURL = URL(string: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")!
+        let elevationSource = ArcGISTiledElevationSource(url: elevationServiceURL)
+        let surface = Surface()
+        surface.addElevationSource(elevationSource)
+        let scene = Scene(basemapStyle: .arcGISTopographic)
+        scene.baseSurface = surface
+        scene.addOperationalLayer(buildingsLayer)
+        
         scene.baseSurface.navigationConstraint = .unconstrained
         scene.baseSurface.opacity = 0
+        
         return scene
     }()
     
-    private let anchorPoint = Point(x: 4.4777, y: 51.9244, spatialReference: .wgs84)
+    private let anchorPoint = Point(
+        x: -122.68350326165559,
+        y: 45.53257485106716,
+        z: 10.189046687446535,
+        spatialReference: .wgs84
+    )
     
     var body: some View {
         TableTopSceneView(
             anchorPoint: anchorPoint,
             translationFactor: 1_000,
-            clippingDistance: 100
+            clippingDistance: 400
         ) { proxy in
             SceneView(scene: scene)
                 .onSingleTapGesture { screen, _ in
                     print("Identifying...")
                     Task.detached {
                         let results = try await proxy.identifyLayers(screenPoint: screen, tolerance: 20)
+                        print("\(results.count) identify result(s).")
                     }
                 }
         }
