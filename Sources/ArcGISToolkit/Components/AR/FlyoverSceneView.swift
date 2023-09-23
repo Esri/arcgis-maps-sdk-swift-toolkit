b7e934bdec76f26c0a59b6c5e6ea5452ef1765bb
Ryan Olson <rolson@esri.com>
2023-09-23 13:04:39 -0600

    research

--- Sources/ArcGISToolkit/Components/AR/FlyoverSceneView.swift
+++ Sources/ArcGISToolkit/Components/AR/FlyoverSceneView.swift
@@ -84,6 +84,19 @@ public struct FlyoverSceneView: View {
             lastGoodDeviceOrientation = deviceOrientation
         }
     }
+    
+    var window: UIWindow? {
+        guard let scene = UIApplication.shared.connectedScenes.first,
+              let windowSceneDelegate = scene.delegate as? UIWindowSceneDelegate,
+              let window = windowSceneDelegate.window else {
+            return nil
+        }
+        return window
+    }
+    
+    var interfaceOrientation: UIInterfaceOrientation? {
+        window?.windowScene?.interfaceOrientation
+    }
 }
 
 extension SceneViewProxy {
