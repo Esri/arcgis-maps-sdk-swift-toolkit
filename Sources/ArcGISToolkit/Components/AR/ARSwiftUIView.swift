8d2beec95eb0efd0ab87d1be3b18bb5eed0fab92
Destiny Hochhalter <dhochhalterruiz@esri.com>
2023-09-29 15:39:20 -0700

    Add onCameraTrackingStateChange closure

--- Sources/ArcGISToolkit/Components/AR/ARSwiftUIView.swift
+++ Sources/ArcGISToolkit/Components/AR/ARSwiftUIView.swift
@@ -20,6 +20,8 @@ typealias ARViewType = ARSCNView
 struct ARSwiftUIView {
     /// The closure to call when the session's frame updates.
     private(set) var onDidUpdateFrameAction: ((ARSession, ARFrame) -> Void)?
+    /// The closure to call when the camera's tracking status changes.
+    private(set) var onCameraTrackingStateChangeAction: ((ARSession, ARCamera) -> Void)?
     /// The closure to call when a node corresponding to a new anchor has been added to the view.
     private(set) var onAddNodeAction: ((SCNSceneRenderer, SCNNode, ARAnchor) -> Void)?
     /// The closure to call when a node has been updated to match it's corresponding anchor.
@@ -41,6 +43,15 @@ struct ARSwiftUIView {
     ) -> Self {
         var view = self
         view.onDidUpdateFrameAction = action
+        return view
+    }
+    
+    /// Sets the closure to call when the camera tracking status changes.
+    func onCameraTrackingStateChange(
+        perform action: @escaping (ARSession, ARCamera) -> Void
+    ) -> Self {
+        var view = self
+        view.onCameraTrackingStateChangeAction = action
         return view
     }
     
@@ -75,6 +86,7 @@ extension ARSwiftUIView: UIViewRepresentable {
     
     func updateUIView(_ uiView: ARViewType, context: Context) {
         context.coordinator.onDidUpdateFrameAction = onDidUpdateFrameAction
+        context.coordinator.onCameraTrackingStateChangeAction = onCameraTrackingStateChangeAction
         context.coordinator.onAddNodeAction = onAddNodeAction
         context.coordinator.onUpdateNodeAction = onUpdateNodeAction
     }
@@ -87,11 +99,16 @@ extension ARSwiftUIView: UIViewRepresentable {
 extension ARSwiftUIView {
     class Coordinator: NSObject, ARSCNViewDelegate, ARSessionDelegate {
         var onDidUpdateFrameAction: ((ARSession, ARFrame) -> Void)?
+        var onCameraTrackingStateChangeAction: ((ARSession, ARCamera) -> Void)?
         var onAddNodeAction: ((SCNSceneRenderer, SCNNode, ARAnchor) -> Void)?
         var onUpdateNodeAction: ((SCNSceneRenderer, SCNNode, ARAnchor) -> Void)?
         
         func session(_ session: ARSession, didUpdate frame: ARFrame) {
             onDidUpdateFrameAction?(session, frame)
+        }
+        
+        func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+            onCameraTrackingStateChangeAction?(session, camera)
         }
         
         func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
