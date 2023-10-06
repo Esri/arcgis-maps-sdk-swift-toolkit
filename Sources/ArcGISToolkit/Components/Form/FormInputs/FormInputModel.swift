7eb0dd04db6a9108fd17650135131b5fd157bc75
Mark Dostal <mdostal@esri.com>
2023-10-06 15:20:24 -0500

    Apply suggestions from code review
    Co-authored-by: David Feinzimer <dfeinzimer@esri.com>

--- Sources/ArcGISToolkit/Components/Form/FormInputs/FormInputModel.swift
+++ Sources/ArcGISToolkit/Components/Form/FormInputs/FormInputModel.swift
@@ -12,14 +12,20 @@
 // limitations under the License.
 
 import ArcGIS
+import Combine
 import SwiftUI
 
 /// - Since: 200.2
 public class FormInputModel: ObservableObject {
+    /// A Boolean value indicating whether a value in the input is required.
     @Published var isRequired: Bool
+    
+    /// A Boolean value indicating whether a value in the input is editable.
     @Published var isEditable: Bool
+    
+    /// The value of the input.
     @Published var value: String
-
+    
     private var element: FieldFormElement
     
     private var tasks = [Task<Void, Never>]()
@@ -28,14 +34,14 @@ public class FormInputModel: ObservableObject {
         clearTasks()
     }
     
-    /// Initializes a form view model.
+    /// Initializes a form input model.
     public init(fieldFormElement: FieldFormElement) {
         element = fieldFormElement
         isRequired = element.isRequired
         isEditable = element.isEditable
         value = element.value
-
-        // Kick off tasks to monitor autoPan, locations, and satellites.
+        
+        // Kick off tasks to monitor required, editable and value.
         tasks.append(
             contentsOf: [
                 observeIsRequiredTask,
@@ -53,11 +59,10 @@ public class FormInputModel: ObservableObject {
         tasks.removeAll()
     }
     
-    /// A detached task observing location display autoPan changes.
+    /// A detached task observing changes in the required state.
     private var observeIsRequiredTask: Task<Void, Never> {
         Task.detached { [unowned self] in
             for await isRequired in element.$isRequired {
-//                print("isRequired changed: \(isRequired) for \(element.label)")
                 await MainActor.run {
                     self.isRequired = isRequired
                 }
@@ -65,11 +70,10 @@ public class FormInputModel: ObservableObject {
         }
     }
     
-    /// A detached task observing location display autoPan changes.
+    /// A detached task observing changes in the editable state.
     private var observeIsEditableTask: Task<Void, Never> {
         Task.detached { [unowned self] in
             for await isEditable in element.$isEditable {
-//                print("isEditable changed: \(isEditable) (oldvalue: \(element.isEditable)) for \(element.label)")
                 await MainActor.run {
                     self.isEditable = isEditable
                 }
