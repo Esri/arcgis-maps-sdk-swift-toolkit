5b9f7834f1b0beb876939f192526f1d28f1caee4
Ryan Olson <rolson@esri.com>
2023-10-10 11:15:23 -0600

    back to world scale

--- Examples/Examples/WorldScaleExampleView.swift
+++ Examples/Examples/WorldScaleExampleView.swift
@@ -15,7 +15,7 @@ import SwiftUI
 import ArcGIS
 import ArcGISToolkit
 
-struct WorldTrackingExampleView: View {
+struct WorldScaleExampleView: View {
     @State private var scene: ArcGIS.Scene = {
 //        // Creates an elevation source from Terrain3D REST service.
 //        let elevationServiceURL = URL(string: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")!
@@ -35,7 +35,7 @@ struct WorldTrackingExampleView: View {
     
     var body: some View {
         VStack {
-            WorldTrackingSceneView { proxy in
+            WorldScaleSceneView { proxy in
                 SceneView(scene: scene)
                     .onSingleTapGesture { screen, _ in
                         print("Identifying...")
