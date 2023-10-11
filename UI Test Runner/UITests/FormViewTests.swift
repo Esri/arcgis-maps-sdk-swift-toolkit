11131375ab0204e539232dea3a449d877f2aa5b5
David Feinzimer <dfeinzimer@esri.com>
2023-10-11 13:53:08 -0700

    Update FormViewTests.swift

--- UI Test Runner/UITests/FormViewTests.swift
+++ UI Test Runner/UITests/FormViewTests.swift
@@ -852,7 +852,7 @@ final class FormViewTests: XCTestCase {
         )
         
         XCTExpectFailure(
-            "The design specifies the value should be String 3 but the value is actually `String 7`"
+            "The design specifies the value should be String 3 but the actual current value may differ."
         )
         
         XCTAssertEqual(
