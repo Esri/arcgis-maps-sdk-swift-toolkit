57bc14b023814cdbb6989c496f65ee616767b896
Destiny Hochhalter <dhochhalterruiz@esri.com>
2023-10-16 14:42:49 -0700

    Add FlyoverSceneView tutorial

---
+++ Sources/ArcGISToolkit/Documentation.docc/Resources/FlyoverSceneView/FlyoverSceneViewStep1.swift
@@ -0,0 +1,17 @@
+***REMOVED***
+***REMOVED***
+***REMOVED***Toolkit
+
+struct FlyoverExampleView: View {
+***REMOVED***var body: some View {
+***REMOVED******REMOVED***FlyoverSceneView(
+***REMOVED******REMOVED******REMOVED***initialLocation: Point(
+***REMOVED******REMOVED******REMOVED******REMOVED***x: 4.4777,
+***REMOVED******REMOVED******REMOVED******REMOVED***y: 51.9244,
+***REMOVED******REMOVED******REMOVED******REMOVED***z: 1_000,
+***REMOVED******REMOVED******REMOVED******REMOVED***spatialReference: .wgs84
+***REMOVED******REMOVED******REMOVED***),
+***REMOVED******REMOVED******REMOVED***translationFactor: 1_000
+***REMOVED******REMOVED***)
+***REMOVED***
+***REMOVED***
