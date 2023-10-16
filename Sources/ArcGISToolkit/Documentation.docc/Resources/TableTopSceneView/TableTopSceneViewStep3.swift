01ba8929bd7dd5e4feef6733454b66f724da49d5
Destiny Hochhalter <dhochhalterruiz@esri.com>
2023-10-16 14:47:48 -0700

    Add TableTopSceneView tutorial

---
+++ Sources/ArcGISToolkit/Documentation.docc/Resources/TableTopSceneView/TableTopSceneViewStep3.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+import ArcGIS
+import ArcGISToolkit
+
+struct TableTopExampleView: View {
+    @State private var scene: ArcGIS.Scene = {
+        // Creates a scene layer from buildings REST service.
+        let buildingsURL = URL(string: "https://tiles.arcgis.com/tiles/P3ePLMYs2RVChkJx/arcgis/rest/services/DevA_BuildingShells/SceneServer")!
+        let buildingsLayer = ArcGISSceneLayer(url: buildingsURL)
+        // Creates an elevation source from Terrain3D REST service.
+        let elevationServiceURL = URL(string: "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer")!
+        let elevationSource = ArcGISTiledElevationSource(url: elevationServiceURL)
+        let surface = Surface()
+        surface.addElevationSource(elevationSource)
+        let scene = Scene()
+        scene.baseSurface = surface
+        scene.addOperationalLayer(buildingsLayer)
+        scene.baseSurface.navigationConstraint = .unconstrained
+        scene.baseSurface.opacity = 0
+        return scene
+    }()
+    
+    private let anchorPoint = Point(
+        x: -122.68350326165559,
+        y: 45.53257485106716,
+        spatialReference: .wgs84
+    )
+    
+    var body: some View {
+        TableTopSceneView(
+            anchorPoint: anchorPoint,
+            translationFactor: 1_000,
+            clippingDistance: 400
+        ) { proxy in
+            SceneView(scene: scene)
+        }
+    }
+}
