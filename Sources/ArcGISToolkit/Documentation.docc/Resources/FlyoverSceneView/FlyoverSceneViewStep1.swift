e4bf811c39aaee8e8cc2c799a04296b7c1c93be7
Destiny Hochhalter <dhochhalterruiz@esri.com>
2023-10-20 12:52:06 -0700

    Use initializer with latitude and longitude

--- Sources/ArcGISToolkit/Documentation.docc/Resources/FlyoverSceneView/FlyoverSceneViewStep1.swift
+++ Sources/ArcGISToolkit/Documentation.docc/Resources/FlyoverSceneView/FlyoverSceneViewStep1.swift
@@ -5,12 +5,9 @@ import ArcGISToolkit
 struct FlyoverExampleView: View {
     var body: some View {
         FlyoverSceneView(
-            initialLocation: Point(
-                x: 4.4777,
-                y: 51.9244,
-                z: 1_000,
-                spatialReference: .wgs84
-            ),
+            initialLatitude: 45.54605,
+            initialLongitude: -122.69033,
+            initialAltitude: 500,
             translationFactor: 1_000
         )
     }
