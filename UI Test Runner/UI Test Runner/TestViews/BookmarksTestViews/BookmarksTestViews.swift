8bdf64ed97f7ea44e01f1275ec99b86bf3ed6501
David Feinzimer <dfeinzimer@esri.com>
2023-12-01 13:20:19 -0800

    Add new test cases

--- UI Test Runner/UI Test Runner/TestViews/BookmarksTestViews/BookmarksTestViews.swift
+++ UI Test Runner/UI Test Runner/TestViews/BookmarksTestViews/BookmarksTestViews.swift
@@ -1,10 +1,11 @@
 // Copyright 2023 Esri.
-
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// http://www.apache.org/licenses/LICENSE-2.0
-
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,15 +14,13 @@
 
 import SwiftUI
 
-struct Tests: View {
+struct BookmarksTestViews: View {
     var body: some View {
-        NavigationView {
-            List {
-                NavigationLink("Basemap Gallery Tests", destination: BasemapGalleryTestView())
-                NavigationLink("Bookmarks Tests", destination: BookmarksTestViews())
-                NavigationLink("Floor Filter Tests", destination: FloorFilterTestView())
-            }
+        List {
+            NavigationLink("Bookmarks Test Case 1", destination: BookmarksTestCase1View())
+            NavigationLink("Bookmarks Test Case 2", destination: BookmarksTestCase2View())
+            NavigationLink("Bookmarks Test Case 3", destination: BookmarksTestCase3View())
+            NavigationLink("Bookmarks Test Case 4", destination: BookmarksTestCase4View())
         }
-        .navigationViewStyle(.stack)
     }
 }
