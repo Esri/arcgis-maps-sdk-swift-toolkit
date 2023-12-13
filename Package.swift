344563c20ead0fa5a091ce89fc10ca361291bb8c
David Feinzimer <dfeinzimer@esri.com>
2023-12-13 12:48:21 -0800

    Use Dejavu `1.0.0`

--- Package.swift
+++ Package.swift
@@ -31,7 +31,7 @@ let package = Package(
 ***REMOVED***],
 ***REMOVED***dependencies: [
 ***REMOVED******REMOVED***.package(url: "https:***REMOVED***github.com/Esri/arcgis-maps-sdk-swift", .upToNextMinor(from: "200.3.0")),
-***REMOVED******REMOVED***.package(url: "https:***REMOVED***github.com/Esri/Dejavu", branch: "main")
+***REMOVED******REMOVED***.package(url: "https:***REMOVED***github.com/Esri/Dejavu", .upToNextMinor(from: "1.0.0"))
 ***REMOVED***],
 ***REMOVED***targets: [
 ***REMOVED******REMOVED***.target(
