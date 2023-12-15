57aa80492223effa076a32d4900c6692b5e6f9eb
David Feinzimer <dfeinzimer@esri.com>
2023-12-15 15:54:07 -0800

    Apply revisions

--- UI Test Runner/UI Test Runner/TestViews/BookmarksTestViews/BookmarksTestCase5View.swift
+++ UI Test Runner/UI Test Runner/TestViews/BookmarksTestViews/BookmarksTestCase5View.swift
@@ -1,10 +1,10 @@
-// Copyright 2023 Esri.
+// Copyright 2023 Esri
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//   http://www.apache.org/licenses/LICENSE-2.0
+//   https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,10 +26,10 @@ struct BookmarksTestCase5View: View {
     var body: some View {
         MapView(map: map)
             .sheet(isPresented: .constant(true)) {
+                Bookmarks(isPresented: .constant(true), bookmarks: bookmarks)
                 Button("Add New") {
                     bookmarks.append(Bookmark(name: "Bookmark \(bookmarks.count + 1)"))
                 }
-                Bookmarks(isPresented: .constant(true), bookmarks: bookmarks)
             }
     }
 }
