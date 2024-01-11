f29432a8df739902539eff95b93cac40b95017e5
David Feinzimer <dfeinzimer@esri.com>
2024-01-10 16:13:36 -0800

    Update InputFooter.swift

--- Sources/ArcGISToolkit/Components/FormView/FormInputs/InputFooter.swift
+++ Sources/ArcGISToolkit/Components/FormView/FormInputs/InputFooter.swift
@@ -28,8 +28,16 @@ struct InputFooter: View {
 ***REMOVED******REMOVED******REMOVED***Group {
 ***REMOVED******REMOVED******REMOVED******REMOVED***if isShowingError {
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***errorMessage
-***REMOVED******REMOVED******REMOVED*** else {
+***REMOVED******REMOVED******REMOVED*** else if !element.description.isEmpty {
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***Text(element.description)
+***REMOVED******REMOVED******REMOVED*** else if element.fieldType == .text {
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***if lengthRange?.lowerBound == lengthRange?.upperBound {
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***exactLengthMessage
+***REMOVED******REMOVED******REMOVED******REMOVED*** else if lengthRange?.lowerBound ?? 0 > 0 {
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***lengthRangeMessage
+***REMOVED******REMOVED******REMOVED******REMOVED*** else {
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***maximumLengthMessage
+***REMOVED******REMOVED******REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED***
 ***REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED***.accessibilityIdentifier("\(element.label) Footer")
@@ -70,21 +78,13 @@ extension InputFooter {
 ***REMOVED******REMOVED******REMOVED***if lengthRange?.lowerBound == lengthRange?.upperBound {
 ***REMOVED******REMOVED******REMOVED******REMOVED***exactLengthMessage
 ***REMOVED******REMOVED*** else {
-***REMOVED******REMOVED******REMOVED******REMOVED***Text(
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***"Maximum \(lengthRange!.upperBound) characters",
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***bundle: .toolkitModule,
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***comment: "Text indicating a field's maximum number of allowed characters."
-***REMOVED******REMOVED******REMOVED******REMOVED***)
+***REMOVED******REMOVED******REMOVED******REMOVED***maximumLengthMessage
 ***REMOVED******REMOVED***
 ***REMOVED******REMOVED***case .featureFormLessThanMinimumLengthError:
 ***REMOVED******REMOVED******REMOVED***if lengthRange?.lowerBound == lengthRange?.upperBound {
 ***REMOVED******REMOVED******REMOVED******REMOVED***exactLengthMessage
 ***REMOVED******REMOVED*** else {
-***REMOVED******REMOVED******REMOVED******REMOVED***Text(
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***"Minimum \(lengthRange!.lowerBound) characters",
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***bundle: .toolkitModule,
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***comment: "Text indicating a field's minimum number of allowed characters."
-***REMOVED******REMOVED******REMOVED******REMOVED***)
+***REMOVED******REMOVED******REMOVED******REMOVED***minimumLengthMessage
 ***REMOVED******REMOVED***
 ***REMOVED******REMOVED***case .featureFormExceedsNumericMaximumError, .featureFormLessThanNumericMinimumError:
 ***REMOVED******REMOVED******REMOVED***if let numericRange = element.domain as? RangeDomain, let minMax = numericRange.displayableMinAndMax {
@@ -145,15 +145,9 @@ extension InputFooter {
 ***REMOVED******REMOVED***)
 ***REMOVED***
 ***REMOVED***
-***REMOVED******REMOVED***/ The error to display for the input. If the element is not focused and has a required error this will be
-***REMOVED******REMOVED***/ the primary error. Otherwise the primary error is the first error in the element's set of errors.
-***REMOVED***var primaryError: ArcGIS.FeatureFormError? {
-***REMOVED******REMOVED***let elementErrors = model.validationErrors[element.fieldName] as? [ArcGIS.FeatureFormError]
-***REMOVED******REMOVED***if let requiredError = elementErrors?.first(where: { $0 == .featureFormFieldIsRequiredError ***REMOVED***), model.focusedElement != element {
-***REMOVED******REMOVED******REMOVED***return requiredError
-***REMOVED*** else {
-***REMOVED******REMOVED******REMOVED***return elementErrors?.first(where: { $0 != .featureFormFieldIsRequiredError ***REMOVED***)
-***REMOVED***
+***REMOVED******REMOVED***/ Determines whether an error is showing in the footer.
+***REMOVED***var isShowingError: Bool {
+***REMOVED******REMOVED***element.isEditable && primaryError != nil && model.previouslyFocusedFields.contains(element)
 ***REMOVED***
 ***REMOVED***
 ***REMOVED******REMOVED***/ The alloable range of number of characters in the input.
@@ -167,9 +161,46 @@ extension InputFooter {
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED******REMOVED***/ Determines whether an error is showing in the footer.
-***REMOVED***var isShowingError: Bool {
-***REMOVED******REMOVED***element.isEditable && primaryError != nil && model.previouslyFocusedFields.contains(element)
+***REMOVED******REMOVED***/ Text indicating a field's value must be within the allowed length range.
+***REMOVED***var lengthRangeMessage: Text {
+***REMOVED******REMOVED***Text(
+***REMOVED******REMOVED******REMOVED***"Enter \(lengthRange!.lowerBound) to \(lengthRange!.upperBound) characters",
+***REMOVED******REMOVED******REMOVED***bundle: .toolkitModule,
+***REMOVED******REMOVED******REMOVED***comment: """
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** Text indicating a field's value must be within the 
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** allowed length range. The first and second parameter
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** hold the minimum and maximum length respectively.
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** """
+***REMOVED******REMOVED***)
+***REMOVED***
+***REMOVED***
+***REMOVED******REMOVED***/ Text indicating a field's maximum number of allowed characters.
+***REMOVED***var maximumLengthMessage: Text {
+***REMOVED******REMOVED***Text(
+***REMOVED******REMOVED******REMOVED***"Maximum \(lengthRange!.upperBound) characters",
+***REMOVED******REMOVED******REMOVED***bundle: .toolkitModule,
+***REMOVED******REMOVED******REMOVED***comment: "Text indicating a field's maximum number of allowed characters."
+***REMOVED******REMOVED***)
+***REMOVED***
+***REMOVED***
+***REMOVED******REMOVED***/ Text indicating a field's maximum number of allowed characters.
+***REMOVED***var minimumLengthMessage: Text {
+***REMOVED******REMOVED***Text(
+***REMOVED******REMOVED******REMOVED***"Minimum \(lengthRange!.lowerBound) characters",
+***REMOVED******REMOVED******REMOVED***bundle: .toolkitModule,
+***REMOVED******REMOVED******REMOVED***comment: "Text indicating a field's minimum number of allowed characters."
+***REMOVED******REMOVED***)
+***REMOVED***
+***REMOVED***
+***REMOVED******REMOVED***/ The error to display for the input. If the element is not focused and has a required error this will be
+***REMOVED******REMOVED***/ the primary error. Otherwise the primary error is the first error in the element's set of errors.
+***REMOVED***var primaryError: ArcGIS.FeatureFormError? {
+***REMOVED******REMOVED***let elementErrors = model.validationErrors[element.fieldName] as? [ArcGIS.FeatureFormError]
+***REMOVED******REMOVED***if let requiredError = elementErrors?.first(where: { $0 == .featureFormFieldIsRequiredError ***REMOVED***), model.focusedElement != element {
+***REMOVED******REMOVED******REMOVED***return requiredError
+***REMOVED*** else {
+***REMOVED******REMOVED******REMOVED***return elementErrors?.first(where: { $0 != .featureFormFieldIsRequiredError ***REMOVED***)
+***REMOVED***
 ***REMOVED***
 ***REMOVED***
 
