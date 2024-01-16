841d60e6ba0ccfd54a5df993b8741452e4861ced
David Feinzimer <dfeinzimer@esri.com>
2024-01-16 10:26:51 -0800

    Use `element.validationErrors` directly

--- Sources/ArcGISToolkit/Components/FormView/FormViewModel.swift
+++ Sources/ArcGISToolkit/Components/FormView/FormViewModel.swift
@@ -98,8 +98,3 @@ import Combine
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED******REMOVED***/ A shortcut to all of the validation erros present in the form.
-***REMOVED***var validationErrors: [String : [Error]] {
-***REMOVED******REMOVED***featureForm.validationErrors
-***REMOVED***
-***REMOVED***
