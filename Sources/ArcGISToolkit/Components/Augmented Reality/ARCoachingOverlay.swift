3d9eb8619ef3d1a4899d92d418ea5de11fb44125
Ting Chen <tchen@esri.com>
2024-01-25 10:18:57 -0800

    Add delegate methods to `ARCoachingOverlay`.

--- Sources/ArcGISToolkit/Components/Augmented Reality/ARCoachingOverlay.swift
+++ Sources/ArcGISToolkit/Components/Augmented Reality/ARCoachingOverlay.swift
@@ -16,7 +16,14 @@ import ARKit
 ***REMOVED***
 
 ***REMOVED***/ A SwiftUI version of an ARCoachingOverlayView view.
-struct ARCoachingOverlay: UIViewRepresentable {
+struct ARCoachingOverlay {
+***REMOVED******REMOVED***/ The closure to call when the coaching overlay view activates.
+***REMOVED***private(set) var onCoachingOverlayActivateAction: ((ARCoachingOverlayView) -> Void)?
+***REMOVED******REMOVED***/ The closure to call when the coaching overlay view deactivates.
+***REMOVED***private(set) var onCoachingOverlayDeactivateAction: ((ARCoachingOverlayView) -> Void)?
+***REMOVED******REMOVED***/ The closure to call when the user taps the coaching overlay view's Start Over button.
+***REMOVED***private(set) var onCoachingOverlayRequestSessionResetAction: ((ARCoachingOverlayView) -> Void)?
+***REMOVED***
 ***REMOVED******REMOVED***/ The data source for an AR session.
 ***REMOVED***var sessionProvider: ARSessionProviding?
 ***REMOVED******REMOVED***/ The goal for the coaching overlay.
@@ -33,6 +40,34 @@ struct ARCoachingOverlay: UIViewRepresentable {
 ***REMOVED******REMOVED***return view
 ***REMOVED***
 ***REMOVED***
+***REMOVED******REMOVED***/ Sets the closure to call when the coaching overlay view activates.
+***REMOVED***func onCoachingOverlayActivate(
+***REMOVED******REMOVED***perform action: @escaping (ARCoachingOverlayView) -> Void
+***REMOVED***) -> Self {
+***REMOVED******REMOVED***var view = self
+***REMOVED******REMOVED***view.onCoachingOverlayActivateAction = action
+***REMOVED******REMOVED***return view
+***REMOVED***
+***REMOVED***
+***REMOVED******REMOVED***/ Sets the closure to call when the coaching experience is completely deactivated.
+***REMOVED***func onCoachingOverlayDeactivate(
+***REMOVED******REMOVED***perform action: @escaping (ARCoachingOverlayView) -> Void
+***REMOVED***) -> Self {
+***REMOVED******REMOVED***var view = self
+***REMOVED******REMOVED***view.onCoachingOverlayDeactivateAction = action
+***REMOVED******REMOVED***return view
+***REMOVED***
+***REMOVED***
+***REMOVED******REMOVED***/ Sets the closure to call when the user taps the coaching overlay view's Start Over button
+***REMOVED******REMOVED***/ while the session is relocalizing.
+***REMOVED***func onCoachingOverlayRequestSessionReset(
+***REMOVED******REMOVED***perform action: @escaping (ARCoachingOverlayView) -> Void
+***REMOVED***) -> Self {
+***REMOVED******REMOVED***var view = self
+***REMOVED******REMOVED***view.onCoachingOverlayRequestSessionResetAction = action
+***REMOVED******REMOVED***return view
+***REMOVED***
+***REMOVED***
 ***REMOVED******REMOVED***/ Sets the AR session data source for the coaching overlay.
 ***REMOVED******REMOVED***/ - Parameter sessionProvider: The AR session data source.
 ***REMOVED******REMOVED***/ - Returns: The `ARCoachingOverlay`.
@@ -42,6 +77,8 @@ struct ARCoachingOverlay: UIViewRepresentable {
 ***REMOVED******REMOVED***return view
 ***REMOVED***
 ***REMOVED***
+
+extension ARCoachingOverlay: UIViewRepresentable {
 ***REMOVED***func makeUIView(context: Context) -> ARCoachingOverlayView {
 ***REMOVED******REMOVED***let view = ARCoachingOverlayView()
 ***REMOVED******REMOVED***view.delegate = context.coordinator
@@ -53,11 +90,32 @@ struct ARCoachingOverlay: UIViewRepresentable {
 ***REMOVED******REMOVED***uiView.sessionProvider = sessionProvider
 ***REMOVED******REMOVED***uiView.goal = goal
 ***REMOVED******REMOVED***uiView.setActive(active, animated: true)
+***REMOVED******REMOVED***context.coordinator.onCoachingOverlayActivateAction = onCoachingOverlayActivateAction
+***REMOVED******REMOVED***context.coordinator.onCoachingOverlayDeactivateAction = onCoachingOverlayDeactivateAction
+***REMOVED******REMOVED***context.coordinator.onCoachingOverlayRequestSessionResetAction = onCoachingOverlayRequestSessionResetAction
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***func makeCoordinator() -> Coordinator {
 ***REMOVED******REMOVED***Coordinator()
 ***REMOVED***
 ***REMOVED***
-***REMOVED***class Coordinator: NSObject, ARCoachingOverlayViewDelegate {***REMOVED***
+
+extension ARCoachingOverlay {
+***REMOVED***class Coordinator: NSObject, ARCoachingOverlayViewDelegate {
+***REMOVED******REMOVED***var onCoachingOverlayActivateAction: ((ARCoachingOverlayView) -> Void)?
+***REMOVED******REMOVED***var onCoachingOverlayDeactivateAction: ((ARCoachingOverlayView) -> Void)?
+***REMOVED******REMOVED***var onCoachingOverlayRequestSessionResetAction: ((ARCoachingOverlayView) -> Void)?
+***REMOVED******REMOVED***
+***REMOVED******REMOVED***func coachingOverlayViewWillActivate(_ coachingOverlayView: ARCoachingOverlayView) {
+***REMOVED******REMOVED******REMOVED***onCoachingOverlayActivateAction?(coachingOverlayView)
+***REMOVED***
+***REMOVED******REMOVED***
+***REMOVED******REMOVED***func coachingOverlayViewDidDeactivate(_ coachingOverlayView: ARCoachingOverlayView) {
+***REMOVED******REMOVED******REMOVED***onCoachingOverlayDeactivateAction?(coachingOverlayView)
+***REMOVED***
+***REMOVED******REMOVED***
+***REMOVED******REMOVED***func coachingOverlayViewDidRequestSessionReset(_ coachingOverlayView: ARCoachingOverlayView) {
+***REMOVED******REMOVED******REMOVED***onCoachingOverlayRequestSessionResetAction?(coachingOverlayView)
+***REMOVED***
+***REMOVED***
 ***REMOVED***
