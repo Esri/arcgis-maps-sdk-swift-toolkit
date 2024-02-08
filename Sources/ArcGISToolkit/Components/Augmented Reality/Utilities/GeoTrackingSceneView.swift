c84ea7c1718f29df8fc7304dfbcaf0b6ab7e6be6
Ting Chen <tchen@esri.com>
2024-02-07 17:28:28 -0800

    Add separated logics.

--- Sources/ArcGISToolkit/Components/Augmented Reality/Utilities/GeoTrackingSceneView.swift
+++ Sources/ArcGISToolkit/Components/Augmented Reality/Utilities/GeoTrackingSceneView.swift
@@ -16,18 +16,16 @@ import ARKit
 import SwiftUI
 import ArcGIS
 
-/// A scene view that provides an augmented reality world scale experience using world-tracking.
-struct WorldTrackingSceneView: View {
+/// A scene view that provides an augmented reality world scale experience using geo-tracking.
+public struct GeoTrackingSceneView: View {
     /// The proxy for the ARSwiftUIView.
     @State private var arViewProxy = ARSwiftUIViewProxy()
     /// The camera controller that will be set on the scene view.
     @State private var cameraController: TransformationMatrixCameraController
-    /// A Boolean value that indicates if the user is calibrating.
-    private let isCalibrating: Bool
-    /// The current interface orientation.
-    @State private var interfaceOrientation: InterfaceOrientation?
     /// The location datasource that is used to access the device location.
     private let locationDataSource: LocationDataSource
+    /// The current interface orientation.
+    @State private var interfaceOrientation: InterfaceOrientation?
     /// A Boolean value indicating if the camera was initially set.
     @Binding var initialCameraIsSet: Bool
     /// A Boolean value that indicates whether the coaching overlay view is active.
@@ -36,33 +34,30 @@ struct WorldTrackingSceneView: View {
     @State private var currentCamera: Camera?
     /// The closure that builds the scene view.
     private let sceneViewBuilder: (SceneViewProxy) -> SceneView
-    /// The configuration for the AR session.
-    private let configuration: ARConfiguration
-    /// The timestamp of the last received location.
-    @State private var lastLocationTimestamp: Date?
+    /// The geo-tracking configuration for the AR session.
+    private let configuration = ARGeoTrackingConfiguration()
     /// The current device location.
     @State private var currentLocation: Location?
     /// The current device heading.
     @State private var currentHeading: Double?
-    /// The valid accuracy threshold for a location in meters.
-    private let validAccuracyThreshold = 0.0
-    /// The distance threshold in meters between camera and device location to reset
-    /// world-tracking session.
-    private let distanceThreshold = 2.0
-    /// Projected point from the spatial reference of the location data source's to the scene view's.
-    private var currentPosition: Point? {
-        guard let currentLocation,
-              let currentCamera,
-              let spatialReference = currentCamera.location.spatialReference,
-              let position = GeometryEngine.project(currentLocation.position, into: spatialReference)
-        else { return nil }
-        return position
-    }
+    
+    /// A Boolean value that indicates if the user is calibrating.
+    private let isCalibrating: Bool
     
     /// The view model for the calibration view.
     @ObservedObject private var calibrationViewModel: WorldScaleSceneView.CalibrationViewModel
     
-    init(
+    /// Creates a world scale geo-tracking scene view.
+    /// - Parameters:
+    ///   - locationDataSource: The location datasource used to acquire the device's location.
+    ///   - clippingDistance: Determines the clipping distance in meters around the camera. A value
+    ///   of `nil` means that no data will be clipped.
+    ///   - sceneView: A closure that builds the scene view to be overlayed on top of the
+    ///   augmented reality video feed.
+    /// - Remark: The provided scene view will have certain properties overridden in order to
+    /// be effectively viewed in augmented reality. Properties such as the camera controller,
+    /// and view drawing mode.
+    public init(
         initialCameraIsSet: Binding<Bool>,
         isCalibrating: Bool,
         locationDataSource: LocationDataSource,
@@ -81,15 +76,9 @@ struct WorldTrackingSceneView: View {
         cameraController.translationFactor = 1
         cameraController.clippingDistance = clippingDistance
         _cameraController = .init(initialValue: cameraController)
-        
-        let worldTrackingConfiguration = ARWorldTrackingConfiguration()
-        // Set world alignment to `gravityAndHeading` so the world-tracking configuration uses
-        // geographic location from the device. Geo-tracking uses it by default.
-        worldTrackingConfiguration.worldAlignment = .gravityAndHeading
-        configuration = worldTrackingConfiguration
     }
     
-    var body: some View {
+    public var body: some View {
         SceneViewReader { sceneViewProxy in
             ZStack {
                 ARSwiftUIView(proxy: arViewProxy)
@@ -106,6 +95,28 @@ struct WorldTrackingSceneView: View {
                             for: frame,
                             orientation: interfaceOrientation
                         )
+                    }
+                    .onDidChangeGeoTrackingStatus { session, status in
+                        // This modifier will only be called when using geo-tracking.
+                        switch status.state {
+                        case .notAvailable, .initializing, .localizing:
+                            initialCameraIsSet = false
+                        case .localized:
+                            // Update the camera controller every time ge-tracking is localized,
+                            // to ensure the best experience.
+                            if !initialCameraIsSet, let currentLocation, let currentHeading {
+                                // Set the initial heading of scene view camera based on location
+                                // and heading. Geo-tracking requires 90 degrees rotation.
+                                updateCameraController(
+                                    location: currentLocation,
+                                    heading: currentHeading + 90,
+                                    altitude: currentLocation.position.z ?? 0
+                                )
+                                initialCameraIsSet = true
+                            }
+                        @unknown default:
+                            fatalError("Unknown ARGeoTrackingStatus.State")
+                        }
                     }
                 
                 if initialCameraIsSet {
@@ -131,9 +142,8 @@ struct WorldTrackingSceneView: View {
                         coachingOverlayIsActive = false
                     }
                     .onCoachingOverlayRequestSessionReset { _ in
-                        if let currentLocation {
-                            updateWorldTrackingSceneView(for: currentLocation)
-                        }
+                        // Reset the AR session to provide the best tracking performance.
+                        arViewProxy.session.run(configuration, options: .resetTracking)
                     }
                     .allowsHitTesting(false)
             }
@@ -147,10 +157,7 @@ struct WorldTrackingSceneView: View {
         }
         .task {
             for await location in locationDataSource.locations {
-                lastLocationTimestamp = location.timestamp
                 currentLocation = location
-                // Call the method to check if world tracking session needs to be updated.
-                updateWorldTrackingSceneView(for: location)
             }
         }
         .task {
@@ -171,79 +178,26 @@ struct WorldTrackingSceneView: View {
         }
     }
     
-    /// Updates the scene view's camera controller with a new location coming from the
-    /// location data source and resets the AR session when using world-tracking configuration.
-    /// - Parameter location: The location data source location.
-    private func updateWorldTrackingSceneView(for location: Location) {
-        // Do not update the scene view when the coaching overlay is in place.
-        guard !coachingOverlayIsActive else { return }
+    /// Updates the scene view's camera controller with location and heading.
+    /// - Parameters:
+    ///   - location: The location for the camera.
+    ///   - heading: The heading for the camera.
+    ///   - altitude: The altitude for the camera.
+    private func updateCameraController(location: Location, heading: Double, altitude: Double) {
+        // Add some of the vertical accuracy to the z value of the position, that way if the
+        // GPS location is not accurate, it won't end up below the earth's surface.
+        let adjustedAltitude = altitude + location.verticalAccuracy
         
-        // Update if the location is more than 10 seconds old.
-        guard abs(lastLocationTimestamp?.timeIntervalSinceNow ?? 0) < 10 else { return }
-        
-        // Make sure that horizontal and vertical accuracy are valid.
-        guard location.horizontalAccuracy > validAccuracyThreshold,
-              location.verticalAccuracy > validAccuracyThreshold else { return }
-        
-        // Make sure we need to update the camera based on distance deviation.
-        guard !initialCameraIsSet || shouldUpdateCamera(for: location) else { return }
-        
-        let altitude = (location.position.z ?? 0) + location.verticalAccuracy
-        
-        if !initialCameraIsSet {
-            cameraController.originCamera = Camera(
-                latitude: location.position.y,
-                longitude: location.position.x,
-                altitude: altitude,
-                heading: 0,
-                pitch: 90,
-                roll: 0
-            )
-        } else {
-            // Ignore location updates when calibrating heading and elevation.
-            guard !isCalibrating else { return }
-            cameraController.originCamera = Camera(
-                latitude: location.position.y,
-                longitude: location.position.x,
-                altitude: altitude + calibrationViewModel.totalElevationCorrection,
-                heading: calibrationViewModel.totalHeadingCorrection,
-                pitch: 90,
-                roll: 0
-            )
-        }
+        cameraController.originCamera = Camera(
+            latitude: location.position.y,
+            longitude: location.position.x,
+            altitude: adjustedAltitude,
+            heading: heading,
+            pitch: 90,
+            roll: 0
+        )
         
         // We have to do this or the error gets bigger and bigger.
         cameraController.transformationMatrix = .identity
-        
-        arViewProxy.session.run(configuration, options: .resetTracking)
-        
-        // If initial camera is not set, then we set it the flag here to true
-        // and set the status text to empty.
-        if !initialCameraIsSet {
-            initialCameraIsSet = true
-        }
-    }
-    
-    /// Returns a Boolean value indicating if the camera should be updated for a location
-    /// coming in from the location data source based on current camera deviation.
-    /// - Parameter location: The location data source location.
-    /// - Returns: A Boolean value indicating if the camera should be updated.
-    func shouldUpdateCamera(for location: Location) -> Bool {
-        guard let currentCamera, let currentPosition else { return false }
-        
-        // Measure the distance between the location datasource's reported location
-        // and the camera's current location.
-        guard let result = GeometryEngine.geodeticDistance(
-            from: currentCamera.location,
-            to: currentPosition,
-            distanceUnit: .meters,
-            azimuthUnit: nil,
-            curveType: .geodesic
-        ) else {
-            return false
-        }
-        
-        // If the location becomes off by over a certain threshold, then update the camera location.
-        return result.distance.value > distanceThreshold ? true : false
     }
 }
--- Sources/ArcGISToolkit/Components/Augmented Reality/WorldScaleSceneView.swift
+++ Sources/ArcGISToolkit/Components/Augmented Reality/WorldScaleSceneView.swift
@@ -1,14 +1,250 @@
+// Copyright 2023 Esri
 //
-//  SwiftUIView.swift
-//  
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Created by Ting Chen on 2/7/24.
+//   https://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
+import ARKit
 import SwiftUI
+import ArcGIS
 
-struct WorldScaleSceneView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+/// A scene view that provides an augmented reality world scale experience.
+public struct WorldScaleSceneView: View {
+    /// Determines the alignment of the calibration view.
+    var calibrationViewAlignment: Alignment = .bottom
+    /// A Boolean value that indicates whether the calibration view is hidden.
+    var calibrationViewIsHidden = false
+    
+    /// The closure that builds the scene view.
+    private let sceneViewBuilder: (SceneViewProxy) -> SceneView
+    
+    private let trackingMode: TrackingMode
+    
+    private let clippingDistance: Double?
+    
+    /// The view model for the calibration view.
+    @StateObject private var calibrationViewModel = CalibrationViewModel()
+    /// A Boolean value that indicates if the user is calibrating.
+    @State private var isCalibrating = false
+    /// The location datasource that is used to access the device location.
+    @State private var locationDataSource = SystemLocationDataSource()
+    
+    /// The current device location.
+    @State private var currentLocation: Location?
+    
+    @State private var geoTrackingIsAvailable = true
+    
+    @State private var initialCameraIsSet = false
+    
+    @State private var error: Error?
+    
+    public init(
+        clippingDistance: Double? = nil,
+        trackingMode: TrackingMode,
+        sceneViewBuilder: @escaping (SceneViewProxy) -> SceneView
+    ) {
+        self.clippingDistance = clippingDistance
+        self.trackingMode = trackingMode
+        self.sceneViewBuilder = sceneViewBuilder
+    }
+    
+    public var body: some View {
+        Group {
+            switch trackingMode {
+            case .automatic:
+                if geoTrackingIsAvailable {
+                    GeoTrackingSceneView(
+                        initialCameraIsSet: $initialCameraIsSet,
+                        isCalibrating: isCalibrating,
+                        locationDataSource: locationDataSource,
+                        calibrationViewModel: calibrationViewModel,
+                        clippingDistance: clippingDistance,
+                        sceneView: sceneViewBuilder
+                    )
+                } else {
+                    WorldTrackingSceneView(
+                        initialCameraIsSet: $initialCameraIsSet,
+                        isCalibrating: isCalibrating,
+                        locationDataSource: locationDataSource,
+                        calibrationViewModel: calibrationViewModel,
+                        clippingDistance: clippingDistance,
+                        sceneView: sceneViewBuilder
+                    )
+                }
+            case .geoTracking:
+                GeoTrackingSceneView(
+                    initialCameraIsSet: $initialCameraIsSet,
+                    isCalibrating: isCalibrating,
+                    locationDataSource: locationDataSource,
+                    calibrationViewModel: calibrationViewModel,
+                    clippingDistance: clippingDistance,
+                    sceneView: sceneViewBuilder
+                )
+            case .worldTracking:
+                WorldTrackingSceneView(
+                    initialCameraIsSet: $initialCameraIsSet,
+                    isCalibrating: isCalibrating,
+                    locationDataSource: locationDataSource,
+                    calibrationViewModel: calibrationViewModel,
+                    clippingDistance: clippingDistance,
+                    sceneView: sceneViewBuilder
+                )
+            }
+        }
+        .onAppear {
+        }
+        .onDisappear {
+            Task { await locationDataSource.stop() }
+        }
+        .task {
+            // Start the location data source when the view appears.
+            
+            // Request when-in-use location authorization.
+            // The view utilizes a location datasource and it will not start until authorized.
+            let locationManager = CLLocationManager()
+            if locationManager.authorizationStatus == .notDetermined {
+                locationManager.requestWhenInUseAuthorization()
+            }
+            if !checkDeviceCapabilities(locationManager) {
+                print("Device doesn't support full accuracy location.")
+            }
+            
+            do {
+                try await locationDataSource.start()
+            } catch {
+                self.error = error
+            }
+            for await location in locationDataSource.locations {
+                currentLocation = location
+            }
+        }
+        .task {
+            do {
+                let isAvailable = try await checkGeoTrackingAvailability()
+                geoTrackingIsAvailable = isAvailable
+            } catch {
+                self.error = error
+            }
+        }
+        .overlay(alignment: calibrationViewAlignment) {
+            if !calibrationViewIsHidden {
+                if !isCalibrating {
+                    Button {
+                        withAnimation {
+                            isCalibrating = true
+                        }
+                    } label: {
+                        Text("Calibrate")
+                            .padding()
+                    }
+                    .background(.regularMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .disabled(!initialCameraIsSet)
+                    .padding()
+                    .padding(.vertical)
+                }
+            }
+        }
+        .overlay(alignment: .bottom) {
+            if isCalibrating {
+                CalibrationView(viewModel: calibrationViewModel, isPresented: $isCalibrating)
+                    .padding(.bottom)
+            }
+        }
+        .overlay(alignment: .top) {
+            accuracyView
+        }
+    }
+    
+    /// A view that displays the horizontal and vertical accuracy of the current location datasource location.
+    @ViewBuilder
+    var accuracyView: some View {
+        if let currentLocation {
+            VStack {
+                Text("H. Accuracy: \(currentLocation.horizontalAccuracy.formatted(.number.precision(.fractionLength(2))))")
+                Text("V. Accuracy: \(currentLocation.verticalAccuracy.formatted(.number.precision(.fractionLength(2))))")
+            }
+            .multilineTextAlignment(.center)
+            .padding(8)
+            .frame(maxWidth: .infinity, alignment: .center)
+            .background(.regularMaterial)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .padding([.horizontal, .top])
+        }
+    }
+    
+    /// Sets the visibility of the calibration view for the AR experience.
+    /// - Parameter hidden: A Boolean value that indicates whether to hide the
+    ///  calibration view.
+    public func calibrationViewHidden(_ hidden: Bool) -> Self {
+        var view = self
+        view.calibrationViewIsHidden = hidden
+        return view
+    }
+    
+    /// Sets the alignment of the calibration view.
+    /// - Parameter alignment: The alignment for the calibration view.
+    public func calibrationViewAlignment(_ alignment: Alignment) -> Self {
+        var view = self
+        view.calibrationViewAlignment = alignment
+        return view
+    }
+    
+    private func checkDeviceCapabilities(_ locationManager: CLLocationManager) -> Bool {
+        let headingAvailable = CLLocationManager.headingAvailable()
+        let fullAccuracy: Bool
+        switch locationManager.accuracyAuthorization {
+        case .fullAccuracy:
+            // World scale AR experience must use full accuracy of device location.
+            fullAccuracy = true
+        default:
+            fullAccuracy = false
+        }
+        return headingAvailable && fullAccuracy
+    }
+    
+    private func checkGeoTrackingAvailability() async throws -> Bool {
+        if !ARGeoTrackingConfiguration.isSupported {
+            return false
+        }
+        return try await ARGeoTrackingConfiguration.checkAvailability()
+    }
+}
+
+public extension WorldScaleSceneView {
+    /// The type of tracking configuration used by the view.
+    enum TrackingMode {
+        /// If geo-tracking is unavailable, fall back to world-tracking.
+        case automatic
+        /// Geo-tracking.
+        case geoTracking
+        /// World-tracking.
+        case worldTracking
+    }
+}
+
+private extension ARGeoTrackingConfiguration {
+    /// Determines the availability of geo tracking at the current location.
+    /// - Returns: A Boolean that indicates whether geo-tracking is available at the current
+    /// location or not. If not, an error will be thrown that indicates why geo tracking is
+    /// not available at the current location.
+    static func checkAvailability() async throws -> Bool {
+        return try await withUnsafeThrowingContinuation { (continuation: UnsafeContinuation<Bool, Error>) in
+            self.checkAvailability { isAvailable, error in
+                if let error = error {
+                    continuation.resume(throwing: error)
+                } else {
+                    continuation.resume(returning: isAvailable)
+                }
+            }
+        }
     }
 }
