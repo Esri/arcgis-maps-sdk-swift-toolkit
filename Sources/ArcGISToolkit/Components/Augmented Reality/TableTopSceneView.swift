8331999160c41422308f39a09a04d4326dc4d1b1
Ting Chen <tchen@esri.com>
2024-02-22 10:03:15 -0800

    Move the tap gesture availability wrapper to extension.

--- Sources/ArcGISToolkit/Components/Augmented Reality/TableTopSceneView.swift
+++ Sources/ArcGISToolkit/Components/Augmented Reality/TableTopSceneView.swift
@@ -227,29 +227,6 @@ public struct TableTopSceneView: View {
 ***REMOVED***
 ***REMOVED***
 
-private extension View {
-***REMOVED******REMOVED***/ Sets a closure to perform when a single tap occurs on the view.
-***REMOVED******REMOVED***/ - Parameters:
-***REMOVED******REMOVED***/   - action: The closure to perform upon single tap.
-***REMOVED******REMOVED***/   - screenPoint: The location of the tap in the view's coordinate space.
-***REMOVED***func onSingleTapGesture(perform action: @escaping (_ screenPoint: CGPoint) -> Void) -> some View {
-***REMOVED******REMOVED***if #available(iOS 16.0, *) {
-***REMOVED******REMOVED******REMOVED***return self.onTapGesture { screenPoint in
-***REMOVED******REMOVED******REMOVED******REMOVED***action(screenPoint)
-***REMOVED******REMOVED***
-***REMOVED*** else {
-***REMOVED******REMOVED******REMOVED******REMOVED*** Use a drag gesture with a minimum dragging distance of zero so the
-***REMOVED******REMOVED******REMOVED******REMOVED*** gesture is recognized with a single tap.
-***REMOVED******REMOVED******REMOVED***return self.gesture(
-***REMOVED******REMOVED******REMOVED******REMOVED***DragGesture(minimumDistance: 0)
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.onEnded { dragAttributes in
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***action(dragAttributes.location)
-***REMOVED******REMOVED******REMOVED******REMOVED***
-***REMOVED******REMOVED******REMOVED***)
-***REMOVED***
-***REMOVED***
-***REMOVED***
-
 private extension SceneViewProxy {
 ***REMOVED******REMOVED***/ Sets the initial transformation used to offset the originCamera.  The initial transformation is based on an AR point determined
 ***REMOVED******REMOVED***/ via existing plane hit detection from `screenPoint`.  If an AR point cannot be determined, this method will return `false`.
