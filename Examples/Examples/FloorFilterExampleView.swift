86bb7c580f7db1f3960c7b1ceca365af775855e9
David Feinzimer <dfeinzimer@esri.com>
2024-03-01 15:29:06 -0800

    Make Floor Filter's alignment a let property

--- Examples/Examples/FloorFilterExampleView.swift
+++ Examples/Examples/FloorFilterExampleView.swift
@@ -18,7 +18,7 @@ import ArcGIS
 
 struct FloorFilterExampleView: View {
     /// Determines the arrangement of the inner `FloorFilter` UI components.
-    private var floorFilterAlignment: Alignment { .bottomLeading }
+    let floorFilterAlignment = Alignment.bottomLeading
     
     /// The height of the map view's attribution bar.
     @State private var attributionBarHeight = 0.0
--- Test Runner/Test Runner/TestViews/FloorFilterTestView.swift
+++ Test Runner/Test Runner/TestViews/FloorFilterTestView.swift
@@ -18,7 +18,7 @@ import SwiftUI
 
 struct FloorFilterTestView: View {
     /// Determines the arrangement of the inner `FloorFilter` UI components.
-    private var floorFilterAlignment: Alignment { .bottomLeading }
+    let floorFilterAlignment = Alignment.bottomLeading
     
     /// The height of the map view's attribution bar.
     @State private var attributionBarHeight = 0.0
