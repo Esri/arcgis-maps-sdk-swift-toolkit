1f5755058b91c5236b037c761450209ab1d2b367
Destiny Hochhalter <dhochhalterruiz@esri.com>
2024-03-18 16:56:19 -0700

    Update WorldScaleExampleView.swift

--- Examples/Examples/WorldScaleExampleView.swift
+++ Examples/Examples/WorldScaleExampleView.swift
@@ -63,7 +63,7 @@ struct WorldScaleExampleView: View {
         .onSingleTapGesture { _, scenePoint in
             graphicsOverlay.addGraphic(Graphic(geometry: scenePoint))
         }
-        .ignoresSafeArea(edges: .bottom)
+        .ignoresSafeArea(edges: [.horizontal, .bottom])
         .task {
             // Request when-in-use location authorization.
             // This is necessary for 2 reasons:
