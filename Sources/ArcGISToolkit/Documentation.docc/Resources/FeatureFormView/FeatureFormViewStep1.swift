2da3981b32dd019a24c656f251ad0aefdf3b20a7
Mark Dostal <mdostal@esri.com>
2024-03-28 11:17:52 -0500

    Add tutorial steps

---
+++ Sources/ArcGISToolkit/Documentation.docc/Resources/FeatureFormView/FeatureFormViewStep1.swift
@@ -0,0 +1,17 @@
+***REMOVED***
+***REMOVED***
+***REMOVED***Toolkit
+
+struct FeatureFormExampleView: View {
+***REMOVED***static func makeMap() -> Map {
+***REMOVED******REMOVED***let portalItem = PortalItem(
+***REMOVED******REMOVED******REMOVED***portal: .arcGISOnline(connection: .anonymous),
+***REMOVED******REMOVED******REMOVED***id: Item.ID("9f3a674e998f461580006e626611f9ad")!
+***REMOVED******REMOVED***)
+***REMOVED******REMOVED***return Map(item: portalItem)
+***REMOVED***
+***REMOVED***
+***REMOVED***@StateObject private var dataModel = MapDataModel(
+***REMOVED******REMOVED***map: makeMap()
+***REMOVED***)
+***REMOVED***
