4db5cc5365488501ceebd63ef833963d5174df96
Mark Dostal <mdostal@esri.com>
2024-03-29 10:38:12 -0500

    Remove Tutorial [Coming Soon], from Doc.

--- Sources/ArcGISToolkit/Components/FeatureFormView/FeatureFormView.swift
+++ Sources/ArcGISToolkit/Components/FeatureFormView/FeatureFormView.swift
@@ -53,7 +53,7 @@
 ***REMOVED***/ To see it in action, try out the [Examples](https:***REMOVED***github.com/Esri/arcgis-maps-sdk-swift-toolkit/tree/Forms/Examples/Examples)
 ***REMOVED***/ and refer to
 ***REMOVED***/ [FeatureFormExampleView.swift](https:***REMOVED***github.com/Esri/arcgis-maps-sdk-swift-toolkit/blob/Forms/Examples/Examples/FeatureFormExampleView.swift)
-***REMOVED***/ in the project. To learn more about using the `FeatureFormView` see the [FeatureFormView Tutorial](https:***REMOVED***developers.arcgis.com/swift/toolkit-api-reference/tutorials/arcgistoolkit/featureformviewtutorial) [Coming Soon].
+***REMOVED***/ in the project. To learn more about using the `FeatureFormView` see the [FeatureFormView Tutorial](https:***REMOVED***developers.arcgis.com/swift/toolkit-api-reference/tutorials/arcgistoolkit/featureformviewtutorial).
 ***REMOVED***/ 
 ***REMOVED***/ - Since: 200.4
 public struct FeatureFormView: View {
