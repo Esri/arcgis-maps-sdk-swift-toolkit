f9c437bb10da63228a3296b6dc0d9af05ff30bad
David Feinzimer <dfeinzimer@esri.com>
2024-03-29 14:03:48 -0700

    Introduce `FullScreenTextInput` for multiline

--- Sources/ArcGISToolkit/Components/FeatureFormView/Subviews/Inputs/TextInput.swift
+++ Sources/ArcGISToolkit/Components/FeatureFormView/Subviews/Inputs/TextInput.swift
@@ -26,6 +26,9 @@ struct TextInput: View {
 ***REMOVED******REMOVED***/ The formatted version of the element's current value.
 ***REMOVED***@State private var formattedValue = ""
 ***REMOVED***
+***REMOVED******REMOVED***/ A Boolean value indicating whether the full screen text input is presented.
+***REMOVED***@State private var fullScreenTextInputIsPresented = false
+***REMOVED***
 ***REMOVED******REMOVED***/ A Boolean value indicating whether placeholder text is shown, thereby indicating the
 ***REMOVED******REMOVED***/ presence of a value.
 ***REMOVED******REMOVED***/
@@ -81,6 +84,12 @@ struct TextInput: View {
 ***REMOVED******REMOVED******REMOVED******REMOVED***element.convertAndUpdateValue(text)
 ***REMOVED******REMOVED******REMOVED******REMOVED***model.evaluateExpressions()
 ***REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED***.onTapGesture {
+***REMOVED******REMOVED******REMOVED******REMOVED***if element.isMultiline {
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***fullScreenTextInputIsPresented = true
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***model.focusedElement = element
+***REMOVED******REMOVED******REMOVED***
+***REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED***.onValueChange(of: element) { newValue, newFormattedValue in
 ***REMOVED******REMOVED******REMOVED******REMOVED***formattedValue = newFormattedValue
 ***REMOVED******REMOVED******REMOVED******REMOVED***updateText()
@@ -109,6 +118,14 @@ private extension TextInput {
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***prompt: Text(element.hint).foregroundColor(.secondary),
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***axis: element.isMultiline ? .vertical : .horizontal
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***)
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.disabled(element.isMultiline)
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.sheet(isPresented: $fullScreenTextInputIsPresented) {
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***FullScreenTextInput(text: $text, element: element)
+#if targetEnvironment(macCatalyst)
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.environmentObject(model)
+#endif
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.padding()
+***REMOVED******REMOVED******REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED*** else if element.isMultiline {
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***TextEditor(text: $text)
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.foregroundColor(isPlaceholder ? .secondary : .primary)
@@ -176,6 +193,50 @@ private extension TextInput {
 ***REMOVED******REMOVED***let text = formattedValue
 ***REMOVED******REMOVED***isPlaceholder = text.isEmpty && !iOS16MinimumIsSupported
 ***REMOVED******REMOVED***self.text = isPlaceholder ? element.hint : text
+***REMOVED***
+***REMOVED***
+
+@available(iOS 16.0, *)
+private extension TextInput {
+***REMOVED******REMOVED***/ A view for displaying a multiline text input outside the body of the feature form view.
+***REMOVED******REMOVED***/
+***REMOVED******REMOVED***/ By moving outside of the feature form view's scroll view we let the text field naturally manage
+***REMOVED******REMOVED***/ keeping the input caret visible.
+***REMOVED***struct FullScreenTextInput: View {
+***REMOVED******REMOVED******REMOVED***/ The current text value.
+***REMOVED******REMOVED***@Binding var text: String
+***REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED***/ An action that dismisses the current presentation.
+***REMOVED******REMOVED***@Environment(\.dismiss) private var dismiss
+***REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED***/ A Boolean value indicating whether the text field is focused.
+***REMOVED******REMOVED***@FocusState private var textFieldIsFocused: Bool
+***REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED***/ The element the input belongs to.
+***REMOVED******REMOVED***let element: FieldFormElement
+***REMOVED******REMOVED***
+***REMOVED******REMOVED***var body: some View {
+***REMOVED******REMOVED******REMOVED***InputHeader(element: element)
+***REMOVED******REMOVED******REMOVED***TextField(
+***REMOVED******REMOVED******REMOVED******REMOVED***element.label,
+***REMOVED******REMOVED******REMOVED******REMOVED***text: $text,
+***REMOVED******REMOVED******REMOVED******REMOVED***prompt: Text(element.hint).foregroundColor(.secondary),
+***REMOVED******REMOVED******REMOVED******REMOVED***axis: .vertical
+***REMOVED******REMOVED******REMOVED***)
+***REMOVED******REMOVED******REMOVED***.focused($textFieldIsFocused, equals: true)
+***REMOVED******REMOVED******REMOVED***.onAppear {
+***REMOVED******REMOVED******REMOVED******REMOVED***textFieldIsFocused = true
+***REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED***Spacer()
+***REMOVED******REMOVED******REMOVED***HStack {
+***REMOVED******REMOVED******REMOVED******REMOVED***InputFooter(element: element)
+***REMOVED******REMOVED******REMOVED******REMOVED***Button("Done") {
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***dismiss()
+***REMOVED******REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED******REMOVED***.buttonStyle(.plain)
+***REMOVED******REMOVED******REMOVED******REMOVED***.foregroundColor(.accentColor)
+***REMOVED******REMOVED***
+***REMOVED***
 ***REMOVED***
 ***REMOVED***
 
