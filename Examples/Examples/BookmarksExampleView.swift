f8e8b23616b7a84a5c5a636baa5d6ca9d1da02bc
David Feinzimer <dfeinzimer@esri.com>
2024-04-10 11:12:49 -0700

    Doc revisions

--- Examples/Examples/BookmarksExampleView.swift
+++ Examples/Examples/BookmarksExampleView.swift
@@ -17,37 +17,34 @@ import ArcGISToolkit
 import SwiftUI
 
 struct BookmarksExampleView: View {
+    /// Indicates whether the `Bookmarks` component is presented or not.
+    @State private var bookmarksIsPresented = false
+    
     /// The `Map` with predefined bookmarks.
     @State private var map = Map(url: URL(string: "https://www.arcgis.com/home/item.html?id=16f1b8ba37b44dc3884afc8d5f454dd2")!)!
     
     /// The selected bookmark.
     @State private var selection: Bookmark?
     
-    /// Indicates if the `Bookmarks` component is shown or not.
-    /// - Remark: This allows a developer to control when the `Bookmarks` component is
-    /// shown/hidden, whether that be in a group of options or a standalone button.
-    @State private var showingBookmarks = false
-    
     var body: some View {
         MapViewReader { mapViewProxy in
             MapView(map: map)
+                .popover(isPresented: $bookmarksIsPresented) {
+                    Bookmarks(
+                        isPresented: $bookmarksIsPresented,
+                        geoModel: map,
+                        selection: $selection,
+                        geoViewProxy: mapViewProxy
+                    )
+                }
                 .toolbar {
                     ToolbarItem(placement: .primaryAction) {
                         Button {
-                            showingBookmarks.toggle()
+                            bookmarksIsPresented.toggle()
                         } label: {
                             Label(
                                 "Show Bookmarks",
                                 systemImage: "bookmark"
-                            )
-                        }
-                        .popover(isPresented: $showingBookmarks) {
-                            // Display the `Bookmarks` component with the list of bookmarks in a map.
-                            Bookmarks(
-                                isPresented: $showingBookmarks,
-                                geoModel: map,
-                                selection: $selection,
-                                geoViewProxy: mapViewProxy
                             )
                         }
                     }
