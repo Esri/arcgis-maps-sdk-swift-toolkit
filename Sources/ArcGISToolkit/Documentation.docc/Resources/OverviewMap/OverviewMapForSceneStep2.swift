13ded09df2acb1454ed0b6a649a115c59e85948b
David Feinzimer <dfeinzimer@esri.com>
2024-04-12 15:50:32 -0700

    Remove `MapDataModel` & `SceneDataModel`

--- Sources/ArcGISToolkit/Documentation.docc/Resources/OverviewMap/OverviewMapForSceneStep2.swift
+++ Sources/ArcGISToolkit/Documentation.docc/Resources/OverviewMap/OverviewMapForSceneStep2.swift
@@ -3,14 +3,12 @@
 ***REMOVED***Toolkit
 
 struct OverviewMapForSceneView: View {
-***REMOVED***@StateObject private var dataModel = SceneDataModel(
-***REMOVED******REMOVED***scene: Scene(basemapStyle: .arcGISImagery)
-***REMOVED***)
+***REMOVED***@State private var scene = Scene(basemapStyle: .arcGISImagery)
 ***REMOVED***
 ***REMOVED***@State private var viewpoint: Viewpoint?
 ***REMOVED***
 ***REMOVED***var body: some View {
-***REMOVED******REMOVED***SceneView(scene: dataModel.scene)
+***REMOVED******REMOVED***SceneView(scene: scene)
 ***REMOVED******REMOVED******REMOVED***.onViewpointChanged(kind: .centerAndScale) { viewpoint = $0 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
