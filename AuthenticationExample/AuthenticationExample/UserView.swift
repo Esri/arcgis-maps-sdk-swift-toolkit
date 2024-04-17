b283a172c6b857d38dd5f0286dea79bc925a10de
David Feinzimer <dfeinzimer@esri.com>
2024-04-17 15:08:49 -0700

    Sort imports

--- AuthenticationExample/AuthenticationExample/UserView.swift
+++ AuthenticationExample/AuthenticationExample/UserView.swift
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import SwiftUI
 import ArcGIS
+import SwiftUI
 
 /// A view that displays details of a portal user.
 struct UserView: View {
