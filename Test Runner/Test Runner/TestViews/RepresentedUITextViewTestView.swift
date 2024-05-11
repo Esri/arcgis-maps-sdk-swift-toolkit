fbe7522855f6d8be5083aad60b68d0b61ed56d76
David Feinzimer <dfeinzimer@esri.com>
2024-05-11 00:05:32 -0700

    Create a test view for RepresentedUITextView

---
+++ Test Runner/Test Runner/TestViews/RepresentedUITextViewTestView.swift
@@ -0,0 +1,70 @@
+// Copyright 2023 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+@testable import ArcGISToolkit
+import SwiftUI
+
+struct RepresentedUITextViewTestView: View {
+    var body: some View {
+        List {
+            NavigationLink("TestInit", destination: TestInit())
+            NavigationLink("TestInitWithActions", destination: TestInitWithActions())
+        }
+    }
+}
+
+extension RepresentedUITextViewTestView {
+    struct TestInit: View {
+        @State private var text = "Hello, World!"
+        
+        var body: some View {
+            HStack(alignment: .center) {
+                Text("Bound Value:")
+                Text(text)
+                    .accessibilityIdentifier("Bound Value")
+            }
+            RepresentedUITextView(text: $text)
+        }
+    }
+    
+    struct TestInitWithActions: View {
+        @State private var text = "Hello, World!"
+        
+        @State private var endValue = ""
+        
+        @FocusState var isFocused
+        
+        var body: some View {
+            HStack(alignment: .center) {
+                Text("Bound Value:")
+                Text(text)
+                    .accessibilityIdentifier("Bound Value")
+            }
+            HStack(alignment: .center) {
+                Text("End Value:")
+                Text(endValue)
+                    .accessibilityIdentifier("End Value")
+            }
+            Button("End Editing") {
+                isFocused = false
+            }
+            RepresentedUITextView(initialText: text) { text in
+                self.text = text
+            } onTextViewDidEndEditing: { text in
+                endValue = text
+            }
+            .focused($isFocused)
+        }
+    }
+}
