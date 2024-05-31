2e682fcd980f0a4d92619458998dac4e2b79d501
Destiny Hochhalter <dhochhalterruiz@esri.com>
2024-05-31 11:37:02 -0700

    Fix imports

--- Sources/ArcGISToolkit/Components/OfflineMapAreasView/OfflineMapAreasView.MapViewModel.swift
+++ Sources/ArcGISToolkit/Components/OfflineMapAreasView/OfflineMapAreasView.MapViewModel.swift
@@ -12,8 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import SwiftUI
 import ArcGIS
+import Combine
+import Foundation
 
 public extension OfflineMapAreasView {
     /// The model class for the offline map areas view.
