6c1a3aa830a8733dca99a7c41a0377e02fc6eb67
David Feinzimer <dfeinzimer@esri.com>
2024-06-04 16:22:58 -0700

    Add `[FeatureEditResult].errors`

--- Examples/Examples/FeatureFormExampleView.swift
+++ Examples/Examples/FeatureFormExampleView.swift
@@ -375,3 +375,19 @@ private extension FeatureForm {
         feature.table?.layer as? FeatureLayer
     }
 }
+
+private extension Array where Element == FeatureEditResult {
+    /// Examines all feature edit results (and their inner attachment results) for any errors.
+    var errors: [Error] {
+        var errors = [Error]()
+        forEach { featureEditResult in
+            if let editResultError = featureEditResult.error { errors.append(editResultError) }
+            featureEditResult.attachmentResults.forEach { attachmentResult in
+                if let error = attachmentResult.error {
+                    errors.append(error)
+                }
+            }
+        }
+        return errors
+    }
+}
