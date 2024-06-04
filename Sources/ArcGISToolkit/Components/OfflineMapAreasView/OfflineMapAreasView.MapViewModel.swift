b563587c8d247219a22471cc7d3ad59323ccb5b9
Destiny Hochhalter <dhochhalterruiz@esri.com>
2024-06-04 09:39:58 -0700

    Address PR feedback

--- Sources/ArcGISToolkit/Components/OfflineMapAreasView/OfflineMapAreasView.MapViewModel.swift
+++ Sources/ArcGISToolkit/Components/OfflineMapAreasView/OfflineMapAreasView.MapViewModel.swift
@@ -39,7 +39,7 @@ public extension OfflineMapAreasView {
 ***REMOVED******REMOVED***let preplannedDirectory: URL
 ***REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED***/ The mobile map packages created from mmpk files in the documents directory.
-***REMOVED******REMOVED***@Published var mobileMapPackages = [MobileMapPackage]()
+***REMOVED******REMOVED***@Published private(set) var mobileMapPackages = [MobileMapPackage]()
 ***REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED***/ The preplanned offline map information.
 ***REMOVED******REMOVED***@Published private(set) var preplannedMapModels: Result<[PreplannedMapModel], Error>?
@@ -55,7 +55,7 @@ public extension OfflineMapAreasView {
 ***REMOVED***
 ***REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED***/ The job manager.
-***REMOVED******REMOVED***var jobManager = JobManager.shared
+***REMOVED******REMOVED***let jobManager = JobManager.shared
 ***REMOVED******REMOVED***
 ***REMOVED******REMOVED***init(map: Map) {
 ***REMOVED******REMOVED******REMOVED***onlineMap = map
@@ -65,7 +65,7 @@ public extension OfflineMapAreasView {
 ***REMOVED******REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED***offlineMapTask = OfflineMapTask(onlineMap: onlineMap)
 ***REMOVED******REMOVED******REMOVED***
-***REMOVED******REMOVED******REMOVED***if let portalItemID = map.item?.id?.description {
+***REMOVED******REMOVED******REMOVED***if let portalItemID = map.item?.id?.rawValue {
 ***REMOVED******REMOVED******REMOVED******REMOVED***FileManager.default.createDirectories(for: portalItemID)
 ***REMOVED******REMOVED******REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED******REMOVED***preplannedDirectory = FileManager.default.preplannedDirectory(portalItemID: portalItemID)
@@ -135,14 +135,13 @@ public extension OfflineMapAreasView {
 ***REMOVED******REMOVED******REMOVED***/   - fileExtension: The file extension to search for.
 ***REMOVED******REMOVED******REMOVED***/ - Returns: An array of file paths.
 ***REMOVED******REMOVED***func searchFiles(in directory: URL, with fileExtension: String) -> [URL] {
-***REMOVED******REMOVED******REMOVED***var files = [URL]()
-***REMOVED******REMOVED******REMOVED***let keys: [URLResourceKey] = [.isDirectoryKey]
-***REMOVED******REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED***guard let enumerator = FileManager.default.enumerator(
 ***REMOVED******REMOVED******REMOVED******REMOVED***at: directory,
-***REMOVED******REMOVED******REMOVED******REMOVED***includingPropertiesForKeys: keys,
+***REMOVED******REMOVED******REMOVED******REMOVED***includingPropertiesForKeys: [.isDirectoryKey],
 ***REMOVED******REMOVED******REMOVED******REMOVED***options: [.skipsHiddenFiles]
 ***REMOVED******REMOVED******REMOVED***) else { return [] ***REMOVED***
+***REMOVED******REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED***var files: [URL] = []
 ***REMOVED******REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED***for case let fileURL as URL in enumerator {
 ***REMOVED******REMOVED******REMOVED******REMOVED***if fileURL.pathExtension == fileExtension {
@@ -167,9 +166,7 @@ public extension OfflineMapAreasView.MapViewModel {
 private extension FileManager {
 ***REMOVED******REMOVED***/ The path to the documents folder.
 ***REMOVED***var documentsDirectory: URL {
-***REMOVED******REMOVED***URL(
-***REMOVED******REMOVED******REMOVED***fileURLWithPath: NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
-***REMOVED******REMOVED***)
+***REMOVED******REMOVED***URL.documentsDirectory
 ***REMOVED***
 ***REMOVED***
 ***REMOVED******REMOVED***/ The path to the offline map areas directory.
