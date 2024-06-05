a57049481fcdf3da697a5d524e1fb1b9271f5a9a
David Feinzimer <dfeinzimer@esri.com>
2024-06-05 14:45:47 -0700

    Tutorial: Update `finishEdits` to `finishEditing `

--- Sources/ArcGISToolkit/Documentation.docc/Resources/FeatureFormView/FeatureFormViewStep10.swift
+++ Sources/ArcGISToolkit/Documentation.docc/Resources/FeatureFormView/FeatureFormViewStep10.swift
@@ -161,24 +161,13 @@ class Model: ObservableObject {
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***private func finishEdits(_ featureForm: FeatureForm) async {
+***REMOVED***private func finishEditing(_ featureForm: FeatureForm) async {
 ***REMOVED******REMOVED***state = .finishingEdits(featureForm)
-***REMOVED******REMOVED***guard let table = featureForm.feature.table as? ServiceFeatureTable else {
-***REMOVED******REMOVED******REMOVED***state = .generalError(featureForm, Text("Error resolving feature table."))
-***REMOVED******REMOVED******REMOVED***return
-***REMOVED***
-***REMOVED******REMOVED***guard table.isEditable else {
-***REMOVED******REMOVED******REMOVED***state = .generalError(featureForm, Text("The feature table isn't editable."))
-***REMOVED******REMOVED******REMOVED***return
-***REMOVED***
 ***REMOVED******REMOVED***do {
-***REMOVED******REMOVED******REMOVED***state = .finishingEdits(featureForm)
-***REMOVED******REMOVED******REMOVED***try await table.update(featureForm.feature)
+***REMOVED******REMOVED******REMOVED***try await featureForm.finishEditing()
 ***REMOVED*** catch {
-***REMOVED******REMOVED******REMOVED***state = .generalError(featureForm, Text("The feature update failed."))
-***REMOVED******REMOVED******REMOVED***return
+***REMOVED******REMOVED******REMOVED***state = .generalError(featureForm, Text("Finish editing failed.\n\n\(error.localizedDescription)"))
 ***REMOVED***
-***REMOVED******REMOVED***await applyEdits(featureForm, table)
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***private func validateChanges(_ featureForm: FeatureForm) async {
