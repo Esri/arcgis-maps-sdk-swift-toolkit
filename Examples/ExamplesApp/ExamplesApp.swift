b698a2e88be843819c1d425e582341f7917204b6
David Feinzimer <dfeinzimer@esri.com>
2024-06-26 11:56:47 -0700

    Update ExamplesApp links
    Co-Authored-By: Mary Harvey <3710427+maryharvey@users.noreply.github.com>

--- Examples/ExamplesApp/ExamplesApp.swift
+++ Examples/ExamplesApp/ExamplesApp.swift
@@ -26,7 +26,7 @@ struct ExamplesApp: App {
     init() {
         #warning("You must set your API Key or request that the user signs in with an ArcGIS account")
         /*
-         Use of ArcGIS location services, such as basemaps, geocoding, and routing, requires either user authentication or API key authentication. For more information see https://links.esri.com/arcgis-maps-sdk-security-auth or https://links.esri.com/arcgis-maps-sdk-license-and-deploy.
+         Use of ArcGIS location services, such as basemap styles, geocoding, and routing services, requires either user authentication or API key authentication. For more information see https://developers.arcgis.com/documentation/security-and-authentication/types-of-authentication/ or https://developers.arcgis.com/swift/security-and-authentication/.
          1) User authentication: Allows users with an ArcGIS account to sign into an application and access secure services.
          2) API key authentication: A long-lived access token that gives your application access to secure services. Go to the [Create an API key](https://developers.arcgis.com/documentation/security-and-authentication/api-key-authentication/tutorials/create-an-api-key/) tutorial to copy the API key access token. To access all services used by this application set the location services privileges to:
          
