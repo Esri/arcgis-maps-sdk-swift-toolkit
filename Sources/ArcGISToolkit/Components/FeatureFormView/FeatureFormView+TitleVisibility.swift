b9321e09dccba7866fa1927ae84b1e5dd733ca02
David Feinzimer <dfeinzimer@esri.com>
2024-08-13 12:32:33 -0700

    Add FeatureFormView+TitleVisibility

---
+++ Sources/ArcGISToolkit/Components/FeatureFormView/FeatureFormView+TitleVisibility.swift
@@ -0,0 +1,34 @@
+***REMOVED*** Copyright 2024 Esri
+***REMOVED***
+***REMOVED*** Licensed under the Apache License, Version 2.0 (the "License");
+***REMOVED*** you may not use this file except in compliance with the License.
+***REMOVED*** You may obtain a copy of the License at
+***REMOVED***
+***REMOVED***   https:***REMOVED***www.apache.org/licenses/LICENSE-2.0
+***REMOVED***
+***REMOVED*** Unless required by applicable law or agreed to in writing, software
+***REMOVED*** distributed under the License is distributed on an "AS IS" BASIS,
+***REMOVED*** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+***REMOVED*** See the License for the specific language governing permissions and
+***REMOVED*** limitations under the License.
+
+***REMOVED***
+
+public extension FeatureFormView {
+***REMOVED******REMOVED***/ The visibility of the form title.
+***REMOVED***enum TitleVisibility: Sendable {
+***REMOVED******REMOVED******REMOVED***/ The form title is hidden.
+***REMOVED******REMOVED***case hidden
+***REMOVED******REMOVED******REMOVED***/ The form title is visible.
+***REMOVED******REMOVED***case visible
+***REMOVED***
+***REMOVED***
+***REMOVED******REMOVED***/ Specifies the visibility of the form title.
+***REMOVED******REMOVED***/ - Parameter visibility: The preferred visibility of the form title.
+***REMOVED******REMOVED***/ - Since: 200.6
+***REMOVED***func formTitle(_ visibility: TitleVisibility) -> FeatureFormView {
+***REMOVED******REMOVED***var copy = self
+***REMOVED******REMOVED***copy.titleVisibility = visibility
+***REMOVED******REMOVED***return copy
+***REMOVED***
+***REMOVED***
