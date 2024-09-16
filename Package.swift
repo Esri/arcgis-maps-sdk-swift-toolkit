5268cf0519ded4f08d553a953a99fdd2b23a78b3
David Feinzimer <dfeinzimer@esri.com>
2024-09-16 10:27:01 -0700

    Bump ArcGIS to 200.5.1

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/Esri/arcgis-maps-sdk-swift", .upToNextMinor(from: "200.5.0")),
+        .package(url: "https://github.com/Esri/arcgis-maps-sdk-swift", .upToNextMinor(from: "200.5.1")),
         .package(url: "https://github.com/swiftlang/swift-markdown.git", .upToNextMinor(from: "0.4.0"))
     ],
     targets: [
