6ceb73154626a66b280e7e6aa7ef037ef0862c0b
Zachary Kline <zkline@esri.com>
2024-09-17 15:57:35 -0700

    Don't allow taking pictures or video on Vision Pro

--- Sources/ArcGISToolkit/Components/FeatureFormView/AttachmentHelpers/AttachmentImportMenu.swift
+++ Sources/ArcGISToolkit/Components/FeatureFormView/AttachmentHelpers/AttachmentImportMenu.swift
@@ -70,6 +70,7 @@ struct AttachmentImportMenu: View {
         }
     }
     
+    @available(visionOS, unavailable)
     private func takePhotoOrVideoButton() -> Button<some View> {
         Button {
             cameraRequester.request {
@@ -107,7 +108,9 @@ struct AttachmentImportMenu: View {
         }
         Menu {
             // Show photo/video and library picker.
+#if !os(visionOS)
             takePhotoOrVideoButton()
+#endif
             chooseFromLibraryButton()
             // Always show file picker, no matter the input type.
             chooseFromFilesButton()
