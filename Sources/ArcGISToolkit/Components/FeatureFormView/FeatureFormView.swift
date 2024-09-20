43f7a0fdf08f38f1101ffbf6f2409618d2ec0440
David Feinzimer <dfeinzimer@esri.com>
2024-09-20 12:46:36 -0700

    Stop excluding barcode

--- Sources/ArcGISToolkit/Components/FeatureFormView/FeatureFormView.swift
+++ Sources/ArcGISToolkit/Components/FeatureFormView/FeatureFormView.swift
@@ -144,8 +144,7 @@ extension FeatureFormView {
 ***REMOVED******REMOVED***/ Makes UI for a field form element including a divider beneath it.
 ***REMOVED******REMOVED***/ - Parameter element: The element to generate UI for.
 ***REMOVED***@ViewBuilder func makeFieldElement(_ element: FieldFormElement) -> some View {
-***REMOVED******REMOVED***if !(element.input is UnsupportedFormInput ||
-***REMOVED******REMOVED******REMOVED*** element.input is BarcodeScannerFormInput) {
+***REMOVED******REMOVED***if !(element.input is UnsupportedFormInput) {
 ***REMOVED******REMOVED******REMOVED***InputWrapper(element: element)
 ***REMOVED******REMOVED******REMOVED***Divider()
 ***REMOVED***
