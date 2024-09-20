ee405fe4864117e421f54c40ba15d1a4305cc6f7
David Feinzimer <dfeinzimer@esri.com>
2024-09-20 13:13:01 -0700

    Create BarcodeScannerInput.swift

---
+++ Sources/ArcGISToolkit/Components/FeatureFormView/Subviews/Inputs/BarcodeScannerInput.swift
@@ -0,0 +1,43 @@
+***REMOVED*** Copyright 2024 Esri
+***REMOVED***
+***REMOVED*** Licensed under the Apache License, Version 2.0 (the "License");
+***REMOVED*** you may not use this file except in compliance with the License.
+***REMOVED*** You may obtain a copy of the License at
+***REMOVED***
+***REMOVED***   https:***REMOVED***www.apache.org/licenses/LICENSE-2.0
+***REMOVED***
+***REMOVED*** Unless required by applicable law or agreed to in writing, software
+***REMOVED*** distributed under the License is distributed on an "AS IS" BASIS,
+***REMOVED*** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+***REMOVED*** See the License for the specific language governing permissions and
+***REMOVED*** limitations under the License.
+
+***REMOVED***
+import AVFoundation
+***REMOVED***
+
+struct BarcodeScannerInput: View {
+***REMOVED******REMOVED***/ The element the input belongs to.
+***REMOVED***private let element: FieldFormElement
+***REMOVED***
+***REMOVED******REMOVED***/ The input configuration of the field.
+***REMOVED***private let input: BarcodeScannerFormInput
+***REMOVED***
+***REMOVED******REMOVED***/ Creates a view for a barcode scanner input.
+***REMOVED******REMOVED***/ - Parameters:
+***REMOVED******REMOVED***/   - element: The input's parent element.
+***REMOVED***init(element: FieldFormElement) {
+***REMOVED******REMOVED***precondition(
+***REMOVED******REMOVED******REMOVED***element.input is BarcodeScannerFormInput,
+***REMOVED******REMOVED******REMOVED***"\(Self.self).\(#function) element's input must be \(BarcodeScannerFormInput.self)."
+***REMOVED******REMOVED***)
+***REMOVED******REMOVED***self.element = element
+***REMOVED******REMOVED***self.input = element.input as! BarcodeScannerFormInput
+***REMOVED***
+***REMOVED***
+***REMOVED***@State private var scannedCode: String?
+***REMOVED***
+***REMOVED***var body: some View {
+***REMOVED******REMOVED***Text("\(BarcodeScannerFormInput.self)")
+***REMOVED***
+***REMOVED***
