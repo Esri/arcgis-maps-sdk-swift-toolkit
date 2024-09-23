0d85eb4c295aaf35a4b6898be863abcb2ebbcc76
David Feinzimer <dfeinzimer@esri.com>
2024-09-23 16:50:06 -0700

    Update FlyoverExampleView.swift
    Main actor-isolated value of type '() async throws -> ()' passed as a strongly transferred parameter; later accesses could race

--- Examples/Examples/FlyoverExampleView.swift
+++ Examples/Examples/FlyoverExampleView.swift
@@ -32,7 +32,7 @@ struct FlyoverExampleView: View {
 ***REMOVED******REMOVED******REMOVED***SceneView(scene: scene)
 ***REMOVED******REMOVED******REMOVED******REMOVED***.onSingleTapGesture { screen, _ in
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***print("Identifying...")
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***Task.detached { @MainActor in
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***Task { @MainActor in
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***let results = try await proxy.identifyLayers(screenPoint: screen, tolerance: 20)
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***print("\(results.count) identify result(s).")
 ***REMOVED******REMOVED******REMOVED******REMOVED***
