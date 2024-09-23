01a236acdb2d71119442c25edf52d32c935b5bf0
David Feinzimer <dfeinzimer@esri.com>
2024-09-23 14:59:00 -0700

    Add missing default case to `TextFormElementView`

--- Sources/ArcGISToolkit/Components/FeatureFormView/Subviews/TextFormElementView.swift
+++ Sources/ArcGISToolkit/Components/FeatureFormView/Subviews/TextFormElementView.swift
@@ -27,6 +27,8 @@ struct TextFormElementView: View {
 ***REMOVED******REMOVED******REMOVED******REMOVED***MarkdownView(markdown: text)
 ***REMOVED******REMOVED******REMOVED***case .plainText:
 ***REMOVED******REMOVED******REMOVED******REMOVED***Text(text)
+***REMOVED******REMOVED******REMOVED***default:
+***REMOVED******REMOVED******REMOVED******REMOVED***EmptyView()
 ***REMOVED******REMOVED***
 ***REMOVED***
 ***REMOVED******REMOVED***.task {
