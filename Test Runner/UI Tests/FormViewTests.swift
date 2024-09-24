5a6522b57b1fb8413cb543833a431d5d6044a8b4
David Feinzimer <dfeinzimer@esri.com>
2024-09-24 15:13:10 -0700

    Update FormViewTests.swift

--- Test Runner/UI Tests/FormViewTests.swift
+++ Test Runner/UI Tests/FormViewTests.swift
@@ -14,6 +14,7 @@
 
 import XCTest
 
+@MainActor
 final class FeatureFormViewTests: XCTestCase {
 ***REMOVED***override func setUp() async throws {
 ***REMOVED******REMOVED***continueAfterFailure = false
