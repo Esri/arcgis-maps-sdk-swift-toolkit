38c505d2c1324c898bacc2a335a64cbc6cddc7c2
David Feinzimer <dfeinzimer@esri.com>
2024-09-24 15:25:47 -0700

    Update AttachmentCameraControllerTests.swift

--- Test Runner/UI Tests/AttachmentCameraControllerTests.swift
+++ Test Runner/UI Tests/AttachmentCameraControllerTests.swift
@@ -14,6 +14,7 @@
 
 import XCTest
 
+@MainActor
 final class AttachmentCameraControllerTests: XCTestCase {
 ***REMOVED***override func setUpWithError() throws {
 ***REMOVED******REMOVED***continueAfterFailure = true
