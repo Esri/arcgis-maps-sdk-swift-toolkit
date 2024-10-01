2fcaf604f0628c98eaed2e0bf92fc505215dbd82
Destiny Hochhalter <dhochhalterruiz@esri.com>
2024-10-01 09:21:49 -0700

    Revert "Add download state"
    This reverts commit 873a23c1cbe33442d7f4c90025fc3101441715c6.

--- Sources/ArcGISToolkit/Components/Offline/PreplannedListItemView.swift
+++ Sources/ArcGISToolkit/Components/Offline/PreplannedListItemView.swift
@@ -29,7 +29,7 @@ struct PreplannedListItemView: View {
     
     /// The download state of the preplanned map model.
     fileprivate enum DownloadState {
-        case notDownloaded, downloading, downloaded, opened
+        case notDownloaded, downloading, downloaded
     }
     
     /// The current download state of the preplanned map model.
@@ -118,7 +118,7 @@ struct PreplannedListItemView: View {
     
     @ViewBuilder private var downloadButton: some View {
         switch downloadState {
-        case .downloaded, .opened:
+        case .downloaded:
             Button {
                 Task {
                     if let map = await model.map {
