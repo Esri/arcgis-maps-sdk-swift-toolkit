83f84fb20d32dfaba132d741dbe73780386ff36b
David Feinzimer <dfeinzimer@esri.com>
2024-10-02 11:10:20 -0700

    Remove unneeded `@MainActor` annotation from `BasemapGalleryExampleView`

--- Examples/Examples/BasemapGalleryExampleView.swift
+++ Examples/Examples/BasemapGalleryExampleView.swift
@@ -16,7 +16,6 @@ import ArcGIS
 import ArcGISToolkit
 import SwiftUI
 
-@MainActor
 struct BasemapGalleryExampleView: View {
     /// A Boolean value indicating whether the basemap gallery is presented.
     @State private var basemapGalleryIsPresented = false
