1edc9fc4323b7cf6d82aed216fafda034721ad63
Zachary Kline <zkline@esri.com>
2024-10-14 11:59:47 -0700

    Remove #if for scalebar tests

--- Tests/ArcGISToolkitTests/ScalebarTests.swift
+++ Tests/ArcGISToolkitTests/ScalebarTests.swift
@@ -12,7 +12,6 @@
 ***REMOVED*** See the License for the specific language governing permissions and
 ***REMOVED*** limitations under the License.
 
-#if !os(visionOS)
 ***REMOVED***
 import Combine
 ***REMOVED***
@@ -107,4 +106,3 @@ extension ScalebarTests {
 ***REMOVED******REMOVED***)
 ***REMOVED***
 ***REMOVED***
-#endif
