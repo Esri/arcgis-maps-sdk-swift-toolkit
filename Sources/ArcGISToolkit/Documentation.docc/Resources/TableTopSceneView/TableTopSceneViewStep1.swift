c4e079d26d8875e8c866803aedeb22569b2d8dda
Caleb Rasmussen <crasmussen@esri.com>
2024-10-28 15:40:58 -0700

    Update tutorial.
    https://github.com/Esri/arcgis-maps-sdk-swift-toolkit/pull/937#issuecomment-2442392972

--- Sources/ArcGISToolkit/Documentation.docc/Resources/TableTopSceneView/TableTopSceneViewStep1.swift
+++ Sources/ArcGISToolkit/Documentation.docc/Resources/TableTopSceneView/TableTopSceneViewStep1.swift
@@ -12,7 +12,7 @@ struct TableTopExampleView: View {
     var body: some View {
         TableTopSceneView(
             anchorPoint: anchorPoint,
-            translationFactor: 1_000,
+            translationFactor: 400,
             clippingDistance: 400
         )
     }
