2bd9d0edcae70c0b96641f1540727a4d13c61644
David Feinzimer <dfeinzimer@esri.com>
2024-10-30 16:07:46 -0700

    Remove AR components from Catalyst

--- Sources/ArcGISToolkit/Components/Augmented Reality/TableTopSceneView.swift
+++ Sources/ArcGISToolkit/Components/Augmented Reality/TableTopSceneView.swift
@@ -17,6 +17,7 @@ import SwiftUI
 import ArcGIS
 
 /// A scene view that provides an augmented reality table top experience.
+@available(macCatalyst, unavailable)
 @available(visionOS, unavailable)
 public struct TableTopSceneView: View {
 #if os(iOS)
