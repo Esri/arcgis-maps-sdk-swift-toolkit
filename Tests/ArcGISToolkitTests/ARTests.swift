942785b589dfd663fc15a9934e6ac0d6267dfb16
David Feinzimer <dfeinzimer@esri.com>
2024-11-01 16:35:39 -0700

    Update Tests/ArcGISToolkitTests/ARTests.swift
    Co-authored-by: Zachary A Kline <zkline@esri.com>

--- Tests/ArcGISToolkitTests/ARTests.swift
+++ Tests/ArcGISToolkitTests/ARTests.swift
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#if !os(visionOS) || !targetEnvironment(macCatalyst)
+#if !os(visionOS) && !targetEnvironment(macCatalyst)
 import ArcGIS
 import SwiftUI
 import XCTest
