7debaec205b3b94acc1e6e8208d45b6cd96fa579
David Feinzimer <dfeinzimer@esri.com>
2024-11-20 16:26:46 -0800

    Update tutorial

--- Sources/ArcGISToolkit/Documentation.docc/Resources/UtilityNetworkTrace/UtilityNetworkTraceStep4.swift
+++ Sources/ArcGISToolkit/Documentation.docc/Resources/UtilityNetworkTrace/UtilityNetworkTraceStep4.swift
@@ -3,33 +3,40 @@
 ***REMOVED***
 
 struct UtilityNetworkTraceExampleView: View {
+***REMOVED***@State private var activeDetent: FloatingPanelDetent = .half
+***REMOVED***
 ***REMOVED***@State private var map = makeMap()
 ***REMOVED***
 ***REMOVED***@State private var mapPoint: Point?
 ***REMOVED***
 ***REMOVED***@State private var resultGraphicsOverlay = GraphicsOverlay()
 ***REMOVED***
-***REMOVED***@State private var screenPoint: CGPoint?
-***REMOVED***
-***REMOVED***@State private var viewpoint: Viewpoint?
-***REMOVED***
 ***REMOVED***var body: some View {
 ***REMOVED******REMOVED***MapViewReader { mapViewProxy in
 ***REMOVED******REMOVED******REMOVED***MapView(
 ***REMOVED******REMOVED******REMOVED******REMOVED***map: map,
-***REMOVED******REMOVED******REMOVED******REMOVED***viewpoint: viewpoint,
 ***REMOVED******REMOVED******REMOVED******REMOVED***graphicsOverlays: [resultGraphicsOverlay]
 ***REMOVED******REMOVED******REMOVED***)
-***REMOVED******REMOVED******REMOVED***.onSingleTapGesture { screenPoint, mapPoint in
-***REMOVED******REMOVED******REMOVED******REMOVED***self.screenPoint = screenPoint
+***REMOVED******REMOVED******REMOVED***.onSingleTapGesture { _, mapPoint in
 ***REMOVED******REMOVED******REMOVED******REMOVED***self.mapPoint = mapPoint
-***REMOVED******REMOVED***
-***REMOVED******REMOVED******REMOVED***.onViewpointChanged(kind: .centerAndScale) {
-***REMOVED******REMOVED******REMOVED******REMOVED***viewpoint = $0
 ***REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED***.task {
 ***REMOVED******REMOVED******REMOVED******REMOVED***let publicSample = try? await ArcGISCredential.publicSample
 ***REMOVED******REMOVED******REMOVED******REMOVED***ArcGISEnvironment.authenticationManager.arcGISCredentialStore.add(publicSample!)
+***REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED***.floatingPanel(
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***backgroundColor: Color(uiColor: .systemGroupedBackground),
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***selectedDetent: $activeDetent,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***horizontalAlignment: .trailing,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***isPresented: .constant(true)
+***REMOVED******REMOVED******REMOVED***) {
+***REMOVED******REMOVED******REMOVED******REMOVED***UtilityNetworkTrace(
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***graphicsOverlay: $resultGraphicsOverlay,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***map: map,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***mapPoint: $mapPoint,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***mapViewProxy: mapViewProxy
+***REMOVED******REMOVED******REMOVED******REMOVED***)
+***REMOVED******REMOVED******REMOVED******REMOVED***.floatingPanelDetent($activeDetent)
 ***REMOVED******REMOVED***
 ***REMOVED***
 ***REMOVED***
