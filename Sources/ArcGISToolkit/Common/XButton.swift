8fb1b536933df574ec1b07443947f2e21e7c722a
David Feinzimer <dfeinzimer@esri.com>
2024-12-03 14:56:11 -0800

    Revise Preview

--- Sources/ArcGISToolkit/Common/XButton.swift
+++ Sources/ArcGISToolkit/Common/XButton.swift
@@ -70,6 +70,7 @@ extension XButton {
             Button {
                 isPresented = true
             } label: {
+                Text(verbatim: "Present")
             }
             .buttonStyle(.bordered)
         }
