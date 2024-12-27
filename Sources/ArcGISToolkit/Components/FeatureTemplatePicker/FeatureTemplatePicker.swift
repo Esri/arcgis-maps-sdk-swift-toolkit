9faba0bfc7bc92e37c9bde4f448d68badffc3cf4
Ryan Olson <rolson@esri.com>
2024-12-27 16:46:58 -0700

    update feature template picker

--- Sources/ArcGISToolkit/Components/FeatureTemplatePicker/FeatureTemplatePicker.swift
+++ Sources/ArcGISToolkit/Components/FeatureTemplatePicker/FeatureTemplatePicker.swift
@@ -15,13 +15,25 @@
 ***REMOVED***
 ***REMOVED***
 
+***REMOVED***/ A view that displays feature temmplates from a geo model.
 public struct FeatureTemplatePicker: View {
 ***REMOVED******REMOVED***/ The model backing the feature template picker.
-***REMOVED***@StateObject var model: Model
-***REMOVED***@Binding var selection: FeatureTemplateInfo?
+***REMOVED***@StateObject private var model: Model
+***REMOVED******REMOVED***/ The selection
+***REMOVED***@Binding private var selection: FeatureTemplateInfo?
 ***REMOVED***
-***REMOVED***init(geoModel: GeoModel, selection: Binding<FeatureTemplateInfo?>) {
-***REMOVED******REMOVED***_model = StateObject(wrappedValue: Model(geoModel: geoModel))
+***REMOVED******REMOVED***/ Creates a feature template picker.
+***REMOVED******REMOVED***/ - Parameters:
+***REMOVED******REMOVED***/   - geoModel: The geo model from which feature templates will be displayed.
+***REMOVED******REMOVED***/   - selection: The selected template.
+***REMOVED******REMOVED***/   - includeNonCreatableFeatureTemplates: Include feature templates from tables where features cannot be created.
+***REMOVED***init(geoModel: GeoModel, selection: Binding<FeatureTemplateInfo?>, includeNonCreatableFeatureTemplates: Bool = false) {
+***REMOVED******REMOVED***_model = StateObject(
+***REMOVED******REMOVED******REMOVED***wrappedValue: Model(
+***REMOVED******REMOVED******REMOVED******REMOVED***geoModel: geoModel,
+***REMOVED******REMOVED******REMOVED******REMOVED***includeNonCreatableFeatureTemplates: includeNonCreatableFeatureTemplates
+***REMOVED******REMOVED******REMOVED***)
+***REMOVED******REMOVED***)
 ***REMOVED******REMOVED***_selection = selection
 ***REMOVED***
 ***REMOVED***
@@ -32,13 +44,13 @@ public struct FeatureTemplatePicker: View {
 ***REMOVED******REMOVED*** else if model.showContentUnavailable {
 ***REMOVED******REMOVED******REMOVED******REMOVED***if #available(iOS 17.0, *) {
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***ContentUnavailableView(
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***"No Feature Templates",
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***systemImage: "list.bullet",
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***description: Text("No feature templates available for this map.")
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***String.noFeatureTemplatesTitle,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***systemImage: "mappin.slash.circle",
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***description: Text(String.noFeatureTemplatesDetail)
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***)
 ***REMOVED******REMOVED******REMOVED*** else {
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** Fallback on earlier versions
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***Text("No feature templates available for this map.")
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***Text(String.noFeatureTemplatesDetail)
 ***REMOVED******REMOVED******REMOVED***
 ***REMOVED******REMOVED*** else {
 ***REMOVED******REMOVED******REMOVED******REMOVED***List {
@@ -50,6 +62,21 @@ public struct FeatureTemplatePicker: View {
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED******REMOVED***.searchable(text: $model.searchText, prompt: String.searchTemplatesPrompt)
+***REMOVED******REMOVED******REMOVED******REMOVED***.overlay {
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***if model.showNoTemplatesFound {
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***if #available(iOS 17.0, *) {
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***ContentUnavailableView(
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***String.noFeatureTemplatesFoundTitle,
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***systemImage: "magnifyingglass.circle",
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***description: Text(String.noFeatureTemplatesFoundDetail)
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***)
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** else {
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED*** Fallback on earlier versions
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***Text(String.noFeatureTemplatesFoundDetail)
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED***
 ***REMOVED******REMOVED***
 ***REMOVED***
 ***REMOVED******REMOVED***.task {
@@ -58,6 +85,59 @@ public struct FeatureTemplatePicker: View {
 ***REMOVED***
 ***REMOVED***
 
+private extension String {
+***REMOVED***static var noFeatureTemplatesTitle: String {
+***REMOVED******REMOVED***String(
+***REMOVED******REMOVED******REMOVED***localized: "No Feature Templates",
+***REMOVED******REMOVED******REMOVED******REMOVED***bundle: .toolkitModule,
+***REMOVED******REMOVED******REMOVED***comment: """
+***REMOVED******REMOVED******REMOVED******REMOVED*** A title for showing a view that tells the user there are no feature templates.
+***REMOVED******REMOVED******REMOVED******REMOVED*** """
+***REMOVED******REMOVED***)
+***REMOVED***
+***REMOVED***static var noFeatureTemplatesDetail: String {
+***REMOVED******REMOVED***String(
+***REMOVED******REMOVED******REMOVED***localized: "There are no feature templates available for this map.",
+***REMOVED******REMOVED******REMOVED******REMOVED***bundle: .toolkitModule,
+***REMOVED******REMOVED******REMOVED***comment: """
+***REMOVED******REMOVED******REMOVED******REMOVED*** Details for showing a view that tells the user there are no feature templates
+***REMOVED******REMOVED******REMOVED******REMOVED*** available in this map.
+***REMOVED******REMOVED******REMOVED******REMOVED*** """
+***REMOVED******REMOVED***)
+***REMOVED***
+***REMOVED***static var searchTemplatesPrompt: String {
+***REMOVED******REMOVED***String(
+***REMOVED******REMOVED******REMOVED***localized: "Search templates",
+***REMOVED******REMOVED******REMOVED******REMOVED***bundle: .toolkitModule,
+***REMOVED******REMOVED******REMOVED***comment: """
+***REMOVED******REMOVED******REMOVED******REMOVED*** A prompt in the search templates text box that instructs the user that they
+***REMOVED******REMOVED******REMOVED******REMOVED*** can type in the field to search for templates.
+***REMOVED******REMOVED******REMOVED******REMOVED*** """
+***REMOVED******REMOVED***)
+***REMOVED***
+***REMOVED***static var noFeatureTemplatesFoundTitle: String {
+***REMOVED******REMOVED***String(
+***REMOVED******REMOVED******REMOVED***localized: "Nothing Found",
+***REMOVED******REMOVED******REMOVED******REMOVED***bundle: .toolkitModule,
+***REMOVED******REMOVED******REMOVED***comment: """
+***REMOVED******REMOVED******REMOVED******REMOVED*** A title for showing a view that tells the user there were no feature templates
+***REMOVED******REMOVED******REMOVED******REMOVED*** found that match their search criteria.
+***REMOVED******REMOVED******REMOVED******REMOVED*** """
+***REMOVED******REMOVED***)
+***REMOVED***
+***REMOVED***static var noFeatureTemplatesFoundDetail: String {
+***REMOVED******REMOVED***String(
+***REMOVED******REMOVED******REMOVED***localized: "There were no feature templates found that match the search criteria.",
+***REMOVED******REMOVED******REMOVED******REMOVED***bundle: .toolkitModule,
+***REMOVED******REMOVED******REMOVED***comment: """
+***REMOVED******REMOVED******REMOVED******REMOVED*** Details for showing a view that tells the user there were no feature templates
+***REMOVED******REMOVED******REMOVED******REMOVED*** found that match their search criteria.
+***REMOVED******REMOVED******REMOVED******REMOVED*** """
+***REMOVED******REMOVED***)
+***REMOVED***
+***REMOVED***
+
+***REMOVED***/ View of a feature teamplate.
 private struct FeatureTemplateView: View {
 ***REMOVED***let info: FeatureTemplateInfo
 ***REMOVED***@Binding var selection: FeatureTemplateInfo?
@@ -88,20 +168,35 @@ private struct FeatureTemplateView: View {
 ***REMOVED***
 
 extension FeatureTemplatePicker {
-***REMOVED******REMOVED***/ The model for the legend.
+***REMOVED******REMOVED***/ The model for the feature template picker.
 ***REMOVED***@MainActor
 ***REMOVED***final class Model: ObservableObject {
 ***REMOVED******REMOVED******REMOVED***/ The associated geo model.
 ***REMOVED******REMOVED***let geoModel: GeoModel
+***REMOVED******REMOVED******REMOVED***/ Include feature templates from tables where features cannot be created.
+***REMOVED******REMOVED***private let includeNonCreatableFeatureTemplates: Bool
+***REMOVED******REMOVED******REMOVED***/ Search text for filtering the list of templates.
+***REMOVED******REMOVED***var searchText: String = "" {
+***REMOVED******REMOVED******REMOVED***didSet { templatesOrSearchTextDidChange() ***REMOVED***
+***REMOVED***
+***REMOVED******REMOVED******REMOVED***/ The complete unfiltered list of feature template sections.
+***REMOVED******REMOVED***private var unfilteredFeatureTemplateSections = [FeatureTemplateSectionInfo]() {
+***REMOVED******REMOVED******REMOVED***didSet { templatesOrSearchTextDidChange() ***REMOVED***
+***REMOVED***
 ***REMOVED******REMOVED***
-***REMOVED******REMOVED******REMOVED***/ The templates.
-***REMOVED******REMOVED***@Published fileprivate var featureTemplateSections = [FeatureTemplateSectionInfo]()
+***REMOVED******REMOVED******REMOVED***/ The sections.
+***REMOVED******REMOVED***@Published fileprivate private(set) var featureTemplateSections = [FeatureTemplateSectionInfo]()
+***REMOVED******REMOVED******REMOVED***/ A Boolean value indicating whether templates are being generated.
 ***REMOVED******REMOVED***@Published var isGeneratingFeatureTemplates = false
+***REMOVED******REMOVED******REMOVED***/ A Boolean value indicating if content is unavailable.
 ***REMOVED******REMOVED***@Published var showContentUnavailable = false
+***REMOVED******REMOVED******REMOVED***/ A Boolean value indicating if no templates were found with the given search text.
+***REMOVED******REMOVED***@Published var showNoTemplatesFound = false
 ***REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED***/ Creates a model for a given geo model.
-***REMOVED******REMOVED***init(geoModel: GeoModel) {
+***REMOVED******REMOVED***init(geoModel: GeoModel, includeNonCreatableFeatureTemplates: Bool) {
 ***REMOVED******REMOVED******REMOVED***self.geoModel = geoModel
+***REMOVED******REMOVED******REMOVED***self.includeNonCreatableFeatureTemplates = includeNonCreatableFeatureTemplates
 ***REMOVED***
 ***REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED***/ Generates the feature templates.
@@ -109,30 +204,17 @@ extension FeatureTemplatePicker {
 ***REMOVED******REMOVED******REMOVED***isGeneratingFeatureTemplates = true
 ***REMOVED******REMOVED******REMOVED***defer { isGeneratingFeatureTemplates = false ***REMOVED***
 ***REMOVED******REMOVED******REMOVED***
-***REMOVED******REMOVED******REMOVED***featureTemplateSections = await geoModel.featureTemplateSections
+***REMOVED******REMOVED******REMOVED***unfilteredFeatureTemplateSections = await makeFeatureTemplateSections()
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-
-extension GeoModel {
-***REMOVED***var arcGISFeatureLayersAndTables: [(layer: FeatureLayer, table: ArcGISFeatureTable)] {
-***REMOVED******REMOVED***featureLayers
-***REMOVED******REMOVED******REMOVED***.map { (layer: $0, table: $0.featureTable) ***REMOVED***
-***REMOVED******REMOVED******REMOVED***.compactMap { tuple in
-***REMOVED******REMOVED******REMOVED******REMOVED***if let table = tuple.table as? ArcGISFeatureTable {
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***return (layer: tuple.layer, table: table)
-***REMOVED******REMOVED******REMOVED*** else {
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***return nil
-***REMOVED******REMOVED******REMOVED***
 ***REMOVED******REMOVED***
-***REMOVED***
-***REMOVED***
-
-private extension GeoModel {
-***REMOVED***var featureTemplateSections: [FeatureTemplateSectionInfo] {
-***REMOVED******REMOVED***get async {
+***REMOVED******REMOVED******REMOVED***/ The feature template section information for the geo model.
+***REMOVED******REMOVED***private func makeFeatureTemplateSections() async -> [FeatureTemplateSectionInfo] {
+***REMOVED******REMOVED******REMOVED***try? await geoModel.load()
 ***REMOVED******REMOVED******REMOVED***var sections = [FeatureTemplateSectionInfo]()
-***REMOVED******REMOVED******REMOVED***for (layer, table) in self.arcGISFeatureLayersAndTables {
+***REMOVED******REMOVED******REMOVED***let layersAndTables = geoModel.arcGISFeatureLayersAndTables
+***REMOVED******REMOVED******REMOVED***await layersAndTables.map(\.table).load()
+***REMOVED******REMOVED******REMOVED***for (layer, table) in layersAndTables {
+***REMOVED******REMOVED******REMOVED******REMOVED***guard includeNonCreatableFeatureTemplates || table.canAddFeature else { continue ***REMOVED***
 ***REMOVED******REMOVED******REMOVED******REMOVED***var infos = [FeatureTemplateInfo]()
 ***REMOVED******REMOVED******REMOVED******REMOVED***for template in table.allTemplates {
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***let feature = table.makeFeature(template: template)
@@ -151,12 +233,39 @@ private extension GeoModel {
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***.init(table: table, infos: infos)
 ***REMOVED******REMOVED******REMOVED******REMOVED***)
 ***REMOVED******REMOVED***
-***REMOVED******REMOVED******REMOVED***return sections
+***REMOVED******REMOVED******REMOVED***return sections.filter { !$0.infos.isEmpty ***REMOVED***
+***REMOVED***
+***REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED***/ Re-filters based on the templates or the search text.
+***REMOVED******REMOVED***private func templatesOrSearchTextDidChange() {
+***REMOVED******REMOVED******REMOVED***featureTemplateSections = unfilteredFeatureTemplateSections.map {
+***REMOVED******REMOVED******REMOVED******REMOVED***$0.filtered(by: searchText)
+***REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED***.filter { !$0.infos.isEmpty ***REMOVED***
+***REMOVED******REMOVED******REMOVED***showContentUnavailable = unfilteredFeatureTemplateSections.isEmpty
+***REMOVED******REMOVED******REMOVED***showNoTemplatesFound = !showContentUnavailable && featureTemplateSections.isEmpty
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
 
-extension ArcGISFeatureTable {
+private extension GeoModel {
+***REMOVED******REMOVED***/ A list containing tuples of the feature layers and the associated
+***REMOVED******REMOVED***/ ArcGIS feature tables in the geo model.
+***REMOVED***var arcGISFeatureLayersAndTables: [(layer: FeatureLayer, table: ArcGISFeatureTable)] {
+***REMOVED******REMOVED***featureLayers
+***REMOVED******REMOVED******REMOVED***.map { (layer: $0, table: $0.featureTable) ***REMOVED***
+***REMOVED******REMOVED******REMOVED***.compactMap { tuple in
+***REMOVED******REMOVED******REMOVED******REMOVED***if let table = tuple.table as? ArcGISFeatureTable {
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***return (layer: tuple.layer, table: table)
+***REMOVED******REMOVED******REMOVED*** else {
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***return nil
+***REMOVED******REMOVED******REMOVED***
+***REMOVED******REMOVED***
+***REMOVED***
+***REMOVED***
+
+private extension ArcGISFeatureTable {
+***REMOVED******REMOVED***/ A flattened list of the feature templates in the table.
 ***REMOVED***var allTemplates: [FeatureTemplate] {
 ***REMOVED******REMOVED***let typeTemplates = featureTypes
 ***REMOVED******REMOVED******REMOVED***.lazy
@@ -165,15 +274,21 @@ extension ArcGISFeatureTable {
 ***REMOVED***
 ***REMOVED***
 
+***REMOVED***/ A value that represents a section in the feature template picker.
 private struct FeatureTemplateSectionInfo: Identifiable {
 ***REMOVED***let table: ArcGISFeatureTable
-***REMOVED***let infos: [FeatureTemplateInfo]
+***REMOVED***var infos: [FeatureTemplateInfo]
+***REMOVED***let id: UUID = UUID()
 ***REMOVED***
-***REMOVED***var id: ObjectIdentifier {
-***REMOVED******REMOVED***ObjectIdentifier(table)
+***REMOVED***func filtered(by searchText: String) -> FeatureTemplateSectionInfo {
+***REMOVED******REMOVED***guard !searchText.isEmpty else { return self ***REMOVED***
+***REMOVED******REMOVED***var copy = self
+***REMOVED******REMOVED***copy.infos = copy.infos.filter { $0.template.name.lowercased().contains(searchText.lowercased()) ***REMOVED***
+***REMOVED******REMOVED***return copy
 ***REMOVED***
 ***REMOVED***
 
+***REMOVED***/ A value that represents a feature template in the picker.
 struct FeatureTemplateInfo: Identifiable, Equatable {
 ***REMOVED***static func == (lhs: FeatureTemplateInfo, rhs: FeatureTemplateInfo) -> Bool {
 ***REMOVED******REMOVED***lhs.template === rhs.template
