f614346c5c0507af11c40d6806466a8cb3720d18
Ryan Olson <rolson@esri.com>
2024-12-31 10:48:59 -0700

    add tests

---
+++ Tests/ArcGISToolkitTests/FeatureTemplatePickerTests.swift
@@ -0,0 +1,51 @@
+//
+//  CompassTests 2.swift
+//  arcgis-maps-sdk-swift-toolkit
+//
+//  Created by Ryan Olson on 12/31/24.
+//
+
+
+// Copyright 2022 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import ArcGIS
+@testable import ArcGISToolkit
+import Foundation
+import SwiftUI
+import Testing
+
+@Suite("FeatureTemplatePicker Tests")
+struct FeatureTemplatePickerTests {
+    @Test
+    @MainActor
+    func testModel() async throws {
+        let map = makeMap()
+        try await map.load()
+        
+        let model = FeatureTemplatePicker.Model(geoModel: map, includeNonCreatableFeatureTemplates: true)
+        #expect(model.includeNonCreatableFeatureTemplates == true)
+        #expect(model.isGeneratingFeatureTemplates == false)
+        #expect(model.showContentUnavailable == false)
+        #expect(model.showNoTemplatesFound == false)
+    }
+}
+
+private func makeMap() -> Map {
+    let map = Map(spatialReference: .webMercator)
+    let featureTable = ServiceFeatureTable(url: URL(string: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/DamageAssessment/FeatureServer/0")!)
+    let featureLayer = FeatureLayer(featureTable: featureTable)
+    map.addOperationalLayer(featureLayer)
+    return map
+}
