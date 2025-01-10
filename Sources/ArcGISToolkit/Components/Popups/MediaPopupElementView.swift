e00f38308530b39af487bce6712a9eeb4c943606
David Feinzimer <dfeinzimer@esri.com>
2025-01-10 12:06:30 -0800

    Revert "Use new iOS 18 onScrollGeometryChange API"
    This reverts commit 5be0e96368fc3d5ab515d612040af033b3089f8b.

--- Sources/ArcGISToolkit/Components/Popups/MediaPopupElementView.swift
+++ Sources/ArcGISToolkit/Components/Popups/MediaPopupElementView.swift
@@ -58,7 +58,7 @@ struct MediaPopupElementView: View {
         @State private var width: CGFloat = .zero
         
         var body: some View {
-            let scrollView = ScrollView(.horizontal) {
+            ScrollView(.horizontal) {
                 HStack(alignment: .top, spacing: 8) {
                     ForEach(popupMedia) { media in
                         Group {
@@ -82,20 +82,10 @@ struct MediaPopupElementView: View {
                     }
                 }
             }
-            if #available(iOS 18.0, *) {
-                scrollView
-                    .onScrollGeometryChange(for: CGFloat.self) { geometry in
-                        geometry.contentSize.width
-                    } action: { _, newValue in
-                        width = newValue * widthScaleFactor
-                    }
-            } else {
-                scrollView
-                    .onGeometryChange(for: CGFloat.self) { proxy in
-                        proxy.frame(in: .global).width
-                    } action: { newValue in
-                        width = newValue * widthScaleFactor
-                    }
+            .onGeometryChange(for: CGRect.self) { proxy in
+                proxy.frame(in: .global)
+            } action: { newValue in
+                width = newValue.width * widthScaleFactor
             }
         }
         
