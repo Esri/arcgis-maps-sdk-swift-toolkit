66f8d13bfbe8ec40881a2c586f8c7d0a17d03e77
David Feinzimer <dfeinzimer@esri.com>
2025-01-29 11:08:03 -0800

    Disable path change notifications for now

--- Sources/ArcGISToolkit/Components/FeatureFormView/FeatureFormView.swift
+++ Sources/ArcGISToolkit/Components/FeatureFormView/FeatureFormView.swift
@@ -112,9 +112,9 @@ public struct FeatureFormView: View {
 ***REMOVED******REMOVED***
 ***REMOVED***
 ***REMOVED******REMOVED***.environment(\.validationErrorVisibility, validationErrorVisibility)
-***REMOVED******REMOVED***.onChange(of: path) { newValue in
-***REMOVED******REMOVED******REMOVED***onPresentedFeatureChangedAction?(newValue.last)
-***REMOVED***
+***REMOVED******REMOVED******REMOVED***.onChange(of: path) { newValue in
+***REMOVED******REMOVED******REMOVED******REMOVED***onPresentedFeatureChangedAction?(newValue.last)
+***REMOVED******REMOVED***
 ***REMOVED***
 ***REMOVED***
 ***REMOVED******REMOVED***/ <#Description#>
