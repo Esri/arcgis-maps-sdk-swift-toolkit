2899179fd7cb1153520a8bc73b877bf4f08ff519
Ryan Olson <rolson@esri.com>
2025-01-28 20:47:40 -0700

    UI cleanup

--- Sources/ArcGISToolkit/Components/Offline/OnDemandConfigurationView.swift
+++ Sources/ArcGISToolkit/Components/Offline/OnDemandConfigurationView.swift
@@ -18,29 +18,33 @@ import SwiftUI
 // TODO: doc
 struct OnDemandConfigurationView: View {
     let map: Map
+    let title: String = "hello"
     let onCompleteAction: (OnDemandMapAreaConfiguration) -> Void
     
-    @State private var titleInput = ""
     @State private var maxScale: CacheScale = .room
-    @State private var polygon: Polygon?
     @State private var currentVisibleArea: Polygon?
     
     @Environment(\.dismiss) private var dismiss
     
-    var cannotAddOnDemandArea: Bool {
-        titleInput.isEmpty || currentVisibleArea == nil
+    var downloadIsDisabled: Bool {
+        currentVisibleArea == nil
     }
     
     var body: some View {
         NavigationStack {
-            VStack(spacing: 0) {
-                // TODO: this should extend the top safe area
-                Text("Pan and zoom to define the area")
-                    .frame(maxWidth: .infinity)
-                    .padding(.vertical, 6)
-                    .background(.thinMaterial, ignoresSafeAreaEdges: .horizontal)
-                
-                mapView
+            VStack {
+                VStack(spacing: 0) {
+                    Divider()
+                    Text("Pan and zoom to define the area")
+                        .font(.footnote)
+                        .foregroundStyle(.secondary)
+                        .padding(8)
+                        .frame(maxWidth: .infinity)
+                    Divider()
+                    mapView
+                }
+            }
+            .safeAreaInset(edge: .bottom) {
                 bottomPane
             }
             .toolbar {
@@ -52,71 +56,84 @@ struct OnDemandConfigurationView: View {
             }
             .navigationBarTitle("Select Area")
             .navigationBarTitleDisplayMode(.inline)
-            .ignoresSafeArea(edges: .bottom)
         }
-    }
-    
-    @ViewBuilder
-    private var bottomPane: some View {
-        VStack {
-            HStack {
-                Text("Title")
-                TextField("Type here", text: $titleInput)
-                    .multilineTextAlignment(.trailing)
-            }
-            
-            HStack {
-                Picker("Max Scale", selection: $maxScale) {
-                    ForEach(CacheScale.allCases, id: \.self) {
-                        Text($0.description)
-                    }
-                }
-                .pickerStyle(.navigationLink)
-            }
-            
-            HStack {
-                Button {
-                    guard let currentVisibleArea else { return }
-                    let configuration = OnDemandMapAreaConfiguration(
-                        title: titleInput,
-                        minScale: CacheScale.worldSmall.scale,
-                        maxScale: maxScale.scale,
-                        areaOfInterest: currentVisibleArea
-                    )
-                    onCompleteAction(configuration)
-                    dismiss()
-                } label: {
-                    Text("Download")
-                        .bold()
-                        .font(.body)
-                        .tint(.white)
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                }
-                .frame(maxWidth: .infinity)
-                .background(.blue)
-                .cornerRadius(10)
-                .disabled(cannotAddOnDemandArea)
-            }
-            .padding(.bottom, 20)
-        }
-        .frame(maxWidth: .infinity)
-        .padding()
-        .background(.thickMaterial)
-        .clipShape(
-            .rect(
-                topLeadingRadius: 10,
-                bottomLeadingRadius: 0,
-                bottomTrailingRadius: 0,
-                topTrailingRadius: 10
-            )
-        )
     }
     
     @ViewBuilder
     private var mapView: some View {
         MapView(map: map)
+            .magnifierDisabled(true)
+            .attributionBarHidden(true)
             .interactionModes([.pan, .zoom])
             .onVisibleAreaChanged { currentVisibleArea = $0 }
+    }
+    
+    @ViewBuilder
+    private var bottomPane: some View {
+        BottomCard(background: Color(uiColor: .systemBackground)) {
+            VStack {
+                HStack {
+                    Picker("Max Scale", selection: $maxScale) {
+                        ForEach(CacheScale.allCases, id: \.self) {
+                            Text($0.description)
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                HStack {
+                    Button {
+                        guard let currentVisibleArea else { return }
+                        let configuration = OnDemandMapAreaConfiguration(
+                            title: title,
+                            minScale: CacheScale.worldSmall.scale,
+                            maxScale: maxScale.scale,
+                            areaOfInterest: currentVisibleArea.extent
+                        )
+                        onCompleteAction(configuration)
+                        dismiss()
+                    } label: {
+                        Label("Download", systemImage: "square.and.arrow.down")
+                            .frame(maxWidth: .infinity)
+                    }
+                    .controlSize(.large)
+                    .buttonStyle(.borderedProminent)
+                    .disabled(downloadIsDisabled)
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+private struct BottomCard<Content: View, Background: ShapeStyle>: View {
+    let content: () -> Content
+    let background: Background
+    
+    init(background: Background,@ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+        self.background = background
+    }
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            content()
+                .background(background)
+                .clipShape(
+                    .rect(
+                        topLeadingRadius: 12,
+                        bottomLeadingRadius: 0,
+                        bottomTrailingRadius: 0,
+                        topTrailingRadius: 12
+                    )
+                )
+            
+            // So it extends into the bottom safe area.
+            VStack {}
+                .frame(height: 1)
+                .frame(maxWidth: .infinity)
+                .background(background)
+        }
+        .ignoresSafeArea(.container, edges: .horizontal)
     }
 }
