3fdaac9d47c61753ecfacfbafc174c9f6c614a47
Ryan Olson <rolson@esri.com>
2025-01-28 17:25:19 -0700

    cleanup

--- Sources/ArcGISToolkit/Components/Offline/OfflineMapViewModel.swift
+++ Sources/ArcGISToolkit/Components/Offline/OfflineMapViewModel.swift
@@ -51,75 +51,6 @@ class OfflineMapViewModel: ObservableObject {
         portalItemID = onlineMap.item!.id!
     }
     
-    // TODO: move to PreplannedMapModel as static
-    /// Gets the preplanned map areas from the offline map task and loads the map models.
-    func loadPreplannedMapModels() async {
-        if offlineMapTask.loadStatus != .loaded {
-            try? await offlineMapTask.retryLoad()
-        }
-        
-        // Reset flag.
-        isShowingOnlyOfflineModels = false
-        
-        preplannedMapModels = await Result { @MainActor in
-            do {
-                return try await offlineMapTask.preplannedMapAreas
-                    .filter { $0.portalItem.id != nil }
-                    .sorted(using: KeyPathComparator(\.portalItem.title))
-                    .map {
-                        PreplannedMapModel(
-                            offlineMapTask: offlineMapTask,
-                            mapArea: $0,
-                            portalItemID: portalItemID,
-                            preplannedMapAreaID: $0.portalItem.id!,
-                            onRemoveDownload: onRemoveDownloadOfPreplannedArea(withID:)
-                        )
-                    }
-            } catch {
-                // If not connected to the internet, then return only the offline models.
-                if let urlError = error as? URLError,
-                   urlError.code == .notConnectedToInternet {
-                    isShowingOnlyOfflineModels = true
-                    return await loadOfflinePreplannedMapModels()
-                } else {
-                    throw error
-                }
-            }
-        }
-    }
-    
-    /// Loads the offline preplanned map models with information from the downloaded mobile map
-    /// packages for the online map.
-    func loadOfflinePreplannedMapModels() async -> [PreplannedMapModel] {
-        let preplannedDirectory = URL.preplannedDirectory(forPortalItemID: portalItemID)
-        
-        guard let mapAreaIDs = try? FileManager.default.contentsOfDirectory(atPath: preplannedDirectory.path()) else { return [] }
-        
-        var preplannedMapModels: [PreplannedMapModel] = []
-        
-        for mapAreaID in mapAreaIDs {
-            guard let preplannedMapAreaID = Item.ID(mapAreaID),
-                  let mapArea = await makeMapArea(
-                    portalItemID: portalItemID,
-                    preplannedMapAreaID: preplannedMapAreaID
-                  ) else {
-                continue
-            }
-            let model = PreplannedMapModel(
-                offlineMapTask: offlineMapTask,
-                mapArea: mapArea,
-                portalItemID: portalItemID,
-                preplannedMapAreaID: mapArea.id!,
-                onRemoveDownload: onRemoveDownloadOfPreplannedArea(withID:)
-            )
-            preplannedMapModels.append(model)
-        }
-        
-        return preplannedMapModels
-            .filter(\.status.isDownloaded)
-            .sorted(by: { $0.preplannedMapArea.title < $1.preplannedMapArea.title })
-    }
-    
     /// The function called when a downloaded preplanned map area is removed.
     func onRemoveDownloadOfPreplannedArea(withID preplannedAreaID: Item.ID) {
         // Delete the saved map info if there are no more downloads for the
@@ -130,36 +61,18 @@ class OfflineMapViewModel: ObservableObject {
         OfflineManager.shared.removeMapInfo(for: portalItemID)
     }
     
-    /// Creates a preplanned map area using a given portal item and map area ID to search for a corresponding
-    /// downloaded mobile map package. If the mobile map package is not found then `nil` is returned.
-    /// - Parameters:
-    ///   - portalItemID: The portal item ID.
-    ///   - preplannedMapAreaID: The preplanned map area ID.
-    /// - Returns: The preplanned map area.
-    private func makeMapArea(
-        portalItemID: Item.ID,
-        preplannedMapAreaID: Item.ID
-    ) async -> OfflinePreplannedMapArea? {
-        let fileURL = URL.preplannedDirectory(
-            forPortalItemID: portalItemID,
-            preplannedMapAreaID: preplannedMapAreaID
+    func loadPreplannedMapModels() async {
+        let models = await PreplannedMapModel.loadPreplannedMapModels(
+            offlineMapTask: offlineMapTask,
+            portalItemID: portalItemID,
+            onRemoveDownload: onRemoveDownloadOfPreplannedArea(withID:)
         )
-        guard FileManager.default.fileExists(atPath: fileURL.path()) else { return nil }
-        let mmpk = MobileMapPackage(fileURL: fileURL)
-        
-        try? await mmpk.load()
-        guard let item = mmpk.item else { return nil }
-        
-        return .init(
-            title: item.title,
-            description: item.description,
-            id: preplannedMapAreaID,
-            thumbnail: item.thumbnail
-        )
+        preplannedMapModels = models.result
+        isShowingOnlyOfflineModels = models.onlyOfflineModelsAreAvailable
     }
     
     func loadOnDemandMapModels() async {
-        onDemandMapModels = await OnDemandMapModel.loadModels(portalItemID: portalItemID)
+        onDemandMapModels = await OnDemandMapModel.loadOnDemandMapModels(portalItemID: portalItemID)
     }
     
     func addOnDemandMapArea(with configuration: OnDemandMapAreaConfiguration) {
@@ -177,19 +90,5 @@ class OfflineMapViewModel: ObservableObject {
             // Download map area.
             await model.downloadOnDemandMapArea()
         }
-    }
-}
-
-private struct OfflinePreplannedMapArea: PreplannedMapAreaProtocol {
-    var title: String
-    var description: String
-    var id: Item.ID?
-    var packagingStatus: PreplannedMapArea.PackagingStatus?
-    var thumbnail: LoadableImage?
-    
-    func retryLoad() async throws {}
-    
-    func makeParameters(using offlineMapTask: OfflineMapTask) async throws -> DownloadPreplannedOfflineMapParameters {
-        fatalError()
     }
 }
