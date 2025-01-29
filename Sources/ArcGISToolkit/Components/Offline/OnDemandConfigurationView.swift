540320e05cb7dda4fe2e3e1404c7ed930fb01598
R Olson <rolson@esri.com>
2025-01-29 15:45:01 -0700

    Update Sources/ArcGISToolkit/Components/Offline/OnDemandConfigurationView.swift
    Co-authored-by: Destiny Hochhalter <117859673+des12437@users.noreply.github.com>

--- Sources/ArcGISToolkit/Components/Offline/OnDemandConfigurationView.swift
+++ Sources/ArcGISToolkit/Components/Offline/OnDemandConfigurationView.swift
@@ -27,7 +27,7 @@ struct OnDemandConfigurationView: View {
 ***REMOVED***let onCompleteAction: (OnDemandMapAreaConfiguration) -> Void
 ***REMOVED***
 ***REMOVED******REMOVED***/ The max scale of the map to take offline.
-***REMOVED***@State private var maxScale: CacheScale = .room
+***REMOVED***@State private var maxScale: CacheScale = .street
 ***REMOVED***
 ***REMOVED******REMOVED***/ The visible area of the map.
 ***REMOVED***@State private var visibleArea: Envelope?
