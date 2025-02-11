f8c604aadd11501e6c3dfd281b1994460d68cc7a
R Olson <rolson@esri.com>
2025-02-11 16:33:56 -0700

    Update Tests/ArcGISToolkitTests/PreplannedMapModelTests.swift
    Co-authored-by: Destiny Hochhalter <117859673+des12437@users.noreply.github.com>

--- Tests/ArcGISToolkitTests/PreplannedMapModelTests.swift
+++ Tests/ArcGISToolkitTests/PreplannedMapModelTests.swift
@@ -374,7 +374,7 @@ class PreplannedMapModelTests: XCTestCase {
 ***REMOVED******REMOVED***)
 ***REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED*** Clean up folder.
-***REMOVED******REMOVED***model.removeDownloadedPreplannedMapArea()
+***REMOVED******REMOVED***model.removeDownloadedArea()
 ***REMOVED******REMOVED***
 ***REMOVED******REMOVED******REMOVED*** Verify statuses after remove.
 ***REMOVED******REMOVED******REMOVED*** First give time for final status to come in.
