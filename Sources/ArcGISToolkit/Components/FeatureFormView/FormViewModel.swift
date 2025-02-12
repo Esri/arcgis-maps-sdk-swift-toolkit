04a8ac241ba1c3d91ece6250e48857e2aa3c8bfa
David Feinzimer <dfeinzimer@esri.com>
2025-02-12 10:24:19 -0800

    Update FormViewModel.swift

--- Sources/ArcGISToolkit/Components/FeatureFormView/FormViewModel.swift
+++ Sources/ArcGISToolkit/Components/FeatureFormView/FormViewModel.swift
@@ -40,6 +40,7 @@
 ***REMOVED******REMOVED***/ The visibility tasks group.
 ***REMOVED***private var isVisibleTask: Task<Void, Never>?
 ***REMOVED***
+#warning("TODO: This property to be removed.")
 ***REMOVED***let utilityNetwork: UtilityNetwork?
 ***REMOVED***
 ***REMOVED******REMOVED***/ Initializes a form view model.
