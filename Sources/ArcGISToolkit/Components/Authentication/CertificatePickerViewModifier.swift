a1b04ab61119db634e150d09dbaba05d19fb821e
Destiny Hochhalter <dhochhalterruiz@esri.com>
2025-02-19 15:34:25 -0800

    Use String.ok

--- Sources/ArcGISToolkit/Components/Authentication/CertificatePickerViewModifier.swift
+++ Sources/ArcGISToolkit/Components/Authentication/CertificatePickerViewModifier.swift
@@ -207,11 +207,7 @@ struct CertificatePickerViewModifier: ViewModifier {
                     }
                 ),
                 continueAction: .init(
-                    title: String(
-                        localized: "OK",
-                        bundle: .toolkitModule,
-                        comment: "A label for button to proceed with an operation."
-                    ),
+                    title: String.ok,
                     handler: { _, password in
                         viewModel.proceedToUseCertificate(withPassword: password)
                     }
