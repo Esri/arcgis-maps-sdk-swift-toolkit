15dea9058478cb9f58f4e24bb638f11bb267185a
David Feinzimer <dfeinzimer@esri.com>
2025-02-27 16:57:49 -0800

    First iteration of using core UNA types

--- Sources/ArcGISToolkit/Components/FeatureFormView/Subviews/UtilityNetworkAssociationFormElementView.swift
+++ Sources/ArcGISToolkit/Components/FeatureFormView/Subviews/UtilityNetworkAssociationFormElementView.swift
@@ -12,68 +12,201 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import ArcGIS
 import SwiftUI
 
-struct UtilityNetworkAssociationFormElementView: View {
-    @EnvironmentObject private var model: NavigationLayerModel
+/// <#Description#>
+struct UtilityAssociationsFormElementView: View {
+    /// <#Description#>
+    @EnvironmentObject private var formViewModel: FormViewModel
     
     /// <#Description#>
-    let description: String
+    @State private var associationsFilterResults = [UtilityAssociationsFilterResult]()
     
     /// <#Description#>
-    let associationKindGroups: [AssociationKindGroup]
-    
-    /// <#Description#>
-    let title: String
+    let element: UtilityAssociationsFormElement
     
     var body: some View {
-        VStack(alignment: .leading) {
-            // TODO: InputHeader to replace following in final implementation --
-            ///
-            Text(title)
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                .lineLimit(1)
-            //
-            // TODO: End InputHeader replacement section -----------------------
-            
-            // TODO: InputFooter to replace following in final implementation --
-            ///
-            Text(description)
-                .font(.caption)
-                .foregroundColor(.secondary)
-            //
-            // TODO: End InputFooter replacement section -----------------------
-            
-            FeatureFormGroupedContentView(
-                content: associationKindGroups.map { group in
-                    Button {
-                        model.push(
-                            title: group.name,
-                            subtitle: group.presentingForm
-                        ) {
-                            AssociationKindGroupView(associationKindGroup: group)
-                        }
-                    } label: {
-                        HStack {
-                            Text(group.name)
-                                .foregroundStyle(.primary)
-                            Spacer()
-                            Group {
-                                Text(group.networkSourceGroups.flatMap( { $0.associations.compactMap { $0 } } ).count.formatted())
-                                Image(systemName: "chevron.right")
-                            }
-                            .foregroundStyle(.secondary)
-                        }
-                    }
-                }
-            )
+        FeatureFormGroupedContentView(content: associationsFilterResults.compactMap {
+            if $0.resultCount > 0 {
+                UtilityAssociationsFilterResultListRowView(utilityAssociationsFilterResult: $0)
+                    .environmentObject(formViewModel)
+            } else {
+                nil
+            }
+        })
+        .task {
+            try? await element.fetchAssociationsFilterResults()
+            associationsFilterResults = element.associationsFilterResults
         }
     }
 }
 
-extension UtilityNetworkAssociationFormElementView {
-    struct Association: Identifiable {
+/// <#Description#>
+private struct UtilityAssociationGroupResultView: View {
+    /// The view model for the form.
+    @EnvironmentObject private var formViewModel: FormViewModel
+    
+    /// <#Description#>
+    @EnvironmentObject private var navigationLayerModel: NavigationLayerModel
+    
+    /// <#Description#>
+    let utilityAssociationGroupResult: UtilityAssociationGroupResult
+    
+    var body: some View {
+        List(utilityAssociationGroupResult.associationResults, id: \.associatedFeature.globalID) { utilityAssociationResult in
+            if let currentFeatureGlobalID = formViewModel.featureForm.feature.globalID {
+                let associatedElement = utilityAssociationResult.association.displayedElement(for: currentFeatureGlobalID)
+                let title: String = {
+                    if let formDefinitionTitle = associatedElement.networkSource.featureTable.featureFormDefinition?.title {
+                        formDefinitionTitle
+                    } else {
+                        "\(associatedElement.assetGroup.name) - \(associatedElement.objectID)"
+                    }
+                }()
+                let connection: UtilityAssociationView.Association.Connection? = switch utilityAssociationResult.association.kind {
+                case .junctionEdgeObjectConnectivityMidspan:
+                        .middle
+                case .connectivity, .junctionEdgeObjectConnectivityFromSide, .junctionEdgeObjectConnectivityToSide:
+                    currentFeatureGlobalID == utilityAssociationResult.association.fromElement.globalID ? .left : .right
+                default:
+                    nil
+                }
+                
+                UtilityAssociationView(
+                    association: UtilityAssociationView.Association(
+                        connectionPoint: connection,
+                        description: nil,
+                        fractionAlongEdge: utilityAssociationResult.association.fractionAlongEdge,
+                        name: title,
+                        selectionAction: {
+                            navigationLayerModel.push {
+                                InternalFeatureFormView(
+                                    featureForm: FeatureForm(feature: utilityAssociationResult.associatedFeature)
+                                )
+                            }
+                        },
+                        terminalName: associatedElement.terminal?.name
+                    )
+                )
+            }
+        }
+    }
+}
+
+/// <#Description#>
+private struct UtilityAssociationsFilterResultListRowView: View {
+    /// <#Description#>
+    @EnvironmentObject private var formViewModel: FormViewModel
+    
+    /// <#Description#>
+    @EnvironmentObject private var navigationLayerModel: NavigationLayerModel
+    
+    /// <#Description#>
+    let utilityAssociationsFilterResult: UtilityAssociationsFilterResult
+    
+    var body: some View {
+        Button {
+            navigationLayerModel.push {
+                UtilityAssociationsFilterResultView(utilityAssociationsFilterResult: utilityAssociationsFilterResult)
+                    .environmentObject(formViewModel)
+            }
+        } label: {
+            HStack {
+                Text("\(utilityAssociationsFilterResult.filter.filterType)".capitalized)
+                Spacer()
+                Text(utilityAssociationsFilterResult.resultCount.formatted())
+                Image(systemName: "chevron.right")
+            }
+        }
+    }
+}
+
+/// <#Description#>
+private struct UtilityAssociationsFilterResultView: View {
+    /// <#Description#>
+    @EnvironmentObject private var formViewModel: FormViewModel
+    
+    /// <#Description#>
+    @EnvironmentObject private var navigationLayerModel: NavigationLayerModel
+    
+    /// <#Description#>
+    let utilityAssociationsFilterResult: UtilityAssociationsFilterResult
+    
+    var body: some View {
+        List(utilityAssociationsFilterResult.groupResults, id: \.name) { utilityAssociationGroupResult in
+            Button(utilityAssociationGroupResult.name) {
+                navigationLayerModel.push {
+                    UtilityAssociationGroupResultView(utilityAssociationGroupResult: utilityAssociationGroupResult)
+                        .environmentObject(formViewModel)
+                }
+            }
+        }
+    }
+}
+
+/// <#Description#>
+private struct UtilityAssociationView: View {
+    /// <#Description#>
+    var association: Association
+    
+    var body: some View {
+        Button {
+            association.selectionAction()
+        } label: {
+            HStack {
+                if let connection = association.connectionPoint {
+                    let image: String = switch connection {
+                    case .left:
+                        "connection-end-left"
+                    case .middle:
+                        "connection-middle"
+                    case .right:
+                        "connection-end-right"
+                    }
+                    Image(image, bundle: .toolkitModule)
+                }
+                
+                VStack(alignment: .leading) {
+                    Text(association.name)
+                        .lineLimit(1)
+                    if let description = association.description {
+                        Text(description)
+                            .font(.caption2)
+                    }
+                }
+                Spacer()
+                Group {
+                    if let percent = association.fractionAlongEdge {
+                        Text(percent.formatted(.percent))
+                    } else if let terminal = association.terminalName {
+                        Text("Terminal: \(terminal)")
+                    }
+                }
+                .padding(2.5)
+                .background(Color(uiColor: .systemBackground))
+                .cornerRadius(5)
+                .font(.caption2)
+            }
+        }
+    }
+}
+
+private extension UtilityAssociation {
+    /// Determines whether to show the `fromElement` or `toElement`.
+    /// - Parameter id: <#id description#>
+    /// - Returns: <#description#>
+    func displayedElement(for id: UUID) -> UtilityElement {
+        if id == toElement.globalID {
+            fromElement
+        } else {
+            toElement
+        }
+    }
+}
+
+private extension UtilityAssociationView {
+    struct Association {
         /// <#Description#>
         enum Connection {
             case left
@@ -91,9 +224,6 @@ extension UtilityNetworkAssociationFormElementView {
         let fractionAlongEdge: Double?
         
         /// <#Description#>
-        let id = UUID()
-        
-        /// <#Description#>
         let name: String
         
         /// <#Description#>
@@ -101,120 +231,5 @@ extension UtilityNetworkAssociationFormElementView {
         
         /// <#Description#>
         let terminalName: String?
-    }
-    
-    struct AssociationView: View {
-        var association: Association
-        
-        var body: some View {
-            Button {
-                association.selectionAction()
-            } label: {
-                HStack {
-                    
-                    if let connection = association.connectionPoint {
-                        let image: String = switch connection {
-                        case .left:
-                            "connection-end-left"
-                        case .middle:
-                            "connection-middle"
-                        case .right:
-                            "connection-end-right"
-                        }
-                        Image(image, bundle: .toolkitModule)
-                    }
-                    
-                    VStack(alignment: .leading) {
-                        Text(association.name)
-                            .lineLimit(1)
-                        if let description = association.description {
-                            Text(description)
-                                .font(.caption2)
-                        }
-                    }
-                    Spacer()
-                    Group {
-                        if let percent = association.fractionAlongEdge {
-                            Text(percent.formatted(.percent))
-                        } else if let terminal = association.terminalName {
-                            Text("Terminal: \(terminal)")
-                        }
-                    }
-                    .padding(2.5)
-                    .background(Color(uiColor: .systemBackground))
-                    .cornerRadius(5)
-                    .font(.caption2)
-                }
-            }
-        }
-    }
-    
-    struct AssociationKindGroup: Identifiable {
-        /// <#Description#>
-        let networkSourceGroups: [NetworkSourceGroup]
-        
-        /// <#Description#>
-        let id = UUID()
-        
-        /// <#Description#>
-        let name: String
-        
-        /// <#Description#>
-        let presentingForm: String
-    }
-    
-    struct AssociationKindGroupView: View {
-        @EnvironmentObject private var model: NavigationLayerModel
-        
-        let associationKindGroup: AssociationKindGroup
-        
-        @State private var isExpanded = false
-        
-        var body: some View {
-            List(associationKindGroup.networkSourceGroups) { group in
-                Button {
-                    model.push(
-                        title: group.name,
-                        subtitle: group.presentingForm
-                    ) {
-                        NetworkSourceGroupView(networkSourceGroup: group)
-                    }
-                } label: {
-                    HStack {
-                        Text(group.name)
-                        Spacer()
-                        Text(group.associations.count.formatted())
-                    }
-                }
-                .listRowBackground(Color(uiColor: .tertiarySystemFill))
-            }
-            .scrollContentBackground(.hidden)
-        }
-    }
-    
-    struct NetworkSourceGroup: Identifiable {
-        /// <#Description#>
-        let associations: [Association]
-        
-        /// <#Description#>
-        let id = UUID()
-        
-        /// <#Description#>
-        let name: String
-        
-        /// <#Description#>
-        let presentingForm: String
-    }
-    
-    struct NetworkSourceGroupView: View {
-        let networkSourceGroup:  NetworkSourceGroup
-        
-        var body: some View {
-            List(networkSourceGroup.associations) {
-                AssociationView(association: $0)
-                    .listRowBackground(Color(uiColor: .tertiarySystemFill))
-            }
-            .scrollContentBackground(.hidden)
-        }
     }
 }
