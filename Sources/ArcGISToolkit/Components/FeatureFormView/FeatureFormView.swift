f3319b68add7beb7265268260ebfb0143b59b242
David Feinzimer <dfeinzimer@esri.com>
2025-03-06 13:55:30 -0800

    Proto

--- Sources/ArcGISToolkit/Components/FeatureFormView/FeatureFormView.swift
+++ Sources/ArcGISToolkit/Components/FeatureFormView/FeatureFormView.swift
@@ -121,6 +121,13 @@ public struct FeatureFormView: View {
                         FormFooter(featureForm: presentedForm, formHandlingEventAction: onFormEditingEventAction)
                     }
                 }
+                .backNavInter { continuation in
+                    if hasEdits {
+                        alertContinuation = (true, { continuation() } )
+                    } else {
+                        continuation()
+                    }
+                }
             }
             .alert(
                 "Discard Edits?",
