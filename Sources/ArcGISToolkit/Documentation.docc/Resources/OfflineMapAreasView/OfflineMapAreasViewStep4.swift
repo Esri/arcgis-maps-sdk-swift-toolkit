7273f0117db004d8092c5fcc1a33a4820af6cc2b
Destiny Hochhalter <dhochhalterruiz@esri.com>
2025-03-21 14:52:15 -0700

    Add OfflineMapAreasView tutorial

---
+++ Sources/ArcGISToolkit/Documentation.docc/Resources/OfflineMapAreasView/OfflineMapAreasViewStep4.swift
@@ -0,0 +1,42 @@
+import ArcGIS
+import ArcGISToolkit
+import SwiftUI
+
+@MainActor
+struct OfflineMapAreasExampleView: View {
+    /// The map of the Naperville water network.
+    @State private var onlineMap = Map(
+        item: PortalItem(
+            portal: .arcGISOnline(connection: .anonymous),
+            id: PortalItem.ID("acc027394bc84c2fb04d1ed317aac674")!
+        )
+    )
+    
+    /// The selected map.
+    @State private var selectedOfflineMap: Map?
+    
+    /// A Boolean value indicating whether the offline map ares view should be presented.
+    @State private var isShowingOfflineMapAreasView = false
+    
+    var body: some View {
+        NavigationStack {
+            MapView(map: selectedOfflineMap ?? onlineMap)
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Menu("Menu") {
+                            Button("Go Online") {
+                                selectedOfflineMap = nil
+                            }
+                            .disabled(selectedOfflineMap == nil)
+                            Button("Offline Maps") {
+                                isShowingOfflineMapAreasView = true
+                            }
+                        }
+                    }
+                }
+                .sheet(isPresented: $isShowingOfflineMapAreasView) {
+                    OfflineMapAreasView(onlineMap: onlineMap, selection: $selectedOfflineMap)
+                }
+        }
+    }
+}
