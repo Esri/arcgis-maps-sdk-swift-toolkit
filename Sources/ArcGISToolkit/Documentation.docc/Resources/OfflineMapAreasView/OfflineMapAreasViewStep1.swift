d966d984923903bf75ee0c439f2ce04f8316b77b
Destiny Hochhalter <dhochhalterruiz@esri.com>
2025-03-21 14:52:15 -0700

    Add OfflineMapAreasView tutorial

---
+++ Sources/ArcGISToolkit/Documentation.docc/Resources/OfflineMapAreasView/OfflineMapAreasViewStep1.swift
@@ -0,0 +1,20 @@
+***REMOVED***
+***REMOVED***Toolkit
+***REMOVED***
+
+@MainActor
+struct OfflineMapAreasExampleView: View {
+***REMOVED******REMOVED***/ The map of the Naperville water network.
+***REMOVED***@State private var onlineMap = Map(
+***REMOVED******REMOVED***item: PortalItem(
+***REMOVED******REMOVED******REMOVED***portal: .arcGISOnline(connection: .anonymous),
+***REMOVED******REMOVED******REMOVED***id: PortalItem.ID("acc027394bc84c2fb04d1ed317aac674")!
+***REMOVED******REMOVED***)
+***REMOVED***)
+***REMOVED***
+***REMOVED***var body: some View {
+***REMOVED******REMOVED***NavigationStack {
+***REMOVED******REMOVED******REMOVED***MapView(map: onlineMap)
+***REMOVED***
+***REMOVED***
+***REMOVED***
