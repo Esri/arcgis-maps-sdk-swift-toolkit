c79dde1f21bb7f1c1125a846e514eefce18ace3e
Ting Chen <tchen@esri.com>
2025-03-28 11:08:56 -0700

    Remove inaccurate initializer doc.

--- Sources/ArcGISToolkit/Components/Offline/OfflineMapAreasView.swift
+++ Sources/ArcGISToolkit/Components/Offline/OfflineMapAreasView.swift
@@ -112,9 +112,6 @@ public struct OfflineMapAreasView: View {
     }
     
     /// Creates a view with an offline-enabled web map.
-    ///
-    /// This initializer is typically used when the device is connected to the
-    /// network.
     /// - Parameters:
     ///   - onlineMap: The web map to be taken offline.
     ///   - selection: A binding to the currently selected offline map.
@@ -129,13 +126,10 @@ public struct OfflineMapAreasView: View {
     }
     
     /// Creates a view with a given offline map info.
-    ///
-    /// This initializer is typically used when the device is disconnected from
-    /// the network. The offline map info can be retrieved from ``OfflineManager/offlineMapInfos``.
-    /// To read more about the offline map info, see the API doc for ``OfflineMapInfo``.
     /// - Parameters:
     ///   - offlineMapInfo: The offline map info for which to create the view.
     ///   - selection: A binding to the currently selected offline map.
+    /// - SeeAlso: ``OfflineManager/offlineMapInfos``.
     public init(offlineMapInfo: OfflineMapInfo, selection: Binding<Map?>) {
         let item = PortalItem(url: offlineMapInfo.portalItemURL)!
         let onlineMap = Map(item: item)
