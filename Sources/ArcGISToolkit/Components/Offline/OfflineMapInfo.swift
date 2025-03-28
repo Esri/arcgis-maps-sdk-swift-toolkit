38774fd0619e99f2e15020fce6db3cd745bdabda
Ryan Olson <rolson@esri.com>
2025-03-28 14:09:20 -0600

    fix build

--- Sources/ArcGISToolkit/Components/Offline/OfflineMapInfo.swift
+++ Sources/ArcGISToolkit/Components/Offline/OfflineMapInfo.swift
@@ -69,6 +69,8 @@ extension OfflineMapInfo: Identifiable {
     public var id: Item.ID { .init(info.portalItemID)! }
 }
 
+private extension OfflineMapInfo {
+     /// The codable info is stored in json.
     struct CodableInfo: Codable {
         let portalItemID: String
         let title: String
