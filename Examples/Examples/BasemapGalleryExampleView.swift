43258689c42ab9172d5bac621bad14e105515aa4
David Feinzimer <dfeinzimer@esri.com>
2025-04-01 14:30:08 -0700

    Revert "Update .swiftlint.yml"
    This reverts commit 05af2e295710a877395874ecb351bea3c11af3df.

--- Examples/Examples/BasemapGalleryExampleView.swift
+++ Examples/Examples/BasemapGalleryExampleView.swift
@@ -65,17 +65,16 @@ struct BasemapGalleryExampleView: View {
     }
     
     private static func makeBasemapGalleryItems() -> [BasemapGalleryItem] {
-        [
+        let identifiers = [
             "46a87c20f09e4fc48fa3c38081e0cae6",
             "f33a34de3a294590ab48f246e99958c9",
-            "52bdc7ab7fb044d98add148764eaa30a", // Mismatched spatial reference
-            "3a8d410a4a034a2ba9738bb0860d68c4"  // Incorrect portal item type
+            "52bdc7ab7fb044d98add148764eaa30a",  // <<== mismatched spatial reference
+            "3a8d410a4a034a2ba9738bb0860d68c4"   // <<== incorrect portal item type
         ]
-            .map { identifier in
-                BasemapGalleryItem(basemap: Basemap(item: PortalItem(
-                    portal: .arcGISOnline(connection: .anonymous),
-                    id: Item.ID(identifier)!)
-                ))
-            }
+        
+        return identifiers.map { identifier in
+            let url = URL(string: "https://www.arcgis.com/home/item.html?id=\(identifier)")!
+            return BasemapGalleryItem(basemap: Basemap(item: PortalItem(url: url)!))
+        }
     }
 }
