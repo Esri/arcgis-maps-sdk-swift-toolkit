330f0821b4f70b0f782f69a3276be6ef66102df2
Destiny Hochhalter <dhochhalterruiz@esri.com>
2025-04-01 14:55:01 -0700

    Fix crash accessing credential

--- Examples/Examples/UtilityNetworkTraceExampleView.swift
+++ Examples/Examples/UtilityNetworkTraceExampleView.swift
@@ -37,6 +37,13 @@ struct UtilityNetworkTraceExampleView: View {
     /// A container for graphical trace results.
     @State private var resultGraphicsOverlay = GraphicsOverlay()
     
+    init() {
+        Task {
+            let publicSample = try? await ArcGISCredential.publicSample
+            ArcGISEnvironment.authenticationManager.arcGISCredentialStore.add(publicSample!)
+        }
+    }
+    
     var body: some View {
         GeometryReader { geometryProxy in
             MapViewReader { mapViewProxy in
@@ -49,10 +56,6 @@ struct UtilityNetworkTraceExampleView: View {
                 }
                 .onSingleTapGesture { _, mapPoint in
                     self.mapPoint = mapPoint
-                }
-                .task {
-                    let publicSample = try? await ArcGISCredential.publicSample
-                    ArcGISEnvironment.authenticationManager.arcGISCredentialStore.add(publicSample!)
                 }
 #if os(visionOS)
                 mapView
