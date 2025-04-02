da1da44814c58c19f17f94f2326921f352c3139f
David Feinzimer <dfeinzimer@esri.com>
2025-04-01 17:05:26 -0700

    Resolve deprecation warnings

--- Sources/ArcGISToolkit/Components/BasemapGallery/BasemapGalleryViewModel.swift
+++ Sources/ArcGISToolkit/Components/BasemapGallery/BasemapGalleryViewModel.swift
@@ -192,7 +192,7 @@ private extension BasemapGalleryViewModel {
                 } else {
                     basemaps = try await portal.basemaps
                 }
-                items += basemaps.map { BasemapGalleryItem(basemap: $0) }
+                items += basemaps.map { BasemapGalleryItem(basemap: $0, is3D: false) }
             } catch {
                 fetchBasemapsError = error
             }
@@ -205,7 +205,7 @@ private extension BasemapGalleryViewModel {
     func load(geoModel: GeoModel) async {
         try? await geoModel.load()
         if let basemap = geoModel.basemap {
-            currentItem = BasemapGalleryItem(basemap: basemap)
+            currentItem = BasemapGalleryItem(basemap: basemap, is3D: false)
         } else {
             currentItem = nil
         }
