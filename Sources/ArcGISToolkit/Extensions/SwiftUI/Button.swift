f90ce25bae66c28b752485e2a63d15765fdcc2fb
Destiny Hochhalter <dhochhalterruiz@esri.com>
2025-04-17 17:44:56 -0700

    Add Button.ok

--- Sources/ArcGISToolkit/Extensions/SwiftUI/Button.swift
+++ Sources/ArcGISToolkit/Extensions/SwiftUI/Button.swift
@@ -21,11 +21,7 @@ extension Button<Text> {
     /// - Returns: A button.
     static nonisolated func cancel(action: @escaping @MainActor () -> Void) -> Self {
         Button(role: .cancel, action: action) {
-            Text(
-                "Cancel",
-                bundle: .toolkitModule,
-                comment: "Title for a button that cancels an action."
-            )
+            Text(LocalizedStringResource.cancel)
         }
     }
     
@@ -37,6 +33,16 @@ extension Button<Text> {
         Button(action: action) {
             Text.done
                 .fontWeight(.semibold)
+        }
+    }
+    
+    /// Returns a button with the title "ok" and the given action.
+    /// - Parameter action: The action to perform when the user triggers the
+    /// button.
+    /// - Returns: A button.
+    static nonisolated func ok(action: @escaping @MainActor () -> Void) -> Self {
+        Button(action: action) {
+            Text(LocalizedStringResource.ok)
         }
     }
 }
