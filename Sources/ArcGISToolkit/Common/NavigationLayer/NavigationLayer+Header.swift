7ba6c042f08f292c9e60cffb17707b3ad95866c4
David Feinzimer <dfeinzimer@esri.com>
2025-04-28 16:24:05 -0700

    Add NavigationLayer

---
+++ Sources/ArcGISToolkit/Common/NavigationLayer/NavigationLayer+Header.swift
@@ -0,0 +1,116 @@
+// Copyright 2025 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import SwiftUI
+
+extension NavigationLayer {
+    struct Header: View {
+        @Environment(NavigationLayerModel.self) private var model
+        
+        @State private var height: CGFloat = .zero
+        
+        /// The optional closure to perform when the back navigation button is pressed.
+        let backNavigationAction: ((NavigationLayerModel) -> Void)?
+        
+        /// The header trailing content.
+        let headerTrailing: (() -> any View)?
+        
+        let width: CGFloat
+        
+        var body: some View {
+            HStack(alignment: .top) {
+                Group {
+                    Button {
+                        if let backNavigationAction {
+                            backNavigationAction(model)
+                        } else {
+                            model.pop()
+                        }
+                    } label: {
+                        let label = Label("Back", systemImage: "chevron.left")
+                        if model.title == nil {
+                            label
+                                .labelStyle(.titleAndIcon)
+                        } else {
+                            label
+                                .labelStyle(.iconOnly)
+                        }
+                    }
+                    .font(.title2)
+                }
+                .opacity(showsBack ? 1 : .zero)
+                .frame(!showsBack, width: width / 6)
+                if showsBack {
+                    Divider()
+                        .frame(height: height)
+                }
+                if !showsBack {
+                    Spacer()
+                }
+                Group {
+                    if let title = model.title, !title.isEmpty {
+                        VStack(alignment: showsBack ? .leading : .center) {
+                            Text(title)
+                                .bold()
+                            if let subtitle = model.subtitle, !subtitle.isEmpty  {
+                                Text(subtitle)
+                                    .font(.subheadline)
+                                    .foregroundStyle(.secondary)
+                            }
+                        }
+                        .lineLimit(1)
+                        .onGeometryChange(for: CGFloat.self) { proxy in
+                            proxy.size.height
+                        } action: { newValue in
+                            height = newValue
+                        }
+                    }
+                }
+                .frame(maxWidth: (width / 6) * 4, alignment: showsBack ? .leading : .center)
+                Spacer()
+                if let headerTrailing {
+                    AnyView(headerTrailing())
+                        .frame(width: width / 6, alignment: .trailing)
+                }
+            }
+            .padding(showsBack || (model.title != nil && !model.title!.isEmpty))
+        }
+        
+        var showsBack: Bool {
+            !model.views.isEmpty
+        }
+    }
+}
+
+fileprivate extension View {
+    @ViewBuilder
+    func frame(_ applied: Bool, width: CGFloat) -> some View {
+        if applied {
+            self.frame(width: width, alignment: .leading)
+        } else {
+            self
+        }
+    }
+    /// Optionally adds an equal padding amount to specific edges of this view.
+    /// - Parameter applied: A Boolean condition indicating whether padding is applied.
+    /// - Returns: A view that’s padded if specified.
+    @ViewBuilder
+    func padding(_ applied: Bool) -> some View {
+        if applied {
+            self.padding()
+        } else {
+            self
+        }
+    }
+}
---
+++ Sources/ArcGISToolkit/Common/NavigationLayer/NavigationLayer.swift
@@ -0,0 +1,244 @@
+// Copyright 2025 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import SwiftUI
+
+/// Navigation designed to work for components hosted in Floating Panels.
+///
+/// As of iOS 18, nested Navigation Stacks aren't supported. Because we don't know how a developer has
+/// presented a Toolkit component, NavigationLayer provides a simple navigation implementation that can be
+/// used in a component presented either modally (e.g. a Sheet) or non-modally (a Floating Panel).
+struct NavigationLayer<Content: View>: View {
+    @Environment(\.isPortraitOrientation) var isPortraitOrientation
+    
+    /// The header trailing content.
+    let headerTrailing: (() -> any View)?
+    
+    /// The footer content.
+    let footer: (() -> any View)?
+    
+    /// The root view.
+    let root: () -> Content
+    
+    /// The optional closure to perform when the back navigation button is pressed.
+    var backNavigationAction: ((NavigationLayerModel) -> Void)? = nil
+    
+    /// The closure to perform when model's path changes.
+    var onNavigationChangedAction: ((NavigationLayerModel.Item?) -> Void)?
+    
+    @State private var model = NavigationLayerModel()
+    
+    init(_ root: @escaping () -> Content) {
+        self.headerTrailing = nil
+        self.footer = nil
+        self.root = root
+    }
+    
+    init(_ root: @escaping () -> Content, @ViewBuilder headerTrailing: (@escaping () -> any View)) {
+        self.headerTrailing = headerTrailing
+        self.footer = nil
+        self.root = root
+    }
+    
+    init(_ root: @escaping () -> Content, @ViewBuilder footer: (@escaping () -> any View)) {
+        self.headerTrailing = nil
+        self.footer = footer
+        self.root = root
+    }
+    
+    init(_ root: @escaping () -> Content, @ViewBuilder headerTrailing: (@escaping () -> any View), @ViewBuilder footer: (@escaping () -> any View)) {
+        self.headerTrailing = headerTrailing
+        self.footer = footer
+        self.root = root
+    }
+    
+    var body: some View {
+        GeometryReader { geometryProxy in
+            VStack(spacing: 0) {
+                Header(
+                    backNavigationAction: backNavigationAction,
+                    headerTrailing: headerTrailing,
+                    width: geometryProxy.size.width
+                )
+                .frame(maxWidth: .infinity, alignment: .leading)
+                Group {
+                    if model.views.isEmpty {
+                        root()
+                            .transition(model.transition)
+                    } else if let presented = model.presented?.view {
+                        AnyView(presented())
+                            // Re-trigger the transition animation when view count changes.
+                            .id(model.views.count)
+                            .transition(model.transition)
+                    }
+                }
+                .onPreferenceChange(NavigationLayerTitle.self) { title in
+                    Task { @MainActor in
+                        self.model.title = title
+                    }
+                }
+                .onPreferenceChange(NavigationLayerSubtitle.self) { subtitle in
+                    Task { @MainActor in
+                        self.model.subtitle = subtitle
+                    }
+                }
+                if let footer {
+                    AnyView(footer())
+                        .padding()
+                        .padding([.bottom], isPortraitOrientation ? nil : .zero)
+                        .overlay(Divider(), alignment: .top)
+                        .transition(.move(edge: .bottom))
+                }
+            }
+            .environment(model)
+            // Apply container width so the animated transitions work correctly.
+            .frame(width: geometryProxy.size.width)
+            .onChange(of: model.views.count) {
+                onNavigationChangedAction?(model.presented ?? nil)
+            }
+        }
+    }
+}
+
+#Preview {
+    struct SampleList: View {
+        @Environment(NavigationLayerModel.self) private var model
+        @Binding var includeFooter: Bool
+        @Binding var includeHeader: Bool
+        @Binding var rootSubtitle: String
+        @Binding var rootTitle: String
+        
+        var body: some View {
+            List {
+                Section("Configuration") {
+                    Toggle("Include header trailing content", isOn: $includeHeader)
+                    Toggle("Include footer", isOn: $includeFooter)
+                    TextField("Root title", text: $rootTitle)
+                    TextField("Root subtitle", text: $rootSubtitle)
+                }
+                Section("Presentation") {
+                    Button("Present a view") {
+                        model.push {
+                            Text("View")
+                        }
+                    }
+                    Button("Present a view with a title") {
+                        model.push {
+                            Text("View")
+                                .navigationLayerTitle("Title")
+                        }
+                    }
+                    Button("Present a view with a title & subtitle") {
+                        model.push {
+                            Text("View")
+                                .navigationLayerTitle("Title", subtitle: "Subtitle")
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    @Previewable @State var includeHeader = false
+    @Previewable @State var includeFooter = false
+    @Previewable @State var isPresented = true
+    @Previewable @State var rootSubtitle = ""
+    @Previewable @State var rootTitle = ""
+    
+    return LinearGradient(
+        colors: [.red, .orange, .yellow],
+        startPoint: .topLeading, endPoint: .bottomTrailing
+    )
+    .onTapGesture {
+        isPresented.toggle()
+    }
+    .ignoresSafeArea(edges: .all)
+    .floatingPanel(isPresented: $isPresented) {
+        NavigationLayer {
+            SampleList(
+                includeFooter: $includeFooter,
+                includeHeader: $includeHeader,
+                rootSubtitle: $rootSubtitle,
+                rootTitle: $rootTitle
+            )
+            .navigationLayerTitle(rootTitle, subtitle: rootSubtitle)
+        } headerTrailing: {
+            if includeHeader {
+                Button("Done") {
+                    isPresented = false
+                }
+            }
+        } footer: {
+            if includeFooter {
+                Text("Footer")
+            }
+        }
+    }
+}
+
+extension NavigationLayer {
+    /// Sets a closure to perform when the back navigation button is pressed.
+    /// - Parameter action: The closure to perform when the back navigation button is pressed.
+    /// - Note: Use this to interrupt reverse navigation (e.g. to warn a user of unsaved edits). The closure
+    /// provides a reference to the navigation layer module which can be used to trigger the reverse
+    /// navigation when ready.
+    func backNavigationAction(perform action: @escaping (NavigationLayerModel) -> Void) -> Self {
+        var copy = self
+        copy.backNavigationAction = action
+        return copy
+    }
+    
+    /// Sets a closure to perform when the navigation layer's path changed.
+    /// - Parameter action: The closure to perform when the navigation layer's path changed
+    /// - Note: If no item is provided, the root view is presented..
+    func onNavigationPathChanged(perform action: @escaping (NavigationLayerModel.Item?) -> Void) -> Self {
+        var copy = self
+        copy.onNavigationChangedAction = action
+        return copy
+    }
+}
+
+struct NavigationLayerTitle: PreferenceKey {
+    static let defaultValue: String? = nil
+    
+    static func reduce(value: inout String?, nextValue: () -> String?) {
+        value = nextValue()
+    }
+}
+
+struct NavigationLayerSubtitle: PreferenceKey {
+    static let defaultValue: String? = nil
+    
+    static func reduce(value: inout String?, nextValue: () -> String?) {
+        value = nextValue()
+    }
+}
+
+extension View {
+    /// Sets a title for the navigation layer destination.
+    /// - Parameters:
+    ///   - title: The title for the navigation layer destination.
+    func navigationLayerTitle(_ title: String) -> some View {
+        preference(key: NavigationLayerTitle.self, value: title)
+    }
+    
+    /// Sets a title and subtitle for the navigation layer destination.
+    /// - Parameters:
+    ///   - title: The title for the navigation layer destination.
+    ///   - subtitle: The subtitle for the navigation layer destination.
+    func navigationLayerTitle(_ title: String, subtitle: String) -> some View {
+        preference(key: NavigationLayerTitle.self, value: title)
+            .preference(key: NavigationLayerSubtitle.self, value: subtitle)
+    }
+}
