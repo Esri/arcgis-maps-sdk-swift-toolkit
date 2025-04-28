7ee9278e5c4efcbce7a8ae00b5920b09c02bb89d
David Feinzimer <dfeinzimer@esri.com>
2025-04-28 16:24:05 -0700

    Add NavigationLayer

---
+++ Sources/ArcGISToolkit/Common/NavigationLayer/NavigationLayerModel.swift
@@ -0,0 +1,53 @@
+// Copyright 2025 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import SwiftUI
+
+@Observable class NavigationLayerModel {
+    struct Item {
+        let view: () -> any View
+    }
+    
+    private(set) var transition: AnyTransition = .push
+    
+    private(set) var views: [Item] = []
+    
+    /// The title for the current destination.
+    var title: String? = nil
+    
+    /// The subtitle for the current destination.
+    var subtitle: String? = nil
+    
+    var presented: Item? {
+        views.last
+    }
+    
+    /// Pop the current view.
+    func pop() {
+        guard !views.isEmpty else { return }
+        transition = .pop
+        withAnimation {
+            _ = views.removeLast()
+        }
+    }
+    
+    /// Push a view.
+    /// - Parameter view: The view to push.
+    func push(_ view: @escaping () -> any View) {
+        transition = .push
+        withAnimation {
+            views.append(.init(view: view))
+        }
+    }
+}
