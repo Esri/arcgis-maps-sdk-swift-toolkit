fb168c4ce0893b9f540060b7d6c68488a37149b5
David Feinzimer <dfeinzimer@esri.com>
2025-05-07 17:06:43 -0700

    FormViewModel -> InternalFeatureFormViewModel

--- Sources/ArcGISToolkit/Components/FeatureFormView/Subviews/Inputs/SwitchInput.swift
+++ Sources/ArcGISToolkit/Components/FeatureFormView/Subviews/Inputs/SwitchInput.swift
@@ -20,7 +20,7 @@ import SwiftUI
 /// The switch represents two mutually exclusive values, such as: yes/no, on/off, true/false.
 struct SwitchInput: View {
     /// The view model for the form.
-    @Environment(FormViewModel.self) private var formViewModel: FormViewModel
+    @Environment(InternalFeatureFormViewModel.self) private var internalFeatureFormViewModel
     
     /// A Boolean value indicating whether the initial element value was received.
     @State private var didReceiveInitialValue = false
@@ -82,7 +82,7 @@ struct SwitchInput: View {
             // defined for `isOn`.
             .onChange(of: isOn) {
                 element.updateValue(isOn ? input.onValue.code : input.offValue.code)
-                formViewModel.evaluateExpressions()
+                internalFeatureFormViewModel.evaluateExpressions()
             }
             // onValueChange(of:action:) is a good signal for user interaction
             // because it will reliably run when the view first loads and each
@@ -91,7 +91,7 @@ struct SwitchInput: View {
             .onValueChange(of: element) { newValue, newFormattedValue in
                 isOn = newFormattedValue == input.onValue.name
                 if didReceiveInitialValue {
-                    formViewModel.focusedElement = element
+                    internalFeatureFormViewModel.focusedElement = element
                 } else {
                     didReceiveInitialValue = true
                 }
