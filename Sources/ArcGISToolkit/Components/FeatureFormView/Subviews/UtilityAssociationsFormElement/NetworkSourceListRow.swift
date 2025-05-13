b675eb423f7018e603c5f05d836b4da42cd6822e
David Feinzimer <dfeinzimer@esri.com>
2025-05-13 09:58:24 -0700

    Create NetworkSourceListRow.swift

---
+++ Sources/ArcGISToolkit/Components/FeatureFormView/Subviews/UtilityAssociationsFormElement/NetworkSourceListRow.swift
@@ -0,0 +1,70 @@
+// Copyright 2025 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import ArcGIS
+import SwiftUI
+
+extension FeatureFormView.AddUtilityAssociationScreen {
+    struct NetworkSourceListRow: View {
+        let layer: Layer
+        
+        /// A Boolean value that indicates if a feature query is running.
+        @Binding var featureQueryIsRunning: Bool
+        
+        /// The set of identified/selected features.
+        @Binding var identifiedFeatures: [ArcGISFeature]
+        
+        /// A Boolean value that indicates if the layer represented by this row is being queried.
+        @State private var rowIsQuerying = false
+        
+        var body: some View {
+            Button {
+                if let featureLayer = layer as? FeatureLayer,
+                   let table = featureLayer.featureTable {
+                    Task {
+                        rowIsQuerying = true
+                        featureQueryIsRunning = true
+                        defer {
+                            featureQueryIsRunning = false
+                            rowIsQuerying = false
+                        }
+                        do {
+                            let queryParameters = QueryParameters()
+                            queryParameters.whereClause = "1=1"
+                            let featureQueryResult = try await table.queryFeatures(using: queryParameters)
+                            identifiedFeatures = Array(featureQueryResult.features()).compactMap { $0 as? ArcGISFeature }
+                        } catch {
+#warning("Present errors to user.")
+                            print(String(reflecting: error))
+                        }
+                    }
+                }
+            } label: {
+                HStack {
+                    Text(layer.name)
+                    Spacer()
+                    if rowIsQuerying {
+                        ProgressView()
+                            .progressViewStyle(.circular)
+                    } else {
+                        Image(systemName: "chevron.right")
+                    }
+                    
+                }
+                .contentShape(.rect)
+                .tint(.primary)
+            }
+        }
+    }
+}
