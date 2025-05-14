5581345495c207fecc1b848959ef4f281bd21f47
David Feinzimer <dfeinzimer@esri.com>
2025-05-14 14:36:41 -0700

    Create FeatureQueryConditionsView.swift

---
+++ Sources/ArcGISToolkit/Components/FeatureFormView/Subviews/UtilityAssociationsFormElement/AddUtilityAssociationView/FeatureQueryConditionsView/FeatureQueryConditionsView.swift
@@ -0,0 +1,52 @@
+// Copyright 2025 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import SwiftUI
+
+struct FeatureQueryConditionsView: View {
+    @Environment(FeatureFormView.AddUtilityAssociationView.Model.self) private var addUtilityAssociationViewModel
+    
+    @State private var conditions = [String]()
+    
+    var body: some View {
+        NavigationLayer { _ in
+            Group {
+                if conditions.isEmpty {
+                    NoConditionsAddedView(conditions: $conditions)
+                } else {
+                    ConditionsForm()
+                }
+            }
+            .navigationLayerTitle("Filter Features")
+        } headerTrailing: {
+            Button {
+                withAnimation {
+                    addUtilityAssociationViewModel.featureQueryConditionsViewIsPresented = false
+                }
+            } label: {
+                Text(
+                    "Apply",
+                    bundle: .toolkitModule,
+                    comment: "A button label to apply the conditions added to the query."
+                )
+            }
+        }
+#if os(visionOS)
+        .background(Color(uiColor: .tertiarySystemGroupedBackground))
+#else
+        .background(Color(uiColor: .systemGroupedBackground))
+#endif
+        .transition(.move(edge: .bottom))
+    }
+}
