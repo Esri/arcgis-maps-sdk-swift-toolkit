908fe16e56c9ed1a6ee89f487119b4769fafef13
David Feinzimer <dfeinzimer@esri.com>
2025-05-20 11:54:53 -0700

    Add new usage specific initializers for AttachmentsFeatureElementView

--- Sources/ArcGISToolkit/Components/Popups/PopupView.swift
+++ Sources/ArcGISToolkit/Components/Popups/PopupView.swift
@@ -175,7 +175,7 @@ extension PopupView {
 ***REMOVED******REMOVED******REMOVED******REMOVED***Group {
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***switch popupElement {
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***case let popupElement as AttachmentsPopupElement:
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***AttachmentsFeatureElementView(featureElement: popupElement)
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***AttachmentsFeatureElementView(popupElement: popupElement)
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***case let popupElement as FieldsPopupElement:
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***FieldsPopupElementView(popupElement: popupElement)
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***case let popupElement as MediaPopupElement:
