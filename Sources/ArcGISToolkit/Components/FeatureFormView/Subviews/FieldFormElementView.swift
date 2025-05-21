c9de49009954a5dc912362b736a42a7de466b087
David Feinzimer <dfeinzimer@esri.com>
2025-05-21 13:08:12 -0700

    Create FieldFormElementView.swift

---
+++ Sources/ArcGISToolkit/Components/FeatureFormView/Subviews/FieldFormElementView.swift
@@ -0,0 +1,50 @@
+// Copyright 2025 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import ArcGIS
+import SwiftUI
+
+struct FieldFormElementView: View {
+    /// A Boolean value indicating whether the input is editable.
+    @State private var isEditable = false
+    
+    /// The field form element the view belongs to.
+    let element: FieldFormElement
+    
+    var body: some View {
+        Group {
+            if isEditable {
+                switch element.input {
+                case is BarcodeScannerFormInput, is TextAreaFormInput, is TextBoxFormInput:
+                    TextInput(element: element)
+                case is ComboBoxFormInput:
+                    ComboBoxInput(element: element)
+                case is DateTimePickerFormInput:
+                    DateTimeInput(element: element)
+                case is RadioButtonsFormInput:
+                    RadioButtonsInput(element: element)
+                case is SwitchFormInput:
+                    SwitchInput(element: element)
+                default:
+                    EmptyView()
+                }
+            } else {
+                ReadOnlyInput(element: element)
+            }
+        }
+        .onIsEditableChange(of: element) { newIsEditable in
+            isEditable = newIsEditable
+        }
+    }
+}
