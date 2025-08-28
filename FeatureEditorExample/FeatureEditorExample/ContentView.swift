66c5ab5c8a7587cf36d56b652833c287f124e974
Caleb Rasmussen <crasmussen@esri.com>
2025-08-28 08:53:48 -0700

    Add example project.

---
+++ FeatureEditorExample/FeatureEditorExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FeatureEditorExample
+//
+//  Created by Caleb Rasmussen on 8/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FeatureEditorExample/FeatureEditorExample/FeatureEditorExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeatureEditorExampleApp.swift
+//  FeatureEditorExample
+//
+//  Created by Caleb Rasmussen on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct FeatureEditorExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
