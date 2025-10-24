708cc3422d1894ad5fc4b43cdaf820f5a5f6c27f
David Feinzimer <dfeinzimer@esri.com>
2025-10-24 11:51:26 -0700

    Create ShowOnMapButton.swift

---
+++ Sources/ArcGISToolkit/Components/FeatureFormView/Subviews/ShowOnMapButton.swift
@@ -0,0 +1,41 @@
+// Copyright 2025 Esri
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import ArcGIS
+import SwiftUI
+
+extension FeatureFormView {
+    struct ShowOnMapButton: View {
+        /// The closure to perform when a ``EditingEvent`` occurs.
+        @Environment(\.onFormEditingEventAction) var onFormEditingEventAction
+        
+        let feature: ArcGISFeature
+        
+        var body: some View {
+            Button {
+                onFormEditingEventAction?(.showOnMapRequested(feature))
+            } label: {
+                Label {
+                    Text(
+                        "Show On Map",
+                        bundle: .toolkitModule,
+                        comment: "A label for a button to show the indicated feature on the map."
+                    )
+                } icon: {
+                    Image(systemName: "scope")
+                }
+            }
+        }
+    }
+}
