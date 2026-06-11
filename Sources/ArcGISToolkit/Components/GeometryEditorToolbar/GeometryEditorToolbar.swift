0d4d57d60748723ec86df3e4fc5bbf1baa840cb3
Ting Chen <tchen@esri.com>
2026-06-11 15:50:13 -0700

    Move doc from GeometryEditorToolbar to FeatureEditorToolbar.

--- Sources/ArcGISToolkit/Components/GeometryEditorToolbar/GeometryEditorToolbar.swift
+++ Sources/ArcGISToolkit/Components/GeometryEditorToolbar/GeometryEditorToolbar.swift
@@ -17,32 +17,6 @@ import SwiftUI
 
 /// The `GeometryEditorToolbar` component allows users to perform common actions on a
 /// `GeometryEditor`.
-///
-/// **Features**
-///
-/// - Displays controls for performing common geometry editor actions:
-///     - Changing the tool.
-///     - Deleting the selected element.
-///     - Undoing the last action on the geometry.
-///     - Redoing the last undone action.
-///     - Configuring snap settings.
-/// - Supports styled vertical and horizontal layouts, or no built-in layout or styling.
-///
-/// **Behavior**
-///
-/// The toolbar is shown only while the geometry editor is started.
-///
-/// By default, the toolbar is displayed in a vertical layout. Pass `nil` for `style` to display
-/// the toolbar without built-in layout or styling, so you can show it in a system toolbar or apply
-/// your own layout and styling to the controls.
-///
-/// The settings button is only shown when the geometry editor has snap settings with non-empty
-/// source settings.
-///
-/// **Associated Types**
-///
-/// - ``Style``
-/// - ``SnapSourceTypes``
 struct GeometryEditorToolbar: View {
     /// The geometry editor that this toolbar controls.
     private let geometryEditor: GeometryEditor
