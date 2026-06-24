ec45700c8cf375b1f201f0ac697ab6cef48ef6c4
Ting Chen <tchen@esri.com>
2026-06-24 15:50:10 -0700

    Reorder imports.

--- Tests/ArcGISToolkitTests/FeatureEditorModelTests.swift
+++ Tests/ArcGISToolkitTests/FeatureEditorModelTests.swift
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import SwiftUI
 @testable import ArcGISToolkit
+import SwiftUI
 import Testing
 
 @Suite("FeatureEditorModel Tests")
