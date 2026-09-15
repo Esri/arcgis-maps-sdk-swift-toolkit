ac83e487b7ba265174ff9db30e6b6313cf746e39
Philip Ridgeway <pridgeway@esri.com>
2026-09-15 10:49:11 -0700

    Remove trailing whitespace from doc comment

--- Sources/ArcGISToolkit/Components/FeatureEditor/FeatureEditorModel.swift
+++ Sources/ArcGISToolkit/Components/FeatureEditor/FeatureEditorModel.swift
@@ -137,7 +137,7 @@ final class FeatureEditorModel {
     /// Adds shared templates from all operational layers of the map to this
     /// model.
     /// - Parameter map: The map from whose operational layers the shared
-    /// templates should be populated. 
+    /// templates should be populated.
     func populateSharedTemplates(from map: Map?) async {
         await featureAddingModel.populateSharedTemplates(from: map)
     }
